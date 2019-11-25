6a12afacff37c2023708f306b589ef3052c2afce
Akim Juillerat <akim.juillerat@camptocamp.com>
2019-11-25 17:32:14 +0100

    Move restrictions from stock_putaway_storage_type_strategy to base module
    Define max height/max weight restriction fields
    
    Dedicated tree view to edit package/location storage type relation
    
    Rename fields to drop stock_ prefix

--- stock_storage_type/models/stock_location.py
+++ stock_storage_type/models/stock_location.py
@@ -7,35 +7,41 @@ class StockLocation(models.Model):
 
     _inherit = 'stock.location'
 
-    stock_location_storage_type_ids = fields.Many2many(
+    location_storage_type_ids = fields.Many2many(
         'stock.location.storage.type',
-        'stock_location_stock_location_storage_type_rel',
-        'stock_location_id',
-        'stock_location_storage_type_id',
+        'stock_location_location_storage_type_rel',
+        'location_id',
+        'location_storage_type_id',
+        help="Location storage types defined here will be applied on all the "
+             "children locations that do not define their own location "
+             "storage types."
     )
 
-    allowed_stock_location_storage_type_ids = fields.Many2many(
+    allowed_location_storage_type_ids = fields.Many2many(
         'stock.location.storage.type',
-        'stock_location_allowed_stock_location_storage_type_rel',
-        'stock_location_id',
-        'stock_location_storage_type_id',
-        compute='_compute_allowed_stock_location_storage_type_ids',
+        'stock_location_allowed_location_storage_type_rel',
+        'location_id',
+        'location_storage_type_id',
+        compute='_compute_allowed_location_storage_type_ids',
         store=True,
+        help="Locations storage types that this location can accept. (If no "
+             "location storage types are defined on this specific location, "
+             "the location storage types of the parent location are applied)."
     )
 
     @api.depends(
-        'stock_location_storage_type_ids', 'location_id',
-        'location_id.allowed_stock_location_storage_type_ids'
+        'location_storage_type_ids', 'location_id',
+        'location_id.allowed_location_storage_type_ids'
     )
-    def _compute_allowed_stock_location_storage_type_ids(self):
+    def _compute_allowed_location_storage_type_ids(self):
         for location in self:
-            if location.stock_location_storage_type_ids:
-                location.allowed_stock_location_storage_type_ids = [
-                    (6, 0, location.stock_location_storage_type_ids.ids)
+            if location.location_storage_type_ids:
+                location.allowed_location_storage_type_ids = [
+                    (6, 0, location.location_storage_type_ids.ids)
                 ]
             else:
                 parent = location.location_id
-                location.allowed_stock_location_storage_type_ids = [
+                location.allowed_location_storage_type_ids = [
                     (6, 0,
-                     parent.allowed_stock_location_storage_type_ids.ids)
+                     parent.allowed_location_storage_type_ids.ids)
                 ]
