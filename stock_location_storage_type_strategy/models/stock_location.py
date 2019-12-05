179ab47d462bd722207055624b6813504a95caba
Akim Juillerat <akim.juillerat@camptocamp.com>
2019-12-05 19:07:44 +0100

    fixup! Add help on Packs storage strategy

--- stock_location_storage_type_strategy/models/stock_location.py
+++ stock_location_storage_type_strategy/models/stock_location.py
@@ -20,14 +20,14 @@ class StockLocation(models.Model):
              'None: when a pack is moved to this location, it will not be put'
              ' away any further.\n'
              'Ordered Children Locations: when a pack is moved to this '
-             'location, a suitable location will be searched in its children'
-             'locations according to the restrictions defined on their'
+             'location, a suitable location will be searched in its children '
+             'locations according to the restrictions defined on their '
              'respective location storage types.',
     )
     storage_location_sequence_ids = fields.One2many(
         'stock.storage.location.sequence',
         'location_id',
-        string='Storage locations',
+        string='Storage locations sequences',
     )
 
     @api.constrains('pack_storage_strategy', 'storage_location_sequence_ids')
