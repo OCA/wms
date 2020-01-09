31cd758f566d8d02d594b102c13d74304a8e1a28
Akim Juillerat <akim.juillerat@camptocamp.com>
2020-01-09 15:45:34 +0100

    [MIG] stock_storage_type: Migration to 13.0
    Add tests
    
    Fix dependency issue for pytest
    
    [IMP] pre-commit XML files
    
    Add setuptools
    
    Add module stock_storage_type_putaway_strategy
    
    [IMP] stock_storage_type_putaway_strategy: black, isort
    
    [MIG] stock_storage_type_putaway_strategy: Migration to 13.0
    
    Compute domain on stock.package_level location_dest_id according to storage_type restrictions
    
    Allow to compute restrictions for multiple products and quants
    
    Add constraint on stock.quant according to stock_storage_type
    
    Add tests
    
    Add tests on package destination domain
    
    Add package_level actual location to allowed domain
    
    Fix constraints do not mix products/lots

--- stock_storage_type/models/stock_package_storage_type.py
+++ stock_storage_type/models/stock_package_storage_type.py
@@ -2,6 +2,8 @@
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl)
 from odoo import fields, models
 
+from odoo.addons.base_m2m_custom_field.fields import Many2manyCustom
+
 
 class StockPackageStorageType(models.Model):
 
@@ -9,14 +11,14 @@ class StockPackageStorageType(models.Model):
     _description = "Package storage type"
 
     name = fields.Char(required=True)
-    location_storage_type_ids = fields.Many2manyCustom(
+    location_storage_type_ids = Many2manyCustom(
         "stock.location.storage.type",
         "stock_location_package_storage_type_rel",
         "package_storage_type_id",
         "location_storage_type_id",
         create_table=False,
         string="Allowed locations storage types",
-        help="Locations storage types that can accept such a package storage " "type.",
+        help="Locations storage types that can accept such a package storage type.",
     )
     product_packaging_ids = fields.One2many(
         "product.packaging", "package_storage_type_id"
