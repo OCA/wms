293b1e8791685d6d0a328f7cf8c4a6949d8ef041
Akim Juillerat <akim.juillerat@camptocamp.com>
2020-01-13 14:28:28 +0100

    [IMP] stock_storage_type_putaway_abc: black, isort

--- stock_storage_type_putaway_abc/models/product_template.py
+++ stock_storage_type_putaway_abc/models/product_template.py
@@ -1,11 +1,12 @@
 # Copyright 2019 Camptocamp SA
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl)
-from odoo import models, fields
+from odoo import fields, models
+
 from .stock_location import ABC_SELECTION
 
 
 class ProductTemplate(models.Model):
 
-    _inherit = 'product.template'
+    _inherit = "product.template"
 
-    abc_storage = fields.Selection(ABC_SELECTION, default='b')
+    abc_storage = fields.Selection(ABC_SELECTION, default="b")
