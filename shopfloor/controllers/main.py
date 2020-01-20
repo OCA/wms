4fa6c4afa13465d727f3b2199b225b229a5a6df2
Benoit <benoit.guillot@akretion.com>
2020-01-20 17:23:12 +0100

    add start controllers and services

--- shopfloor/controllers/main.py
+++ shopfloor/controllers/main.py
@@ -1,3 +1,11 @@
-from odoo import http
+from odoo.addons.base_rest.controllers import main
+import json
+
+
+class ShopfloorController(main.RestController):
+    _root_path = "/shopfloor/"
+    _collection_name = "shopfloor.services"
+    _default_auth = "api_key"
+
 
 
