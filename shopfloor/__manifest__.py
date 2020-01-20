678755f1ebf5e5bf91e551bd29ebd7bd45fd2f31
Alexandre Fayolle <alexandre.fayolle@camptocamp.com>
2020-01-20 16:08:05 +0100

    add some boilerplate

---
+++ shopfloor/__manifest__.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+# © 2020 Camptocamp, Akretion, BCIM
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+
+{
+    "name": "Shopfloor",
+    "summary": "manage warehouse operations with barcode scanners",
+    "version": "13.0.1.0.0",
+    "category": "Inventory",
+    "website": "https://odoo-community.org",
+    "author": "Akretion, BCIM, Camptocamp, Odoo Community Association (OCA)",
+    "licence": "AGPL-3",
+    "application": True,
+    "depends": [
+        "stock",
+        "base_rest",
+    ],
+    "data": [
+    ],
+}
