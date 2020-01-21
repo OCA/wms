5fefff7cbefb4df1d8771be38926ec68fb75f61d
Guewen Baconnier <guewen.baconnier@camptocamp.com>
2020-01-21 16:16:30 +0100

    Add schema validation for menu /search output

--- shopfloor/services/menu.py
+++ shopfloor/services/menu.py
@@ -1,6 +1,5 @@
+from odoo.addons.base_rest.components.service import to_int
 from odoo.addons.component.core import Component
-
-from odoo.addons.base_rest.components.service import skip_secure_response
 
 
 class ShopfloorMenu(Component):
@@ -8,14 +7,6 @@ class ShopfloorMenu(Component):
     _name = "shopfloor.menu"
     _usage = "menu"
     _expose_model = "shopfloor.menu"
-
-    @skip_secure_response
-    def search(self, name_fragment=None):
-        domain = self._get_base_search_domain()
-        if name_fragment:
-            domain.append(("name", "ilike", name_fragment))
-        records = self.env[self._expose_model].search(domain)
-        return {"size": len(records), "data": self._to_json(records)}
 
     def _get_base_search_domain(self):
         user = self.env.user
@@ -25,17 +16,32 @@ class ShopfloorMenu(Component):
             ("operation_group_ids.user_ids", "=", user.id),
         ]
 
+    def search(self, name_fragment=None):
+        domain = self._get_base_search_domain()
+        if name_fragment:
+            domain.append(("name", "ilike", name_fragment))
+        records = self.env[self._expose_model].search(domain)
+        return {"size": len(records), "data": self._to_json(records)}
+
     def _validator_search(self):
         return {
-            "name_fragment": {
-                "type": "string",
-                "nullable": True,
-                "required": False,
-            }
+            "name_fragment": {"type": "string", "nullable": True, "required": False}
+        }
+
+    def _validator_return_search(self):
+        return {
+            "size": {"coerce": to_int, "required": True, "type": "integer"},
+            "data": {
+                "type": "list",
+                "schema": {
+                    "type": "dict",
+                    "schema": {
+                        "id": {"coerce": to_int, "required": True, "type": "integer"},
+                        "name": {"type": "string", "nullable": False, "required": True},
+                    },
+                },
+            },
         }
 
     def _convert_one_record(self, record):
-        return {
-            "id": record.id,
-            "name": record.name,
-        }
+        return {"id": record.id, "name": record.name}
