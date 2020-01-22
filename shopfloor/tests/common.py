8092992eda8f65f2bfc0c2820b842ecb42ac9ecd
Guewen Baconnier <guewen.baconnier@camptocamp.com>
2020-01-22 13:41:36 +0100

    Fix initialization of tests
    Components have to be initalized (when running pytest-odoo, we don't
    need it, but to run odoo tests, we have to use the ComponentMixin
    to initialize the system).

--- shopfloor/tests/common.py
+++ shopfloor/tests/common.py
@@ -4,9 +4,14 @@ from odoo.tests.common import SavepointCase
 
 from odoo.addons.base_rest.controllers.main import _PseudoCollection
 from odoo.addons.component.core import WorkContext
+from odoo.addons.component.tests.common import ComponentMixin
 
 
-class CommonCase(SavepointCase):
+class CommonCase(SavepointCase, ComponentMixin):
+
+    # by default disable tracking suite-wise, it's a time saver :)
+    tracking_disable = True
+
     @contextmanager
     def work_on_services(self, **params):
         params = params or {}
@@ -14,3 +19,20 @@ class CommonCase(SavepointCase):
         yield WorkContext(
             model_name="rest.service.registration", collection=collection, **params
         )
+
+    # pylint: disable=method-required-super
+    # super is called "the old-style way" to call both super classes in the
+    # order we want
+    def setUp(self):
+        # Have to initialize both odoo env and stuff +
+        # the Component registry of the mixin
+        SavepointCase.setUp(self)
+        ComponentMixin.setUp(self)
+
+    @classmethod
+    def setUpClass(cls):
+        super(CommonCase, cls).setUpClass()
+        cls.env = cls.env(
+            context=dict(cls.env.context, tracking_disable=cls.tracking_disable)
+        )
+        cls.setUpComponent()
