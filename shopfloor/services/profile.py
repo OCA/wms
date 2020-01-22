057ff98808d9a0a27d687a51d36c42fe09351fa1
Guewen Baconnier <guewen.baconnier@camptocamp.com>
2020-01-22 13:20:09 +0100

    Rename 'device' to 'profile'
    We don't want to force having one device record per hardware device,
    since adding a new scanner would require to create a new device on Odoo.
    Instead, we prefer to have profiles, which has to be selected at loading
    of the client application. The profile will hold the configuration for
    the interactions (warehouse, but maybe later printer, ...).
    
    The allowed profiles can be restricted to groups. A user can be forced
    to use a profile.

--- shopfloor/services/profile.py
+++ shopfloor/services/profile.py
@@ -5,14 +5,27 @@ from odoo.addons.base_rest.components.service import to_int
 from odoo.addons.component.core import Component
 
 
-class ShopfloorDevice(Component):
+class ShopfloorProfile(Component):
+    """Profile storing the configuration for the interaction from the client.
+
+    A client application must use a profile, passed to every request in the
+    HTTP header (TODO put the name of the header).
+
+    The list of profiles available for a user is restricted by 2 things:
+
+    * If the profile has operation groups, the profile can be used only
+      if the user is at least in one of these groups.
+    * If the user has an assigned profile, the user can use only this profile.
+
+    """
+
     _inherit = "base.shopfloor.service"
-    _name = "shopfloor.device"
-    _usage = "device"
-    _expose_model = "shopfloor.device"
+    _name = "shopfloor.profile"
+    _usage = "profile"
+    _expose_model = "shopfloor.profile"
 
     def search(self, name_fragment=None):
-        """List available devices for current user"""
+        """List available profiles for current user"""
         domain = self._get_base_search_domain()
         if name_fragment:
             domain.append(("name", "ilike", name_fragment))
@@ -20,12 +33,12 @@ class ShopfloorDevice(Component):
         return {"size": len(records), "data": self._to_json(records)}
 
     def _get_base_search_domain(self):
-        # shopfloor_device_ids is a one2one
+        # shopfloor_profile_ids is a one2one in practice.
         base_domain = super()._get_base_search_domain()
         user = self.env.user
-        assigned_device = fields.first(user.shopfloor_device_ids)
-        if assigned_device:
-            return expression.AND([base_domain, [("id", "=", assigned_device.id)]])
+        assigned_profile = fields.first(user.shopfloor_profile_ids)
+        if assigned_profile:
+            return expression.AND([base_domain, [("id", "=", assigned_profile.id)]])
 
         return expression.AND(
             [
