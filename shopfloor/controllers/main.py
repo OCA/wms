c3459ab7671cd8780d2d191425fe0e6197f4ecce
Guewen Baconnier <guewen.baconnier@camptocamp.com>
2020-01-22 17:27:15 +0100

    Rework HTTP Header parameters
    * We need the menu id and the profile id, from there, we can find the
      configuration for the processes
    * Get recordsets in the work context, so we can conveniently use them
      from service components (and no actual SELECT is issued until we use
      them)
    * Return 400 BadRequest if a parameter is missing, but do not check
      existence of the record to avoid queries before we actually need the
      records, considering we won't need them for every request (a method
      toinspect a pack for instance does not care about the menu or the
      profile)

--- shopfloor/controllers/main.py
+++ shopfloor/controllers/main.py
@@ -1,3 +1,5 @@
+from werkzeug.exceptions import BadRequest
+
 from odoo.http import request
 
 from odoo.addons.base_rest.controllers import main
@@ -11,13 +13,22 @@ class ShopfloorController(main.RestController):
     def _get_component_context(self):
         """
         This method adds the component context:
-        * the process name
-        * the process menu
+        * the shopfloor menu in ``self.work.menu`` from the service Components
+        * the shopfloor profile in ``self.work.profile`` from the service
+          Components
         """
         res = super(ShopfloorController, self)._get_component_context()
         headers = request.httprequest.environ
-        for k, v in headers.items():
-            if k.startswith("HTTP_SERVICE_CTX_"):
-                key_name = k[17:].lower()
-                res[key_name] = v
+        try:
+            menu_id = int(headers.get("HTTP_SERVICE_CTX_MENU_ID"))
+        except (TypeError, ValueError):
+            raise BadRequest("HTTP_SERVICE_CTX_MENU_ID must be set with an integer")
+        res["menu"] = request.env["shopfloor.menu"].browse(menu_id)
+
+        try:
+            profile_id = int(headers.get("HTTP_SERVICE_CTX_PROFILE_ID"))
+        except (TypeError, ValueError):
+            raise BadRequest("HTTP_SERVICE_CTX_PROFILE_ID must be set with an integer")
+        res["profile"] = request.env["shopfloor.profile"].browse(profile_id)
+
         return res
