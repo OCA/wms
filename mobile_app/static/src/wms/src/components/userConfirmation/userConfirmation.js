4f33cd245826d435d9ff715d41fd29433c690467
Thierry Ducrest <thierry.ducrest@camptocamp.com>
2020-01-23 09:43:13 +0100

    Add user confirmation component

---
+++ mobile_app/static/src/wms/src/components/userConfirmation/userConfirmation.js
@@ -0,0 +1,18 @@
+var userConfirmation = Vue.component('user-confirmation', {
+  props: ['title', 'question'],
+  methods: {},
+  template: `
+
+    <div class="confirm">
+        <div class="alert alert-danger error" role="alert">
+            <h4 class="alert-heading">{{ this.title }}</h4>
+            <p>{{ question }}</p>
+            <form>
+               <input v-on:click="$emit('user-confirmation', 'yes')" class="btn btn-lg btn-success" type="submit" value="Yes"></input>
+               <input v-on:click="$emit('user-confirmation', 'no')" class="btn btn-lg btn-danger float-right" type="reset" value="No"></input>
+            </form>
+        </div>
+    </div>
+
+`,
+})
--- mobile_app/static/src/wms/src/scenario/pallet_transfer/pallet_transfer.js
+++ mobile_app/static/src/wms/src/scenario/pallet_transfer/pallet_transfer.js
@@ -1,8 +1,111 @@
+
+var odoo_service = {
+    'fetchOperation': function (barcode) {
+        console.log('ask odoo about barcode of pack', barcode.pack);
+        return Promise.resolve({ 'source': 'def', 'destination': 'abc', 'id': 1233232, 'name': 'PACK0001', 'barcode': barcode.pack});
+    },
+}
+
 var pt = Vue.component('pallet-transfer', {
-    template: `<div>
-    <h1><a href="#" class="btn btn-large btn-outline-secondary" style="margin-right:10px;">&lt;</a>Pallet Transfer</h1>
+    template: `
+        <div>
+        <h1><a href="#" class="btn btn-large btn-outline-secondary" style="margin-right:10px;">&lt;</a>Pallet Transfer</h1>
+        {{ current_state }}
+        <searchbar v-on:found="scanned" v-bind:hint="hint" v-bind:placeholder="scanTip">ici lasearch</searchbar>
+        <user-confirmation v-if="current_state=='confirmPallet'" v-on:user-confirmation="onUserConfirmation" v-bind:title="user_confirmation.title" v-bind:question="user_confirmation.question"></user-confirmation>
     </div>
-    `
+    `,
+    data: function () {
+        return {
+            'user_confirmation': {
+                'title': 'Pallet looks good.',
+                'question': 'Is that the goood one?',
+            },
+            'hint': 'pallet',
+            'operation': {},
+            // 'error_msg': '',
+            'current_state': 'getPallet',
+            'state': {
+                'getPallet': {
+                    enter: () => {
+                        this.hint = 'pallet';
+                    },
+                    on_scan: (scanned) => {
+                        this.go_state(
+                            'confirmPallet',
+                            odoo_service.fetchOperation({
+                                'pack': scanned
+                            })
+                        );
+                    },
+                },
+                'confirmPallet': {
+                    enter: () => {
+                    },
+                    on_scan: (scanned) => {
+                    },
+                    on_answer: (answer) => {
+                        if (answer == 'yes'){
+                            this.go_state('getLocation');
+                        } else {
+                            this.go_state('getPallet');
+                        }
+                    },
+                },
+                'getLocation': {
+                    enter: () => {
+                        this.hint = 'location';
+                    },
+                    on_scan: (scanned) => {
+                        this.go_state(
+                            'transferDone',
+                        );
+                    },
+                },
+                'transferDone': {
+                    enter: () => {
+                        this.go_state('getPallet');
+                    }
+                },
+                // 'waitFetchOperation': {
+                //     success: (result) => {
+                //         this.operation = result;
+                //         this.go_state('operationSet');
+                //     },
+                //     'error': (result) => {
+                //         console.error('no operation found');
+                //         this.go_state('init');
+                //     }
+                // },
+            }
+        };
+    },
+    methods: {
+        onUserConfirmation: function(answer){
+            this.state[this.current_state].on_answer(answer);
+        },
+        scanned: function(barcode) {
+            this.state[this.current_state].on_scan(barcode);
+        },
+        go_state: function(state, promise) {
+            if (this.state[this.current_state].exit)
+                this.state[this.current_state].exit();
+            this.current_state = state;
+            if (promise) {
+                promise.then(
+                    this.state[state].success,
+                    this.state[state].error,
+                );
+            } else {
+                this.state[state].enter();
+            }
+        },
+    },
+    computed: {
+      scanTip: function () {
+        return 'Scan ' + this.hint;
+      }
+    },
 });
 
 
