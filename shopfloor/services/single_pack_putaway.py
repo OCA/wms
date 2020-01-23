6166b4a66c0e6cf66807e8901e19c8ddb088651a
Benoit <benoit.guillot@akretion.com>
2020-01-23 12:20:53 +0100

    fix return format

--- shopfloor/services/single_pack_putaway.py
+++ shopfloor/services/single_pack_putaway.py
@@ -24,11 +24,11 @@ class SinglePackPutaway(Component):
         pack = self.env["stock.quant.package"].search([("name", "=", barcode)])
         if not pack:
             return {
-                "success": False,
-                "code": "not_found",
+                "state": "start",
                 "message": {
+                    "message_type": "error",
                     "title": "Pack not found",
-                    "body": "the pack %s doesn't exists" % barcode,
+                    "message": "the pack %s doesn't exists" % barcode,
                 },
             }
         allowed_locations = self.env["stock.location"].search(
@@ -36,11 +36,11 @@ class SinglePackPutaway(Component):
         )
         if pack.location_id not in allowed_locations:
             return {
-                "success": False,
-                "code": "forbidden",
+                "state": "start",
                 "message": {
+                    "message_type": "error",
                     "title": "do not process",
-                    "body": "pack %s is not in %s location"
+                    "message": "pack %s is not in %s location"
                     % (barcode, picking_type.default_location_src_id.name),
                 },
             }
@@ -53,11 +53,11 @@ class SinglePackPutaway(Component):
             and existing_operations[0].picking_id.picking_type_id != picking_type
         ):
             return {
-                "success": False,
-                "code": "forbidden",
+                "state": "start",
                 "message": {
+                    "message_type": "error",
                     "title": "do not process",
-                    "body": "An operation exists in %s %s. "
+                    "message": "An operation exists in %s %s. "
                     "You cannot process it with this shopfloor process."
                     % (
                         existing_operations[0].picking_id.picking_type_id.name,
@@ -84,11 +84,11 @@ class SinglePackPutaway(Component):
                     },
                     "picking": {"id": move.picking_id.id, "name": move.picking_id.name},
                 },
-                "success": False,
-                "code": "need_confirmation",
+                "state": "confirm_start",
                 "message": {
+                    "message_type": "warning",
                     "title": "Already started",
-                    "body": "Operation already running. "
+                    "message": "Operation already running. "
                     "Would you like to take it over ?",
                 },
             }
@@ -107,7 +107,7 @@ class SinglePackPutaway(Component):
             "company_id": company.id,
         }
         move = self.env["stock.move"].create(move_vals)
-        move._action_confirm()
+        move._action_confirm(merge=False)
         location_dest_id = (
             default_location_dest._get_putaway_strategy(product).id
             or default_location_dest.id
@@ -123,8 +123,13 @@ class SinglePackPutaway(Component):
             }
         )
         move.picking_id.action_assign()
-        return_vals = {
-            "success": True,
+        return {
+            "state": "scan_location",
+            "message": {
+                "message_type": "info",
+                "title": "Start",
+                "message": "The move is ready, you can scan the destination location.",
+            },
             "data": {
                 "id": move.move_line_ids[0].package_level_id.id,
                 "location_src": {
@@ -139,7 +144,6 @@ class SinglePackPutaway(Component):
                 "picking": {"id": move.picking_id.id, "name": move.picking_id.name},
             },
         }
-        return return_vals
 
     def validate(self, package_level_id, location_name, confirmation=False):
         package = self.env["stock.package_level"].browse(package_level_id)
@@ -162,9 +166,12 @@ class SinglePackPutaway(Component):
         )
         if dest_location not in allowed_locations:
             return {
-                "success": False,
-                "code": "forbidden",
-                "message": {"title": "Forbidden", "body": "You cannot place it here"},
+                "state": "scan_location",
+                "message": {
+                    "message_type": "error",
+                    "title": "Forbidden",
+                    "message": "You cannot place it here",
+                },
             }
         elif (
             dest_location in allowed_locations
@@ -172,27 +179,44 @@ class SinglePackPutaway(Component):
             and confirmation
         ):
             return {
-                "success": False,
-                "code": "need_confirmation",
-                "message": {"title": "Confirm", "body": "Are you sure ?"},
+                "state": "confirm_location",
+                "message": {
+                    "message_type": "warning",
+                    "title": "Confirm",
+                    "message": "Are you sure ?",
+                },
             }
         if move.state == "cancel":
             return {
-                "success": False,
-                "code": "restart",
+                "state": "start",
                 "message": {
+                    "message_type": "warning",
                     "title": "Restart",
-                    "body": "Restart the operation someone has canceld it.",
+                    "message": "Restart the operation someone has canceled it.",
                 },
             }
         move.move_line_ids[0].location_dest_id = dest_location.id
         move._action_done()
-        return {"success": True}
+        return {
+            "state": "start",
+            "message": {
+                "message_type": "info",
+                "title": "Start",
+                "message": "The pack has been moved, you can scan a new pack.",
+            },
+        }
 
     def cancel(self, package_level_id):
         package = self.env["stock.package_level"].browse(package_level_id)
         package.move_ids[0].cancel()
-        return {"success": True}
+        return {
+            "state": "start",
+            "message": {
+                "message_type": "info",
+                "title": "Start",
+                "message": "The move has been canceled, you can scan a new pack.",
+            },
+        }
 
     def _validator_cancel(self):
         return {
