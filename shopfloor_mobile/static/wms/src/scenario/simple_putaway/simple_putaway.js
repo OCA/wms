53ade1426fe8118d1e0fb00c764116d31cdf3e31
Simone Orsi <simahawk@gmail.com>
2020-01-24 15:53:55 +0100

    Cleanup `;`

--- shopfloor_mobile/static/wms/src/scenario/simple_putaway/simple_putaway.js
+++ shopfloor_mobile/static/wms/src/scenario/simple_putaway/simple_putaway.js
@@ -32,14 +32,14 @@ var sp = Vue.component('simple-pack-putaway', {
             'state': {
                 'scan_pack': {
                     enter: () => {
-                        this.hint = 'pack';
+                        this.hint = 'pack'
                         this.reset_erp_data('operation')
                     },
                     on_scan: (scanned) => {
                         this.go_state(
                             'wait_call',
                             odoo_service.fetchOperation(scanned)
-                        );
+                        )
                     },
                 },
                 'wait_call': {
@@ -51,21 +51,21 @@ var sp = Vue.component('simple-pack-putaway', {
                 },
                 'scan_location': {
                     enter: () => {
-                        this.hint = 'location';
-                        this.erp_data.operation.location_barcode = false;
+                        this.hint = 'location'
+                        this.erp_data.operation.location_barcode = false
                     },
                     on_scan: (scanned) => {
                         this.erp_data.operation.location_barcode = scanned
                         this.go_state('wait_validation',
-                            odoo_service.validate(this.erp_data.operation));
+                            odoo_service.validate(this.erp_data.operation))
                     }
                 },
                 'wait_validation': {
                     success: (result) => {
-                        this.go_state(result.state);
+                        this.go_state(result.state)
                     },
                     error: (result) => {
-                        this.go_state('scan_location');
+                        this.go_state('scan_location')
                     },
                 },
                 'confirm_location': { // this one may be mered with scan_location
@@ -74,39 +74,39 @@ var sp = Vue.component('simple-pack-putaway', {
                             this.go_state(
                                 'wait_validation',
                                 odoo_service.validate(this.erp_data.operation, true)
-                            );
+                            )
                         } else {
-                            this.go_state('scan_location');
+                            this.go_state('scan_location')
                         }
                     },
                     on_scan:(barcode) => {
-                        this.on_exit();
-                        this.current_state = 'scan_location';
-                        this.state[this.current_state].on_scan(barcode);
+                        this.on_exit()
+                        this.current_state = 'scan_location'
+                        this.state[this.current_state].on_scan(barcode)
                     }
                 },
                 'takeover': { // this one may be mered with scan_location
                     enter: () => {
-                        this.need_confirmation = true;
+                        this.need_confirmation = true
                     },
                     exit: () => {
-                        this.need_confirmation = false;
+                        this.need_confirmation = false
                     },
                     on_confirmation: (answer) => {
                         if (answer == 'yes'){
-                            this.go_state('scan_location');
+                            this.go_state('scan_location')
                         } else {
-                            this.go_state('scan_pack');
+                            this.go_state('scan_pack')
                         }
                     },
                     on_scan:(barcode) => {
-                        this.on_exit();
-                        this.current_state = 'scan_location';
-                        this.state[this.current_state].on_scan(barcode);
+                        this.on_exit()
+                        this.current_state = 'scan_location'
+                        this.state[this.current_state].on_scan(barcode)
                     }
                 },
             }
-        };
+        }
     },
     computed: {
       scanTip: function () {
@@ -120,23 +120,23 @@ var sp = Vue.component('simple-pack-putaway', {
         go_state: function(state, promise) {
             console.log('GO TO STATE', state)
             this.on_exit()
-            this.current_state = state;
+            this.current_state = state
             if (promise) {
                 promise.then(
                     this.on_success,
                     this.on_error,
-                );
+                )
             } else {
                 this.on_enter()
             }
         },
         on_enter: function () {
             if (this.state[this.current_state].enter)
-                this.state[this.current_state].enter();
+                this.state[this.current_state].enter()
         },
         on_exit: function () {
             if (this.state[this.current_state].exit)
-                this.state[this.current_state].exit();
+                this.state[this.current_state].exit()
         },
         on_success: function (result) {
             if (result.message) {
@@ -144,31 +144,31 @@ var sp = Vue.component('simple-pack-putaway', {
             } else {
                 this.reset_notification()
             }
-            this.state[this.current_state].success(result);
+            this.state[this.current_state].success(result)
         },
         on_error: function (result) {
-            this.state[this.current_state].error(result);
+            this.state[this.current_state].error(result)
         },
         scanned: function(barcode) {
-            this.state[this.current_state].on_scan(barcode);
+            this.state[this.current_state].on_scan(barcode)
         },
         reset: function (e) {
-            console.log('on reset ');
+            console.log('on reset ')
         },
         onUserConfirmation: function(answer){
-            this.state[this.current_state].on_confirmation(answer);
-            this.need_confirmation = false;
-            this.reset_notification();
+            this.state[this.current_state].on_confirmation(answer)
+            this.need_confirmation = false
+            this.reset_notification()
         },
         set_notification: function(message) {
-            this.user_notification.message = message.body;
-            this.user_notification.message_type = message.message_type;
-            console.log('USER NOTIF SET', this.user_notification);
+            this.user_notification.message = message.body
+            this.user_notification.message_type = message.message_type
+            console.log('USER NOTIF SET', this.user_notification)
         },
         reset_notification: function() {
-            this.user_notification.message = false;
-            this.user_notification.message_type = false;
-            console.log('USER NOTIF HIDE');
+            this.user_notification.message = false
+            this.user_notification.message_type = false
+            console.log('USER NOTIF RESET')
         },
         set_erp_data: function (key, data) {
             this.$set(this.erp_data, key, data)
@@ -178,7 +178,7 @@ var sp = Vue.component('simple-pack-putaway', {
             this.$set(this.erp_data, key, {})
         }
     }
-});
+})
 
 
 export function simple_putaway () { return sp }
