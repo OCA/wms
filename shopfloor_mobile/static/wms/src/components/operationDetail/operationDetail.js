c26dc90b25e614cca47b4cfc65ef0aeae35bbf0c
Simone Orsi <simahawk@gmail.com>
2020-01-24 14:49:02 +0100

    Operation detail: show real info

--- shopfloor_mobile/static/wms/src/components/operationDetail/operationDetail.js
+++ shopfloor_mobile/static/wms/src/components/operationDetail/operationDetail.js
@@ -4,26 +4,25 @@ var operationDetail = Vue.component('operation-detail', {
     return {};
   },
   props:['operation'],
-  methods: {},
   template: `
-  <div class="detail operation-detail">
+  <div class="detail operation-detail" v-if="!_.isEmpty(operation)">
     <div class="table-responsive">
       <table class="table">
-        <tr class="table-success">
+        <tr class="table-success" v-if="operation.name">
           <th>Name</th>
           <td>{{ operation.name }}</td>
         </tr>
-        <tr class="table-success">
+        <tr class="table-success" v-if="operation.barcode">
           <th>Barcode</th>
           <td>{{ operation.barcode }}</td>
         </tr>
         <tr class="table-success">
           <th class="table-success">Source</th>
-          <td>{{ operation.source }}</td>
+          <td>{{ operation.location_src.name }}</td>
         </tr>
         <tr class="table-warning">
           <th>Destination</th>
-          <td>{{ operation.destination }}</td>
+          <td>{{ operation.location_dst.name }}</td>
         </tr>
       </table>
     </div>
