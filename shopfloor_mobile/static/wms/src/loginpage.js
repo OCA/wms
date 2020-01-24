1d1599e7117e52901a70912bc5c954e633ddb4c2
hparfr <raph@hpar.fr>
2020-01-24 10:32:44 +0100

    Fix login

--- shopfloor_mobile/static/wms/src/loginpage.js
+++ shopfloor_mobile/static/wms/src/loginpage.js
@@ -3,7 +3,8 @@ var loginpage = Vue.component('login-page', {
         'apikey': '',
     }},
     methods: {
-        login: function() {
+        login: function(evt) {
+            evt.preventDefault();
             // call odoo application load => set the result in the local storage in json
 
             if (this.apikey == '123') {
@@ -17,12 +18,12 @@ var loginpage = Vue.component('login-page', {
     },
     template: `
 
-    <form>
+    <form v-on:submit="login">
     <h1 class="text-center">WMS</h1>
   <div class="form-group">
     <input type="text" class="form-control" v-model="apikey" placeholder="Scan your access badge or fill your credential" autofocus>
   </div>
-  <button type="button" class="btn btn-primary btn-block" v-on:click="login()">Login</button>
+  <input type="submit" class="btn btn-primary btn-block" >Login</button>
 </form>
 
 
