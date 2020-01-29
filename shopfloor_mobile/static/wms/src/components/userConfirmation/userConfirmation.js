9ba22855cc2678732daa2ba5c068b34a84d45839
Simone Orsi <simahawk@gmail.com>
2020-01-29 17:30:20 +0100

    Vuetify alerts

--- shopfloor_mobile/static/wms/src/components/userConfirmation/userConfirmation.js
+++ shopfloor_mobile/static/wms/src/components/userConfirmation/userConfirmation.js
@@ -3,15 +3,25 @@ var userConfirmation = Vue.component('user-confirmation', {
   methods: {},
   template: `
 
-    <div class="confirm">
-        <div class="alert alert-warning" role="alert">
-            <!--h4 class="alert-heading">{{ title }}</h4-->
-            <p>{{ question }}</p>
-            <form>
-               <input v-on:click="$emit('user-confirmation', 'yes')" class="btn btn-lg btn-success" type="submit" value="Yes"></input>
-               <input v-on:click="$emit('user-confirmation', 'no')" class="btn btn-lg btn-danger float-right" type="reset" value="No"></input>
-            </form>
-        </div>
+    <div class="confirm mt-4">
+        <v-alert type="warning">
+            {{ question }}
+            <v-form class="mt-4">
+                <v-btn
+                    color="success"
+                    type="submit"
+                    value="yes"
+                    @click="$emit('user-confirmation', 'yes')">Yes</v-btn>
+
+                <v-btn
+                    class="float-right"
+                    color="error"
+                    type="reset"
+                    value="no"
+                    @click="$emit('user-confirmation', 'no')">No</v-btn>
+            
+            </v-form>
+        </v-alert>
     </div>
 
 `,
