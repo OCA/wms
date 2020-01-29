64f8a90fa98df7b8de8ea1c51352ca84da3df404
Simone Orsi <simahawk@gmail.com>
2020-01-29 16:06:31 +0100

    shopfloor_mobile: fix depency and authors

--- shopfloor_mobile/__manifest__.py
+++ shopfloor_mobile/__manifest__.py
@@ -1,12 +1,12 @@
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 
 {
-    "name": "Mobbile app for WMS",
+    "name": "Mobile app for WMS",
     "summary": ".",
     "version": "13.0.1.0.0",
     "development_status": "Alpha",
-    "depends": ["stock"],
-    "author": "Akretion, Odoo Community Association (OCA)",
+    "depends": ["shopfloor"],
+    "author": "Camptocamp, Akretion, Odoo Community Association (OCA)",
     "website": "https://github.com/OCA/stock-logistics-warehouse",
     "category": "Warehouse Management",
     "license": "AGPL-3",
