f9cba921c8c968805a0f8521ee3787b804a1eeed
Guewen Baconnier <guewen.baconnier@camptocamp.com>
2020-02-04 16:54:48 +0100

    Extend tests coverage for single pack put-away

--- shopfloor/actions/base_action.py
+++ shopfloor/actions/base_action.py
@@ -2,6 +2,8 @@ from odoo.addons.component.core import AbstractComponent
 
 
 class ShopFloorProcessAction(AbstractComponent):
+    """Base Component for actions"""
+
     _name = "shopfloor.process.action"
     _collection = "shopfloor.action"
     _usage = "actions"
