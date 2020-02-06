71242c64e8b11096ec3e8b6485ca92bd657fe5a5
Thierry Ducrest <thierry.ducrest@camptocamp.com>
2020-02-06 16:11:09 +0100

    Add focus to search area

--- shopfloor_mobile/static/wms/src/components/searchbar/searchbar.js
+++ shopfloor_mobile/static/wms/src/components/searchbar/searchbar.js
@@ -1,27 +1,24 @@
 Vue.component('searchbar', {
-  data: function () {
-    return {
-      entered: '',
-    }
-  },
-  props:['input_placeholder'],
-  methods: {
-  	search: function(e,) {
-  		e.preventDefault();
-		this.$emit('found', this.entered);  //talk to parent
-		this.reset();
-  	},
-  	reset: function () {
-  		this.entered = '';
-  	}
-  },
+    data: function () {
+        return {
+            entered: '',
+        }
+    },
+    props:['input_placeholder'],
+    methods: {
+        search: function(e,) {
+            e.preventDefault();
+            this.$emit('found', this.entered);  //talk to parent
+            this.reset();
+        },
+        reset: function () {
+            this.entered = '';
+        }
+    },
 
-  template: `
-  <v-form
-      v-on:submit="search"
-      ref="form"
-      >
-    <v-text-field required v-model="entered" :placeholder="input_placeholder" ></v-text-field>
-  </v-form>
-  `
+    template: `
+    <v-form v-on:submit="search" ref="form" >
+    <v-text-field required v-model="entered" :placeholder="input_placeholder" autofocus></v-text-field>
+    </v-form>
+    `
 })
