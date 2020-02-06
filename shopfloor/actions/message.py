af1c5896832d6faf71328605e449fc7e12d9486a
Guewen Baconnier <guewen.baconnier@camptocamp.com>
2020-02-06 17:23:20 +0100

    Remove title from messages
    The message and kind for styling are enough. Title is often a bit random
    and we do not have much room on the UI anyway.

--- shopfloor/actions/message.py
+++ shopfloor/actions/message.py
@@ -2,8 +2,6 @@ from odoo import _
 
 from odoo.addons.component.core import Component
 
-# TODO remove 'title', useless
-
 
 class MessageAction(Component):
     """Provide message templates
@@ -22,28 +20,24 @@ class MessageAction(Component):
     def no_picking_type(self):
         return {
             "message_type": "error",
-            "title": _("Configuration error"),
             "message": _("No operation type found for this menu and profile."),
         }
 
     def several_picking_types(self):
         return {
             "message_type": "error",
-            "title": _("Configuration error"),
             "message": _("Several operation types found for this menu and profile."),
         }
 
     def package_not_found_for_barcode(self, barcode):
         return {
             "message_type": "error",
-            "title": _("Package not found"),
             "message": _("The package %s doesn't exist") % barcode,
         }
 
     def package_not_allowed_in_src_location(self, barcode, picking_type):
         return {
             "message_type": "error",
-            "title": _("Cannot proceed"),
             "message": _("You cannot work on a package (%s) outside of location: %s")
             % (barcode, picking_type.default_location_src_id.name),
         }
@@ -51,86 +45,62 @@ class MessageAction(Component):
     def already_running_ask_confirmation(self):
         return {
             "message_type": "warning",
-            "title": _("Already started"),
             "message": _(
                 "Operation's already running. Would you like to take it over?"
             ),
         }
 
     def scan_destination(self):
-        return {
-            "message_type": "info",
-            "title": _("Scan"),
-            "message": _("Scan the destination location"),
-        }
+        return {"message_type": "info", "message": _("Scan the destination location")}
 
     def operation_not_found(self):
         return {
             "message_type": "error",
-            "title": _("Start again"),
             "message": _("This operation does not exist anymore."),
         }
 
     def operation_has_been_canceled_elsewhere(self):
         return {
             "message_type": "warning",
-            "title": _("Restart"),
             "message": _("Restart the operation, someone has canceled it."),
         }
 
     def no_location_found(self):
         return {
             "message_type": "error",
-            "title": _("Scan"),
             "message": _("No location found for this barcode."),
         }
 
     def dest_location_not_allowed(self):
-        return {
-            "message_type": "error",
-            "title": _("Forbidden"),
-            "message": _("You cannot place it here"),
-        }
+        return {"message_type": "error", "message": _("You cannot place it here")}
 
     def need_confirmation(self):
-        return {
-            "message_type": "warning",
-            "title": _("Confirm"),
-            "message": _("Are you sure?"),
-        }
+        return {"message_type": "warning", "message": _("Are you sure?")}
 
     def confirm_pack_moved(self):
         return {
             "message_type": "info",
-            "title": _("Start"),
             "message": _("The pack has been moved, you can scan a new pack."),
         }
 
     def already_done(self):
-        return {
-            "message_type": "info",
-            "title": _("Continue"),
-            "message": _("Operation already processed."),
-        }
+        return {"message_type": "info", "message": _("Operation already processed.")}
 
     def confirm_canceled_scan_next_pack(self):
         return {
             "message_type": "info",
-            "title": _("Continue"),
             "message": _("Canceled, you can scan a new pack."),
         }
 
     def no_pack_in_location(self, location):
         return {
             "message_type": "error",
-            "title": _("Scan package"),
             "message": _("Location %s doesn’t contain any package." % location.name),
         }
 
     def several_packs_in_location(self, location):
         return {
             "message_type": "error",
-            "title": _("Scan package"),
             "message": _(
                 "Several PACKs found in %s, please scan one PACK." % location.name
             ),
@@ -139,6 +109,5 @@ class MessageAction(Component):
     def no_pending_operation_for_pack(self, pack):
         return {
             "message_type": "error",
-            "title": _("Error"),
             "message": _("No pending operation for package %s." % pack.name),
         }
