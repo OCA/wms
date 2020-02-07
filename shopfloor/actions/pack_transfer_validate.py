2da21d69e03c57b1bcc47c2ce3e457a6ea40d70b
Guewen Baconnier <guewen.baconnier@camptocamp.com>
2020-02-07 08:29:10 +0100

    Add method to check if location is a sublocation of another

--- shopfloor/actions/pack_transfer_validate.py
+++ shopfloor/actions/pack_transfer_validate.py
@@ -18,31 +18,16 @@ class PackTransferValidateAction(Component):
     def is_move_state_valid(self, move):
         return move.state != "cancel"
 
-    # TODO generic method "is_location_below"
     def is_dest_location_valid(self, move, scanned_location):
         """Forbid a dest location to be used"""
-        allowed_location = self.env["stock.location"].search_count(
-            [
-                (
-                    "id",
-                    "child_of",
-                    move.picking_id.picking_type_id.default_location_dest_id.id,
-                ),
-                ("id", "=", scanned_location.id),
-            ]
+        return scanned_location.is_sublocation_of(
+            move.picking_id.picking_type_id.default_location_dest_id
         )
-        return bool(allowed_location)
 
     def is_dest_location_to_confirm(self, move, scanned_location):
         """Destination that could be used but need confirmation"""
         move_dest_location = move.move_line_ids[0].location_dest_id
-        in_dest_location = self.env["stock.location"].search_count(
-            [
-                ("id", "child_of", move_dest_location.id),
-                ("id", "=", scanned_location.id),
-            ]
-        )
-        return not bool(in_dest_location)
+        return not scanned_location.is_sublocation_of(move_dest_location)
 
     def set_destination_and_done(self, move, scanned_location):
         move.move_line_ids[0].location_dest_id = scanned_location.id
