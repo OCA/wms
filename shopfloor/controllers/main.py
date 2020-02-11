9caea9f1c4b7accdb5b6e77697ec1b99e8420654
Simone Orsi <simahawk@gmail.com>
2020-02-11 11:48:28 +0100

    backend: special headers are needed for process only

--- shopfloor/controllers/main.py
+++ shopfloor/controllers/main.py
@@ -9,6 +9,7 @@ class ShopfloorController(main.RestController):
     _root_path = "/shopfloor/"
     _collection_name = "shopfloor.service"
     _default_auth = "api_key"
+    _non_process_services = ('app', 'menu', 'profile')
 
     def _get_component_context(self):
         """
@@ -19,16 +20,29 @@ class ShopfloorController(main.RestController):
         """
         res = super(ShopfloorController, self)._get_component_context()
         headers = request.httprequest.environ
+
+        res['menu'] = None
+        res['profile'] = None
+        if self._is_process_enpoint(request.httprequest.path):
+            res.update(self._get_process_context(headers, request.env))
+        return res
+
+    def _is_process_enpoint(self, request_path):
+        # '/shopfloor/app/user_config' -> app/config
+        service_path = request_path.split(self._root_path)[-1]
+        return not service_path.startswith(self._non_process_services)
+
+    def _get_process_context(self, headers, env):
+        ctx = {}
         try:
             menu_id = int(headers.get("HTTP_SERVICE_CTX_MENU_ID"))
         except (TypeError, ValueError):
             raise BadRequest("HTTP_SERVICE_CTX_MENU_ID must be set with an integer")
-        res["menu"] = request.env["shopfloor.menu"].browse(menu_id)
+        ctx["menu"] = env["shopfloor.menu"].browse(menu_id)
 
         try:
             profile_id = int(headers.get("HTTP_SERVICE_CTX_PROFILE_ID"))
         except (TypeError, ValueError):
             raise BadRequest("HTTP_SERVICE_CTX_PROFILE_ID must be set with an integer")
-        res["profile"] = request.env["shopfloor.profile"].browse(profile_id)
-
-        return res
+        ctx["profile"] = env["shopfloor.profile"].browse(profile_id)
+        return ctx
