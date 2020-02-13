1f45981a196d7a6698569e00b3129cbb4e3f18f0
Thierry Ducrest <thierry.ducrest@camptocamp.com>
2020-02-13 08:23:14 +0100

    Add login/auth to the router

--- shopfloor_mobile/static/wms/src/router.js
+++ shopfloor_mobile/static/wms/src/router.js
@@ -4,18 +4,28 @@ const Bar = { template: '<div>Bar</div>' }
 const NotFound = { template: '<div>Lost in the scanner app.</div>' }
 
 import {ScanAnything} from "./scenario/scan_anything.js";
+import {LoginPage} from './loginpage.js'
 
 const routes = [
   { path: '/foo', component: Foo },
   { path: '/bar', component: Bar },
   { path: '/', component: Home, name: 'home'},
+  { path: '/login', component: LoginPage, name: 'login'},
   { path: '/scananything/:codebar?', component: ScanAnything, name: 'scananything' },
   // TODO Fix this it needs to be the last route, but I think it is not anymore with the dynamic one added.
   // { path: '*', component: NotFound },
 ]
 
 const router = new VueRouter({
-    routes: routes
+    routes: routes,
+})
+router.beforeEach(async (to, from, next) => {
+    await Vue.nextTick()
+    if (!router.app.config.authenticated && to.name!='login'){
+        next('login')
+    } else {
+        next()
+    }
 })
 
 export {router};
