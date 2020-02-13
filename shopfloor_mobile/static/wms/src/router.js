cec21e5e491f3824475c24b658ba956e9a53abe5
Thierry Ducrest <thierry.ducrest@camptocamp.com>
2020-02-13 11:20:24 +0100

    Cleanup front router

--- shopfloor_mobile/static/wms/src/router.js
+++ shopfloor_mobile/static/wms/src/router.js
@@ -1,15 +1,10 @@
-const Home = { template: '<home-page v-bind:routes="routes"></home-page>', props: {routes:"myroutes"}} // { props: {routes: AllRoutes} }}
-const Foo = { template: '<div>foo</div>' }
-const Bar = { template: '<div>Bar</div>' }
-const NotFound = { template: '<div>Lost in the scanner app.</div>' }
-
+import {HomePage} from './homepage.js'
 import {ScanAnything} from "./scenario/scan_anything.js";
 import {LoginPage} from './loginpage.js'
+// const NotFound = { template: '<div>Lost in the scanner app.</div>' }
 
 const routes = [
-  { path: '/foo', component: Foo },
-  { path: '/bar', component: Bar },
-  { path: '/', component: Home, name: 'home'},
+  { path: '/', component: HomePage, name: 'home'},
   { path: '/login', component: LoginPage, name: 'login'},
   { path: '/scananything/:codebar?', component: ScanAnything, name: 'scananything' },
   // TODO Fix this it needs to be the last route, but I think it is not anymore with the dynamic one added.
