a4ff850fb1024c219fbb53d739bb4ab57824df6f
Guewen Baconnier <guewen.baconnier@camptocamp.com>
2020-02-20 10:14:08 +0100

    rest api: Start documenting Checkout service

--- shopfloor/services/__init__.py
+++ shopfloor/services/__init__.py
@@ -1,11 +1,17 @@
+# core classes
 from . import service
 from . import validator
+
+# generic services
 from . import app
-from . import profile
+from . import location
 from . import menu
 from . import pack
-from . import location
+from . import profile
+
+# process services
+from . import checkout
 from . import cluster_picking
+from . import picking_batch
 from . import single_pack_putaway
 from . import single_pack_transfer
-from . import picking_batch
