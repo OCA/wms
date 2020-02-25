83db7890310877184bc3bbd335f25a7c644c82cf
Guewen Baconnier <guewen.baconnier@camptocamp.com>
2020-02-25 15:30:08 +0100

    cluster_picking: refine /set_destination_all

--- shopfloor/models/__init__.py
+++ shopfloor/models/__init__.py
@@ -6,4 +6,5 @@ from . import shopfloor_profile
 from . import stock_location
 from . import stock_move_line
 from . import stock_picking_batch
+from . import stock_quant_package
 from . import res_users
