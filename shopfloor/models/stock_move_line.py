96dbe446e75bca93d0b09ca815d69705a0463d12
Simone Orsi <simahawk@gmail.com>
2020-02-25 09:33:09 +0100

    backend: prepare cluster_picking for /skip_line

--- shopfloor/models/stock_move_line.py
+++ shopfloor/models/stock_move_line.py
@@ -5,3 +5,9 @@ class StockMoveLine(models.Model):
     _inherit = "stock.move.line"
 
     shopfloor_unloaded = fields.Boolean(default=False)
+    shopfloor_postponed = fields.Boolean(
+        default=False,
+        copy=False,
+        help="Technical field. "
+             "Indicates if a the move has been postponed in a process.",
+    )
