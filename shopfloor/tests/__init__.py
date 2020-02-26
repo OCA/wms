bc4c90781a5694c03f52ebe8a7cdb2578bd3cb54
Simone Orsi <simahawk@gmail.com>
2020-02-26 10:31:37 +0100

    backend: split cluster_picking tests

--- shopfloor/tests/__init__.py
+++ shopfloor/tests/__init__.py
@@ -4,5 +4,9 @@ from . import test_profile
 from . import test_picking_batch
 from . import test_single_pack_putaway
 from . import test_single_pack_transfer
-from . import test_cluster_picking
+from . import test_cluster_picking_base
+from . import test_cluster_picking_select
+from . import test_cluster_picking_scan
+from . import test_cluster_picking_skip
+from . import test_cluster_picking_unload
 from . import test_checkout
