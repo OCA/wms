4509213daba4ab68be257c0a41c7af8bf6e42bc3
Simone Orsi <simahawk@gmail.com>
2020-02-28 12:44:45 +0100

    frontend: make alerts tiled

--- shopfloor_mobile/static/wms/src/scenario/single_pack_putaway.js
+++ shopfloor_mobile/static/wms/src/scenario/single_pack_putaway.js
@@ -4,8 +4,6 @@ export var SinglePackPutAway = Vue.component('single-pack-putaway', {
     mixins: [ScenarioBaseMixin, GenericStatesMixin],
     template: `
         <Screen :title="menuItem.name" :klass="usage">
-            <!-- FOR DEBUG -->
-            <!-- <v-alert type="warning">{{ current_state_key }}</v-alert> -->
             <searchbar v-on:found="on_scan" :input_placeholder="search_input_placeholder"></searchbar>
             <user-information v-if="!need_confirmation && user_notification.message" v-bind:info="user_notification"></user-information>
             <user-confirmation v-if="need_confirmation" v-on:user-confirmation="on_user_confirm" v-bind:question="user_notification.message"></user-confirmation>
