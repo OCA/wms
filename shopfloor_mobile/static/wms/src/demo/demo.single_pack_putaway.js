761de6a1540ec33639e51d6b4b93e92188a1de3c
Simone Orsi <simahawk@gmail.com>
2020-02-28 14:16:46 +0100

    frontend: fix demo data

--- shopfloor_mobile/static/wms/src/demo/demo.single_pack_putaway.js
+++ shopfloor_mobile/static/wms/src/demo/demo.single_pack_putaway.js
@@ -1,193 +1,193 @@
 /* eslint no-use-before-define: 0 */ // --> OFF
 var CANCEL_MSG_DEFAULT = {
-    'message': 'Operation cancelled',
-    'message_type': 'info',
+    "message": "Operation cancelled",
+    "message_type": "info",
 };
 
 var DEMO_SINGLE_PUTAWAY_1 = {
-    'start' : {
+    "start" : {
         "data": {
             "scan_location": {
                 "id": 1,
                 "name": "A nice pack 1",
                 "location_src": {
                     "id": 1,
-                    "name":  'Location SRC 1',
+                    "name":  "Location SRC 1",
                 },
                 "location_dst": {
                     "id": 2,
-                    "name": 'Location SRC 2',
+                    "name": "Location SRC 2",
                 },
-                "product": [{"id": 1, "name": 'Product 1', "qty": 5}, {"id": 2, "name": 'Product 2', "qty": 2}],
-                "picking": {"id": 1, "name": 'Picking 1'},
+                "product": [{"id": 1, "name": "Product 1", "qty": 5}, {"id": 2, "name": "Product 2", "qty": 2}],
+                "picking": {"id": 1, "name": "Picking 1"},
             },
         },
         "next_state": "scan_location",
         "message": undefined,
     },
-    'validate' : {
+    "validate" : {
         "data": undefined,
         "next_state": "start_scan_pack",
         "message": {
-            'message': 'Pack validated',
-            'message_type': 'info',
+            "message": "Pack validated",
+            "message_type": "success",
         },
     },
-    'cancel' : {
+    "cancel" : {
         "next_state": "start",
         "message": CANCEL_MSG_DEFAULT,
     },
 };
 
 var DEMO_SINGLE_PUTAWAY_2 = {
-    'start' : {
+    "start" : {
         "data": undefined,
         "next_state": "start_scan_pack",
         "message": {"message_type": "error", "message": "You cannot do that!"},
     },
 };
 var DEMO_SINGLE_PUTAWAY_3 = {
-    'start' : {
+    "start" : {
         "data": undefined,
         "next_state": "start_scan_pack",
         "message": {"message_type": "error", "message": "No pkg found"},
     },
 };
 var DEMO_SINGLE_PUTAWAY_4 = {
-    'start' : {
+    "start" : {
         "data": {
             "confirm_start": {
                 "id": 1,
                 "name": "A nice pack 4",
                 "location_src": {
                     "id": 1,
-                    "name":  'Location SRC 4',
+                    "name":  "Location SRC 4",
                 },
                 "location_dst": {
                     "id": 2,
-                    "name": 'Location DST 4',
+                    "name": "Location DST 4",
                 },
-                "product": {"id": 1, "name": 'Product 4'},
-                "picking": {"id": 1, "name": 'Picking 4'},
+                "product": {"id": 1, "name": "Product 4"},
+                "picking": {"id": 1, "name": "Picking 4"},
             },
         },
         "next_state": "confirm_start",
         "message": {"message_type": "info", "message": "Benoit is at the toilette: do you take over?"},
     },
-    'validate' : {
+    "validate" : {
         "data": undefined,
         "next_state": "start_scan_pack",
         "message": {
-            'message': 'Pack validated',
-            'message_type': 'info',
+            "message": "Pack validated",
+            "message_type": "success",
         },
     },
-    'cancel' : {
+    "cancel" : {
         "next_state": "start",
         "message": CANCEL_MSG_DEFAULT,
     },
 };
 var DEMO_SINGLE_PUTAWAY_5 = {
-    'start' : {
+    "start" : {
         "data": {
             "scan_location": {
                 "id": 1,
                 "name": "A nice pack 5",
                 "location_src": {
                     "id": 1,
-                    "name":  'Location SRC 5',
+                    "name":  "Location SRC 5",
                 },
                 "location_dst": {
                     "id": 2,
-                    "name": 'Location DST 5',
+                    "name": "Location DST 5",
                 },
-                "product": {"id": 1, "name": 'Product 5'},
-                "picking": {"id": 1, "name": 'Picking 5'},
+                "product": {"id": 1, "name": "Product 5"},
+                "picking": {"id": 1, "name": "Picking 5"},
             },
         },
         "next_state": "scan_location",
         "message": undefined,
     },
-    'cancel' : {
+    "cancel" : {
         "next_state": "start",
         "message": CANCEL_MSG_DEFAULT,
     },
-    'validate' : {
+    "validate" : {
         "data": undefined,
         "next_state": "start_scan_pack",
         "message": {
-            'message': 'Pack validated',
-            'message_type': 'info',
+            "message": "Pack validated",
+            "message_type": "success",
         },
     },
 };
 var DEMO_SINGLE_PUTAWAY_6 = {
-    'start' : {
+    "start" : {
         "data": {
             "scan_location": {
                 "id": 1,
                 "name": "A nice pack 6",
                 "location_src": {
                     "id": 1,
-                    "name":  'Location SRC 6',
+                    "name":  "Location SRC 6",
                 },
                 "location_dst": {
                     "id": 2,
-                    "name": 'Location DST 6',
+                    "name": "Location DST 6",
                 },
-                "product": {"id": 1, "name": 'Product 6'},
-                "picking": {"id": 1, "name": 'Picking 6'},
+                "product": {"id": 1, "name": "Product 6"},
+                "picking": {"id": 1, "name": "Picking 6"},
             },
         },
         "next_state": "scan_location",
         "message": undefined,
     },
-    'validate' : {
+    "validate" : {
         "data": undefined,
         "next_state": "confirm_location",
         "message": {"message_type": "warning", "message": "Are you sure of this location?"},
     },
-    'LOC6' : {
+    "LOC6" : {
         "data": undefined,
         "next_state": "start_scan_pack",
         "message": {
-            'message': 'Pack validated',
-            'message_type': 'info',
+            "message": "Pack validated",
+            "message_type": "success",
         },
     },
 };
 var DEMO_SINGLE_PUTAWAY_7 = {
-    'start' : {
+    "start" : {
         "data": {
             "scan_location": {
                 "id": 1,
                 "name": "A nice pack 7",
                 "location_src": {
                     "id": 1,
-                    "name":  'Location SRC 7',
+                    "name":  "Location SRC 7",
                 },
                 "location_dst": {
                     "id": 2,
-                    "name": 'Location DST 7',
+                    "name": "Location DST 7",
                 },
-                "product": {"id": 1, "name": 'Product 7'},
-                "picking": {"id": 1, "name": 'Picking 7'},
+                "product": {"id": 1, "name": "Product 7"},
+                "picking": {"id": 1, "name": "Picking 7"},
             },
         },
         "next_state": "scan_location",
         "message": undefined,
     },
-    'validate' : {
+    "validate" : {
         "data": undefined,
         "next_state": "scan_location",
         "message": {"message_type": "error", "message": "You cannot move to this location"},
     },
-    'LOC7' : {
+    "LOC7" : {
         "data": undefined,
         "next_state": "start_scan_pack",
         "message": {
-            'message': 'Pack validated',
-            'message_type': 'info',
+            "message": "Pack validated",
+            "message_type": "success",
         },
     },
 };
--- shopfloor_mobile/static/wms/src/demo/demo.single_pack_transfer.js
+++ shopfloor_mobile/static/wms/src/demo/demo.single_pack_transfer.js
@@ -1,13 +1,13 @@
 /* eslint no-use-before-define: 0 */ // --> OFF
 /* eslint no-use-before-define: 0 */ // --> OFF
 var CANCEL_MSG_DEFAULT = {
-    'message': 'Operation cancelled',
-    'message_type': 'info',
+    "message": "Operation cancelled",
+    "message_type": "info",
 };
 
 
 var DEMO_SINGLE_PACK_TRANSFER_1 = {
-    'start' : {
+    "start" : {
         "data": {
             "scan_location": {
                 "type": "pack",
@@ -15,55 +15,55 @@ var DEMO_SINGLE_PACK_TRANSFER_1 = {
                 "name": "A nice pack 1",
                 "location_src": {
                     "id": 1,
-                    "name":  'Location SRC 1',
+                    "name":  "Location SRC 1",
                 },
                 "location_dst": {
                     "id": 2,
-                    "name": 'Location SRC 2',
+                    "name": "Location SRC 2",
                 },
-                "product": [{"id": 1, "name": 'Product 1', "qty": 5}, {"id": 2, "name": 'Product 2', "qty": 2}],
-                "picking": {"id": 1, "name": 'Picking 1'},
+                "product": [{"id": 1, "name": "Product 1", "qty": 5}, {"id": 2, "name": "Product 2", "qty": 2}],
+                "picking": {"id": 1, "name": "Picking 1"},
             },
         },
         "next_state": "scan_location",
         "message": undefined,
     },
-    'validate' : {
+    "validate" : {
         "data": undefined,
         "next_state": "start",
         "message": {
-            'message': 'Pack validated',
-            'message_type': 'info',
+            "message": "Pack validated",
+            "message_type": "success",
         },
     },
-    'cancel' : {
+    "cancel" : {
         "next_state": "start",
         "message": CANCEL_MSG_DEFAULT,
     },
-    'LOC1' : {
+    "LOC1" : {
         "data": undefined,
         "next_state": "start",
         "message": {
-            'message': 'Pack validated',
-            'message_type': 'info',
+            "message": "Pack validated",
+            "message_type": "success",
         },
     },
-    'LOC2' : {
+    "LOC2" : {
         "data": {
             "show_completion_info": {
-                'last_operation_name': 'Last operation XYZ',
-                'next_operation_name': 'Next operation XYZ',
+                "last_operation_name": "Last operation XYZ",
+                "next_operation_name": "Next operation XYZ",
             },
         },
         "next_state": "show_completion_info",
         "message": {},
     },
-    'LOC3' : {
+    "LOC3" : {
         "data": undefined,
         "next_state": "start",
         "message": {
-            'message': 'Pack validated',
-            'message_type': 'info',
+            "message": "Pack validated",
+            "message_type": "success",
         },
     },
 };
