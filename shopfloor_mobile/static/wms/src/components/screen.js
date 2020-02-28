d6d81f7e5b9389403e68c4a076d01f97d30a2bfe
Simone Orsi <simahawk@gmail.com>
2020-02-28 11:22:03 +0100

    frontend: remove unused option menu

--- shopfloor_mobile/static/wms/src/components/screen.js
+++ shopfloor_mobile/static/wms/src/components/screen.js
@@ -64,26 +64,6 @@ Vue.component('Screen', {
             <v-btn icon @click="$router.push('scananything')">
                 <v-icon >mdi-magnify</v-icon>
             </v-btn>
-            <v-menu
-                left
-                bottom
-                >
-                <template v-slot:activator="{ on }">
-                    <v-btn icon v-on="on">
-                    <v-icon>mdi-dots-vertical</v-icon>
-                    </v-btn>
-                </template>
-
-                <v-list>
-                    <v-list-item
-                    v-for="n in 5"
-                    :key="n"
-                    @click="() => {}"
-                    >
-                    <v-list-item-title>Option {{ n }}</v-list-item-title>
-                    </v-list-item>
-                </v-list>
-            </v-menu>
         </v-app-bar>
         <v-content>
             <div class="header" v-if="$slots.header">
