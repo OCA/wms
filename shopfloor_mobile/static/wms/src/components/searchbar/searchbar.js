2cc7ee7dfdb64fed8184d1847577e49eec96441b
Simone Orsi <simahawk@gmail.com>
2020-02-28 09:17:42 +0100

    frontend: searchbar linting

--- shopfloor_mobile/static/wms/src/components/searchbar/searchbar.js
+++ shopfloor_mobile/static/wms/src/components/searchbar/searchbar.js
@@ -1,35 +1,39 @@
 Vue.component('searchbar', {
-    data: function () {
-        return {
-            entered: '',
-            autofocus: {
-                'type': Boolean,
-                'default': true,
-            },
-        };
+  data: function () {
+    return {
+      entered: '',
+      autofocus: {
+        'type': Boolean,
+        'default': true,
+      },
+    };
+  },
+  props: ['input_placeholder', 'input_data_type'],
+  methods: {
+    search: function (e, ) {
+      e.preventDefault();
+      this.$emit('found', {
+        text: this.entered,
+        type: e.target.dataset.type,
+      }); // Talk to parent
+      this.reset();
     },
-    props:['input_placeholder', 'input_data_type'],
-    methods: {
-  	search: function (e, ) {
-  		e.preventDefault();
-            this.$emit('found', {
-                text: this.entered,
-                type: e.target.dataset.type,
-            }); // Talk to parent
-            this.reset();
-        },
-  	reset: function () {
-  		this.entered = '';
-  	},
+    reset: function () {
+      this.entered = '';
     },
+  },
 
-    template: `
+  template: `
   <v-form
       v-on:submit="search"
       :data-type="input_data_type"
       ref="form"
       >
-    <v-text-field required v-model="entered" :placeholder="input_placeholder" :autofocus="autofocus ? 'autofocus' : null"></v-text-field>
+    <v-text-field
+      required v-model="entered"
+      :placeholder="input_placeholder"
+      :autofocus="autofocus ? 'autofocus' : null"
+      />
   </v-form>
   `,
 });
