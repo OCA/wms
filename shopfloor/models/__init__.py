75556302c67d8329481688f7288721aed518e1eb
Guewen Baconnier <guewen.baconnier@camptocamp.com>
2020-03-06 12:44:18 +0100

    cluster picking: implement /change_pack_lot
    When an operator picks a move line, they can use a button to replace the
    package or the lot.
    
    Replacing a lot
    ---------------
    
    The lot can be replaced when:
    
    * the move was not moving a package
    * or the move was moving a package, but the lot is in a single
      package in the current move line's location
    
    Replacing the lot may lead negative quants if the available quantity
    was insuficient, but physically, the quantity was there, so the
    situation should balance itself eventually, and when it happens, a draft
    inventory is created in the location for a user to check.
    
    Replacing a package
    -------------------
    
    The package can be replaced when:
    
    * the package content is the same, same products and quantities, but the
      lots can be different
    * the package is in another package level with the "is_done" flag, in
      this situation, the package is really not supposed to be here
    
    Some tricky situations may happen:
    
    * Physically, the replacement package is in the move line's location,
      but in the quants, it's elsewhere. It means it's an error in the data,
      and we should not prevent someone to pick it. The quant is updated
      with inventory moves to move the package in the correct location
      before doing the replacement.
    * The package is already reserved for another package level (not
      "is_done" yet). The operator may be allowed to choose another identical
      package, in this case, it switches the packages between the 2
      transfers (we know their content is the same, except the lots).

--- shopfloor/models/__init__.py
+++ shopfloor/models/__init__.py
@@ -3,6 +3,7 @@ from . import stock_picking_type
 from . import shopfloor_profile
 from . import stock_location
 from . import stock_move_line
+from . import stock_package_level
 from . import stock_picking
 from . import stock_picking_batch
 from . import stock_quant_package
