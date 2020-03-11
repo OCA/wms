4d0a458f1a4f8129024e12fb304e5559dac4a85a
Simone Orsi <simahawk@gmail.com>
2020-03-11 09:49:45 +0100

    frontend: checkout process selection + dropdown

--- shopfloor_mobile/static/wms/src/components/checkout_picking_detail.js
+++ shopfloor_mobile/static/wms/src/components/checkout_picking_detail.js
@@ -1,4 +1,4 @@
-export var checkout_picking_detail = Vue.component('checkout-picking-detail', {
+var CheckoutPickingDetailMixin = {
     props: {
         'info': Object,
         'grouped_lines': Array,
@@ -8,6 +8,7 @@ export var checkout_picking_detail = Vue.component('checkout-picking-detail', {
             'default': function () {
                 return {
                     'showActions': false,
+                    'list_item_component': 'checkout-select-content',
                 };
             },
         },
@@ -30,12 +31,14 @@ export var checkout_picking_detail = Vue.component('checkout-picking-detail', {
     <manual-select
         :records="info.lines"
         :grouped_records="grouped_lines"
-        :key_value="'id'"
-        :list_item_content_component="'checkout-select-content'"
         :options="select_options"
         />
 </div>
 `,
+};
+
+var checkout_picking_detail = Vue.component('checkout-picking-detail', {
+    mixins: [CheckoutPickingDetailMixin],
 });
 
 Vue.component('checkout-select-content', {
@@ -69,6 +72,26 @@ Vue.component('checkout-select-content', {
   `,
 });
 
+var checkout_summary_detail = Vue.component('checkout-summary-detail', {
+    mixins: [CheckoutPickingDetailMixin],
+    props: {
+        // TODO: maybe this should be provided by manual select component
+        'select_options': {
+            'type': Object,
+            'default': function () {
+                return {
+                    multiple: true,
+                    initSelectAll: true,
+                    showCounters: true,
+                    list_item_component: 'checkout-summary-content',
+                    list_item_extra_component: 'checkout-summary-extra-content',
+                };
+            },
+        },
+    },
+});
+
+
 Vue.component('checkout-summary-content', {
     props: {
         'record': Object,
@@ -76,26 +99,76 @@ Vue.component('checkout-summary-content', {
         'index': Number,
         'count': Number,
     },
+    methods: {
+        debug_it: function (record) {
+            console.dir(record);
+        },
+    },
     template: `
-    <div>
-        <div class="has_pack" v-if="record.pack">
-            <div class="item-counter">
-                <span>{{ index + 1 }} / {{ count }}</span>
-            </div>
-            <span>{{ record.pack.name }}</span>
+    <div class="summary-content">
+        <div class="has-pack" v-if="record.key != 'no-pack'">
+            <v-list-item-title>
+                <span class="item-counter">
+                    <span>{{ index + 1 }} / {{ count }}</span>
+                </span>
+                {{ record.title }}
+            </v-list-item-title>
         </div>
-        <div class="no_pack" v-if="!record.pack">
-            <div class="item-counter">
-                <span>{{ index + 1 }} / {{ count }}</span>
-            </div>
-            <span>{{ record.product.display_name }}</span>
-            <div class="lot" v-if="record.lot">
-                <span class="label">Lot:</span> <span>{{ record.lot.name }}</span>
-            </div>
-            <div class="qty">
-                <span class="label">Qty:</span> <span>{{ record.product.qty }}</span>
+        <div class="no-pack" v-else>
+            <div v-for="(subrec, i) in record.records">
+                <checkout-summary-product-detail :record="subrec" />
             </div>
         </div>
     </div>
-  `,
+    `,
+});
+
+
+Vue.component('checkout-summary-product-detail', {
+    props: {
+        'record': Object,
+    },
+    template: `
+        <div class="summary-content-item">
+            <v-list-item-title v-text="record.product.display_name"></v-list-item-title>
+            <v-list-item-subtitle>
+                <div class="lot" v-if="record.lot">
+                    <span class="label">Lot:</span> <span>{{ record.lot.name }}</span>
+                </div>
+                <div class="qty">
+                    <span class="label">Qty:</span> <span>{{ record.product.qty }}</span>
+                </div>
+            </v-list-item-subtitle>
+        </div>
+    `,
+});
+
+
+Vue.component('checkout-summary-extra-content', {
+    props: {
+        'record': Object,
+    },
+    methods: {
+        debug_it: function (record) {
+            console.dir(record);
+            debugger;
+        },
+    },
+    template: `
+    <div>
+    <!--{{ debug_it(record) }} -->
+    <v-expansion-panels flat v-if="record.key != 'no-pack' && record.records_by_pkg_type">
+        <v-expansion-panel v-for="pkg_type in record.records_by_pkg_type" :key="pkg_type.key">
+            <v-expansion-panel-header>
+                <span>{{ pkg_type.title }}</span>
+            </v-expansion-panel-header>
+            <v-expansion-panel-content>
+                <div v-for="(prod, i) in pkg_type.records">
+                    <checkout-summary-product-detail :record="prod" />
+                </div>
+            </v-expansion-panel-content>
+        </v-expansion-panel>
+    </v-expansion-panels>
+    </div>
+    `,
 });
