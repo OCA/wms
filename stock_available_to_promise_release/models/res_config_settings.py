e7d6fe8db082c5b2f17f9c686ab8999d63658394
Simone Orsi <simahawk@gmail.com>
2020-03-23 12:42:44 +0100

    stock_available_to_promise_release: fix settings edit

--- stock_available_to_promise_release/models/res_config_settings.py
+++ stock_available_to_promise_release/models/res_config_settings.py
@@ -9,5 +9,5 @@ class ResConfigSettings(models.TransientModel):
     _inherit = "res.config.settings"
 
     stock_reservation_horizon = fields.Integer(
-        related="company_id.stock_reservation_horizon",
+        related="company_id.stock_reservation_horizon", readonly=False,
     )
