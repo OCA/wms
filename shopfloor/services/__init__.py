f583933d6bf2835b288c0af9182af1c7cca85760
Guewen Baconnier <guewen.baconnier@camptocamp.com>
2020-03-24 10:02:43 +0100

    Add skeleton for delivery service

--- shopfloor/services/__init__.py
+++ shopfloor/services/__init__.py
@@ -13,6 +13,7 @@ from . import profile
 # process services
 from . import checkout
 from . import cluster_picking
+from . import delivery
 from . import picking_batch
 from . import single_pack_putaway
 from . import single_pack_transfer
