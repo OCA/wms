a80de40517f2363489c6704084b0fcab9fb16d03
Guewen Baconnier <guewen.baconnier@camptocamp.com>
2020-03-26 08:07:28 +0100

    checkout: implement /list_dest_package

--- shopfloor/tests/__init__.py
+++ shopfloor/tests/__init__.py
@@ -18,5 +18,6 @@ from . import test_checkout_select_line
 from . import test_checkout_set_qty
 from . import test_checkout_scan_package_action
 from . import test_checkout_new_package
+from . import test_checkout_list_package
 from . import test_checkout_summary
 from . import test_delivery_base
