e1b0e27f10350e000809fad20262bc8649d6fa14
Simone Orsi <simahawk@gmail.com>
2020-04-29 11:09:58 +0200

    backend: checkout summary load only done lines

--- shopfloor/tests/test_checkout_done.py
+++ shopfloor/tests/test_checkout_done.py
@@ -7,7 +7,7 @@ class CheckoutDoneCase(CheckoutCommonCase):
         self._fill_stock_for_moves(picking.move_lines, in_package=True)
         picking.action_assign()
         # line is done
-        picking.move_line_ids.write({"qty_done": 10, "shopfloor_checkout_packed": True})
+        picking.move_line_ids.write({"qty_done": 10, "shopfloor_checkout_done": True})
         response = self.service.dispatch("done", params={"picking_id": picking.id})
 
         self.assertRecordValues(picking, [{"state": "done"}])
@@ -33,8 +33,8 @@ class CheckoutDonePartialCase(CheckoutCommonCase):
         picking.action_assign()
         cls.line1 = picking.move_line_ids[0]
         cls.line2 = picking.move_line_ids[1]
-        cls.line1.write({"qty_done": 10, "shopfloor_checkout_packed": True})
-        cls.line2.write({"qty_done": 2, "shopfloor_checkout_packed": True})
+        cls.line1.write({"qty_done": 10, "shopfloor_checkout_done": True})
+        cls.line2.write({"qty_done": 2, "shopfloor_checkout_done": True})
 
     def test_done_partial(self):
         # line is done
@@ -45,7 +45,7 @@ class CheckoutDonePartialCase(CheckoutCommonCase):
         self.assert_response(
             response,
             next_state="confirm_done",
-            data={"picking": self._stock_picking_data(self.picking)},
+            data={"picking": self._stock_picking_data(self.picking, done=True)},
             message={
                 "message_type": "warning",
                 "message": "Not all lines have been processed, do"
@@ -87,11 +87,11 @@ class CheckoutDoneRawUnpackedCase(CheckoutCommonCase):
         cls.line1.write(
             {
                 "qty_done": 10,
-                "shopfloor_checkout_packed": True,
+                "shopfloor_checkout_done": True,
                 "result_package_id": cls.package.id,
             }
         )
-        cls.line2.write({"qty_done": 10, "shopfloor_checkout_packed": False})
+        cls.line2.write({"qty_done": 10, "shopfloor_checkout_done": False})
 
     def test_done_partial(self):
         # line is done
@@ -102,7 +102,7 @@ class CheckoutDoneRawUnpackedCase(CheckoutCommonCase):
         self.assert_response(
             response,
             next_state="confirm_done",
-            data={"picking": self._stock_picking_data(self.picking)},
+            data={"picking": self._stock_picking_data(self.picking, done=True)},
             message={
                 "message_type": "warning",
                 "message": "Remaining raw product not packed, proceed anyway?",
