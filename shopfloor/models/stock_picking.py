932c0d4c7ccf41709e79409b1204d85550c2d853
Simone Orsi <simahawk@gmail.com>
2020-05-06 12:29:37 +0200

    backend: add info fields for pickings

---
+++ shopfloor/models/stock_picking.py
@@ -0,0 +1,32 @@
+from odoo import api, fields, models
+
+
+class StockPicking(models.Model):
+    _inherit = "stock.picking"
+
+    total_weight = fields.Float(
+        compute="_compute_picking_info",
+        help="Technical field. Indicates total weight of pickings included.",
+    )
+    move_line_count = fields.Integer(
+        compute="_compute_picking_info",
+        help="Technical field. Indicates number of move lines included.",
+    )
+
+    @api.depends(
+        "move_line_ids", "move_line_ids.product_qty", "move_line_ids.product_id.weight"
+    )
+    def _compute_picking_info(self):
+        for item in self:
+            item.update(
+                {
+                    "total_weight": item._calc_weight(),
+                    "move_line_count": len(item.move_line_ids),
+                }
+            )
+
+    def _calc_weight(self):
+        weight = 0.0
+        for move_line in self.mapped("move_line_ids"):
+            weight += move_line.product_qty * move_line.product_id.weight
+        return weight
