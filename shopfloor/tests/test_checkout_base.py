fa06fe5d5b4b4bf51688fd0c0722dd7d72e80892
Simone Orsi <simahawk@gmail.com>
2020-05-06 16:12:05 +0200

    backend: REF data action w/ jsonify
    * use jsonify
    * provide multiple + single converter
    * use it all around (except pack transfer scenario as it needs
    refactoring)

--- shopfloor/tests/test_checkout_base.py
+++ shopfloor/tests/test_checkout_base.py
@@ -21,7 +21,7 @@ class CheckoutCommonCase(CommonCase):
 
     # we test the methods that structure data in test_actions_data.py
     def _picking_summary_data(self, picking):
-        return self.service.actions_for("data").picking_summary(picking)
+        return self.service.actions_for("data").picking(picking)
 
     def _move_line_data(self, move_line):
         return self.service.actions_for("data").move_line(move_line)
