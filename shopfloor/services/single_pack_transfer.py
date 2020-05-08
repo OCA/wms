3a41d9a577a6f4d7788d993095fd4a29a1f2d076
Simone Orsi <simahawk@gmail.com>
2020-05-08 10:20:36 +0200

    backend: fix non-process services api-docs
    Some headers are required only for specific services.
    Use a specific base component + flags to handle this properly

--- shopfloor/services/single_pack_transfer.py
+++ shopfloor/services/single_pack_transfer.py
@@ -5,7 +5,7 @@ from odoo.addons.component.core import Component
 class SinglePackTransfer(Component):
     """Methods for the Single Pack Transfer Process"""
 
-    _inherit = "base.shopfloor.service"
+    _inherit = "base.shopfloor.process"
     _name = "shopfloor.single.pack.transfer"
     _usage = "single_pack_transfer"
     _description = __doc__
