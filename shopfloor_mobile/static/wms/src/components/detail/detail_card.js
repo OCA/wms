ae09178ecf879d8708d52cff8b613f6feb6dd1de
Simone Orsi <simahawk@gmail.com>
2020-05-13 08:14:09 +0200

    frontend: improve detail card
    * allow key_title to be a path
    * use field.klass to customize field-detail class

--- shopfloor_mobile/static/wms/src/components/detail/detail_card.js
+++ shopfloor_mobile/static/wms/src/components/detail/detail_card.js
@@ -7,7 +7,7 @@ Vue.component("item-detail-card", {
         <v-card outlined :class="opts.main ? 'main' : ''" v-if="!_.isEmpty(record)">
             <v-card-title v-if="!opts.no_title">
                 <slot name="title">
-                    <span v-text="record[opts.key_title]" />
+                    <span v-text="_.result(record, opts.key_title)" />
                 </slot>
             </v-card-title>
             <v-card-subtitle v-if="$slots.subtitle">
@@ -16,7 +16,7 @@ Vue.component("item-detail-card", {
             <slot name="details">
                 <!-- TODO: this loop is the same in list-item => make it a component -->
                 <v-card-text class="details" v-if="opts.fields.length">
-                    <div v-for="(field, index) in opts.fields" :class="'field-detail ' + field.path.replace('.', '-')" >
+                    <div v-for="(field, index) in opts.fields" :class="'field-detail ' + field.path.replace('.', '-') + ' ' + (field.klass || '')">
                         <div v-if="raw_value(record, field) || field.display_no_value">
                             <span v-if="field.label" class="label">{{ field.label }}:</span> {{ render_field_value(record, field) }}
                             <v-btn icon class="detail-action"
