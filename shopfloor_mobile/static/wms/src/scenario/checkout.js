36f8205f44cfee8e322e628a6c414361dcbcf32e
Simone Orsi <simahawk@gmail.com>
2020-05-13 07:50:07 +0200

    frontend: move utils out of checkout.js

--- shopfloor_mobile/static/wms/src/scenario/checkout.js
+++ shopfloor_mobile/static/wms/src/scenario/checkout.js
@@ -1,5 +1,6 @@
 import {ScenarioBaseMixin} from "./mixins.js";
 import {process_registry} from "../services/process_registry.js";
+import {utils} from "../utils.js";
 import {demotools} from "../demo/demo.core.js"; // FIXME: dev only
 import {} from "../demo/demo.checkout.js"; // FIXME: dev only
 
@@ -15,13 +16,6 @@ export var Checkout = Vue.component("checkout", {
         you assume to have different instance but indeed you get only 1
         which is reused every time!
     */
-    /*
-    TODO QUESTIONS:
-
-    * summary: raw products not listed -> no move lines from backend
-    * destroy raw product: what to do? Just drop the flag?
-    *
-     */
     template: `
         <Screen :title="screen_info.title" :klass="screen_info.klass">
             <!-- FOR DEBUG -->
@@ -65,7 +59,7 @@ export var Checkout = Vue.component("checkout", {
                 <detail-picking-select
                     :picking="state.data.picking"
                     :select_records="state.data.picking.move_lines"
-                    :select_records_grouped="group_lines_by_location(state.data.picking.move_lines, {'prepare_records': only_one_package})"
+                    :select_records_grouped="utils.group_lines_by_location(state.data.picking.move_lines, {'prepare_records': utils.only_one_package})"
                     :key="current_state_key + '-detail-picking-select'"
                     />
                 <div class="button-list button-vertical-list full">
@@ -115,7 +109,7 @@ export var Checkout = Vue.component("checkout", {
             <div v-if="state_is('summary')">
                 <checkout-summary-detail
                     :picking="state.data.picking"
-                    :records_grouped="group_lines_by_location(state.data.picking.move_lines, {'group_key': 'location_dest', 'prepare_records': group_by_pack})"
+                    :records_grouped="utils.group_lines_by_location(state.data.picking.move_lines, {'group_key': 'location_dest', 'prepare_records': utils.group_by_pack})"
                     :key="current_state_key + '-checkout-summary-detail'"
                     />
                 <div class="button-list button-vertical-list full">
@@ -197,6 +191,9 @@ export var Checkout = Vue.component("checkout", {
         </Screen>
         `,
     computed: {
+        utils: function() {
+            return utils;
+        },
         // TODO: move these to methods
         manual_select_picking_fields: function() {
             return [
@@ -227,92 +224,6 @@ export var Checkout = Vue.component("checkout", {
             // TODO: double check when the process is done if this is still needed or not.
             // `manual-select` can now buble up events w/ full record.
             return _.first(_.filter(records, {id: _id}));
-        },
-        group_lines_by_location: function(lines, options) {
-            // {'key': 'no-group', 'title': '', 'records': []}
-            options = _.defaults(options || {}, {
-                group_key: "location_src",
-                name_prefix: "Location",
-                prepare_records: function(recs) {
-                    return recs;
-                },
-            });
-            const res = [];
-            const locations = _.uniqBy(
-                _.map(lines, function(x) {
-                    return x[options.group_key];
-                }),
-                "id"
-            );
-            const grouped = _.groupBy(lines, options.group_key + ".id");
-            _.forEach(grouped, function(value, loc_id) {
-                const location = _.first(_.filter(locations, {id: parseInt(loc_id)}));
-                const title = options.name_prefix
-                    ? options.name_prefix + ": " + location.name
-                    : location.name;
-                res.push({
-                    key: loc_id,
-                    title: title,
-                    records: options.prepare_records(value),
-                });
-            });
-            return res;
-        },
-        group_by_pack: function(lines) {
-            const self = this;
-            const res = [];
-            const packs = _.uniqBy(
-                _.map(lines, function(x) {
-                    return x.package_dest;
-                }),
-                "id"
-            );
-            const grouped = _.groupBy(lines, "package_dest.id");
-            let counter = 0;
-            _.forEach(grouped, function(products, pack_id) {
-                counter++;
-                const pack = _.first(_.filter(packs, {id: parseInt(pack_id)}));
-                res.push({
-                    key: pack ? pack_id : products[0].id + "-" + counter,
-                    // No pack, just display the product name
-                    title: pack ? pack.name : products[0].display_name,
-                    pack: pack,
-                    records: products,
-                    records_by_pkg_type: pack
-                        ? self.group_by_package_type(products)
-                        : null,
-                });
-            });
-            return res;
-        },
-        group_by_package_type: function(lines) {
-            const res = [];
-            const grouped = _.groupBy(lines, "package_dest.packaging_name");
-            _.forEach(grouped, function(products, packaging_name) {
-                res.push({
-                    key: packaging_name,
-                    title: packaging_name,
-                    records: products,
-                });
-            });
-            return res;
-        },
-        only_one_package: function(lines) {
-            let res = [];
-            let pkg_seen = [];
-            lines.forEach(function(line) {
-                if (line.package_dest) {
-                    if (!pkg_seen.includes(line.package_dest.id)) {
-                        // got a pack
-                        res.push(line);
-                        pkg_seen.push(line.package_dest.id);
-                    }
-                } else {
-                    // no pack
-                    res.push(line);
-                }
-            });
-            return res;
         },
     },
     data: function() {
