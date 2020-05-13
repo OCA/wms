6634cd46fb31bdfa91103dcc4bff14fdf1ab02fa
Simone Orsi <simahawk@gmail.com>
2020-05-13 08:15:29 +0200

    frontend: adapt scan_anything demo data to backend changes

--- shopfloor_mobile/static/wms/src/demo/demo.scan_anything.js
+++ shopfloor_mobile/static/wms/src/demo/demo.scan_anything.js
@@ -1,10 +1,26 @@
 import {demotools} from "./demo.core.js";
 
-const DEMO_SCAN_ANYTHING_PACK = {
+const DEMO_SCAN_ANYTHING_PACK1 = {
     data: {
-        type: "pack",
-        barcode: "akdlsjf",
-        detail_info: demotools.makePack({}, {full_detail: true}),
+        type: "package",
+        barcode: "pack",
+        record: demotools.makePackFullDetail({}, {}),
+    },
+};
+
+const DEMO_SCAN_ANYTHING_PACK2 = {
+    data: {
+        type: "package",
+        barcode: "pack2",
+        record: demotools.makePackFullDetail({}, {}),
+    },
+};
+
+const DEMO_SCAN_ANYTHING_PACK3 = {
+    data: {
+        type: "package",
+        barcode: "pack3",
+        record: demotools.makePackFullDetail({}, {}),
     },
 };
 
@@ -12,21 +28,21 @@ const DEMO_SCAN_ANYTHING_PRODUCT = {
     data: {
         type: "product",
         barcode: "009932424",
-        detail_info: demotools.makeProductFullDetail(),
+        record: demotools.makeProductFullDetail(),
     },
 };
 const DEMO_SCAN_ANYTHING_PRODUCT_2 = {
     data: {
         type: "product",
         barcode: "prod2",
-        detail_info: demotools.makeProductFullDetail(),
+        record: demotools.makeProductFullDetail(),
     },
 };
 const DEMO_SCAN_ANYTHING_LOCATION_1 = {
     data: {
         type: "location",
         barcode: "loc1",
-        detail_info: demotools.makeLocation({}, {full_detail: true}),
+        record: demotools.makeLocation({}, {full_detail: true}),
     },
 };
 
@@ -34,23 +50,67 @@ const DEMO_SCAN_ANYTHING_LOCATION_2 = {
     data: {
         type: "location",
         barcode: "283429834",
-        detail_info: demotools.makeLocation({}, {full_detail: true}),
+        record: demotools.makeLocation({}, {full_detail: true}),
     },
 };
 
-const DEMO_SCAN_ANYTHING_OPERATION = {
+const DEMO_SCAN_ANYTHING_TRANSFER = {
     data: {
-        type: "operation",
-        barcode: "280009834",
-        detail_info: demotools.makePicking({}, {full_detail: true}),
+        type: "transfer",
+        barcode: "transf",
+        record: demotools.makePicking({}, {full_detail: true}),
+    },
+};
+const DEMO_SCAN_ANYTHING_TRANSFER1 = {
+    data: {
+        type: "transfer",
+        barcode: "transf1",
+        record: demotools.makePicking({}, {full_detail: true}),
+    },
+};
+const DEMO_SCAN_ANYTHING_TRANSFER2 = {
+    data: {
+        type: "transfer",
+        barcode: "transf2",
+        record: demotools.makePicking({}, {full_detail: true}),
+    },
+};
+
+const DEMO_SCAN_ANYTHING_LOT = {
+    data: {
+        type: "lot",
+        barcode: "lot",
+        record: demotools.makeLotFullDetail(),
+    },
+};
+
+const DEMO_SCAN_ANYTHING_LOT1 = {
+    data: {
+        type: "lot",
+        barcode: "lot1",
+        record: demotools.makeLotFullDetail(),
+    },
+};
+const DEMO_SCAN_ANYTHING_LOT2 = {
+    data: {
+        type: "lot",
+        barcode: "lot2",
+        record: demotools.makeLotFullDetail(),
     },
 };
 
 demotools.add_case("scan_anything", {
-    pack: DEMO_SCAN_ANYTHING_PACK,
+    pack: DEMO_SCAN_ANYTHING_PACK1,
+    pack2: DEMO_SCAN_ANYTHING_PACK2,
+    pack3: DEMO_SCAN_ANYTHING_PACK3,
     prod: DEMO_SCAN_ANYTHING_PRODUCT,
     prod2: DEMO_SCAN_ANYTHING_PRODUCT_2,
-    loc1: DEMO_SCAN_ANYTHING_LOCATION_1,
+    loc: DEMO_SCAN_ANYTHING_LOCATION_1,
     loc2: DEMO_SCAN_ANYTHING_LOCATION_2,
-    op: DEMO_SCAN_ANYTHING_OPERATION,
+    tr: DEMO_SCAN_ANYTHING_TRANSFER,
+    tr1: DEMO_SCAN_ANYTHING_TRANSFER1,
+    tr2: DEMO_SCAN_ANYTHING_TRANSFER2,
+    lot: DEMO_SCAN_ANYTHING_LOT,
+    lot1: DEMO_SCAN_ANYTHING_LOT1,
+    lot2: DEMO_SCAN_ANYTHING_LOT2,
 });
