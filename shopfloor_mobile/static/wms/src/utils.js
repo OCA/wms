bfd61945401bd789137480299dccfb431be56a7b
Simone Orsi <simahawk@gmail.com>
2020-05-13 08:10:53 +0200

    frontend: detail mixin provide date renderer

--- shopfloor_mobile/static/wms/src/utils.js
+++ shopfloor_mobile/static/wms/src/utils.js
@@ -124,6 +124,23 @@ export class Utils {
         });
         return res;
     }
+
+    format_date_display(date_string, options = {}) {
+        _.defaults(options, {
+            locale: navigator ? navigator.language : "en-US",
+            format: {
+                day: "numeric",
+                month: "short",
+                year: "numeric",
+                hour: "numeric",
+                minute: "2-digit",
+            },
+        });
+        return new Date(Date.parse(date_string)).toLocaleString(
+            options.locale,
+            options.format
+        );
+    }
 }
 
 export const utils = new Utils();
