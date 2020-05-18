c4a2068eeaf14b52f3e84fb964257d5e7e439aaa
Simone Orsi <simahawk@gmail.com>
2020-05-18 15:31:22 +0200

    frontend: demo.delivery improve data

--- shopfloor_mobile/static/wms/src/demo/demo.delivery.js
+++ shopfloor_mobile/static/wms/src/demo/demo.delivery.js
@@ -4,21 +4,30 @@ const deliver_data = {
     picking: demotools.makePicking({}, {lines_count: 5, line_random_pack: true}),
 };
 
-const move_lines = demotools.makePickingLines(
-    {},
-    {lines_count: 5, line_random_pack: true}
-);
-// set at least one as done
-move_lines[0].qty_done = move_lines[0].quantity;
+let pickings = [];
+const count = 8;
 
-const picking1 = demotools.makePicking(
-    {
-        move_lines: move_lines,
-    },
-    {no_lines: true}
-);
+for (let i = 1; i < count + 1; i++) {
+    const move_lines = demotools.makePickingLines(
+        {},
+        {lines_count: demotools.getRandomInt(5), line_random_pack: true}
+    );
+    for (let i = 0; i < demotools.getRandomInt(move_lines.length); i++) {
+        // set some as done
+        move_lines[i].qty_done = move_lines[i].quantity;
+    }
+    pickings.push(
+        demotools.makePicking(
+            {
+                move_lines_count: move_lines.length,
+                move_lines: move_lines,
+            },
+            {no_lines: true}
+        )
+    );
+}
 
-const manual_selection_pickings = _.sampleSize([picking1], _.random(1, 8));
+const manual_selection_pickings = _.sampleSize(pickings, _.random(1, 8));
 
 const DEMO_DELIVERY = {
     scan_deliver: {
