6573c58cb35f5754341bc7fdfc19c9c389f7ff41
Simone Orsi <simahawk@gmail.com>
2020-05-20 12:23:20 +0200

    frontend: cluster picking line actions close popup fix

--- shopfloor_mobile/static/wms/src/components/batch_picking_line_detail.js
+++ shopfloor_mobile/static/wms/src/components/batch_picking_line_detail.js
@@ -59,6 +59,12 @@ export var batch_picking_line_actions = Vue.component("batch-picking-line-action
             dialog: false,
         };
     },
+    methods: {
+        handle_action(action) {
+            this.$emit("action", action);
+            this.dialog = false;
+        },
+    },
     template: `
   <div class="batch-picking-line-actions">
     <v-dialog v-model="dialog" fullscreen tile class="actions fullscreen text-center">
@@ -75,23 +81,23 @@ export var batch_picking_line_actions = Vue.component("batch-picking-line-action
         <div class="button-list button-vertical-list full">
           <v-row align="center">
             <v-col class="text-center" cols="12">
-              <v-btn depressed x-large color="primary" @click="$emit('action', 'action_full_bin')">Go to destination - full bin(s)</v-btn>
+              <v-btn depressed x-large color="primary" @click="handle_action('action_full_bin')">Go to destination - full bin(s)</v-btn>
             </v-col>
           </v-row>
           <v-row align="center">
             <v-col class="text-center" cols="12">
-              <v-btn depressed x-large color="primary" @click="$emit('action', 'action_skip_line')">Skip line</v-btn>
+              <v-btn depressed x-large color="primary" @click="handle_action('action_skip_line')">Skip line</v-btn>
             </v-col>
           </v-row>
           <v-row align="center">
             <v-col class="text-center" cols="12">
               <v-btn depressed x-large color="primary"
-                  @click="$emit('action', 'action_stock_out')">Declare stock out</v-btn>
+                  @click="handle_action('action_stock_out')">Declare stock out</v-btn>
             </v-col>
           </v-row>
           <v-row align="center">
             <v-col class="text-center" cols="12">
-              <v-btn depressed x-large color="primary" @click="$emit('action', 'action_change_pack_or_lot')">Change lot or pack</v-btn>
+              <v-btn depressed x-large color="primary" @click="handle_action('action_change_pack_or_lot')">Change lot or pack</v-btn>
             </v-col>
           </v-row>
           <v-row align="center">
@@ -110,18 +116,23 @@ export var batch_picking_line_stock_out = Vue.component(
     "batch-picking-line-stock-out",
     {
         props: ["line"],
+        methods: {
+            handle_action(action) {
+                this.$emit("action", action);
+            },
+        },
         template: `
     <div class="batch-picking-line-stock-out">
       <batch-picking-line-detail :line="line" :showFullInfo="false" />
       <div class="button-list button-vertical-list full">
         <v-row align="center">
           <v-col class="text-center" cols="12">
-            <v-btn depressed x-large color="primary" @click="$emit('action', 'confirm_stock_issue')">Confirm stock = 0</v-btn>
+            <v-btn depressed x-large color="primary" @click="handle_action('confirm_stock_issue')">Confirm stock = 0</v-btn>
           </v-col>
         </v-row>
         <v-row align="center">
           <v-col class="text-center" cols="12">
-            <v-btn depressed x-large color="default" @click="$emit('action', 'back')">back</v-btn>
+            <v-btn depressed x-large color="default" @click="handle_action('back')">back</v-btn>
           </v-col>
         </v-row>
       </div>
