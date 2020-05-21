e296d0caa23d2475a10024a551822188bc8b39eb
Simone Orsi <simahawk@gmail.com>
2020-05-21 12:24:38 +0200

    frontend: add color registry to unify handling of colors

--- shopfloor_mobile/static/wms/src/services/registry.js
+++ shopfloor_mobile/static/wms/src/services/registry.js
@@ -19,3 +19,28 @@ export class Registry {
         return _.template("/${ code }/:menu_id/:state?")({code: code});
     }
 }
+
+export class ColorRegistry {
+    constructor(theme, _default = "default") {
+        this.themes = [];
+        this.colors = {};
+        this.default_theme = _default;
+    }
+
+    add_theme(colors, theme) {
+        if (_.isUndefined(theme)) theme = this.default_theme;
+        if (!this.themes.includes(theme)) {
+            this.themes.push(theme);
+        }
+        this.colors[theme] = colors;
+    }
+
+    color_for(key, theme) {
+        if (_.isUndefined(theme)) theme = this.default_theme;
+        if (!this.themes.includes(theme)) {
+            console.log("Theme", theme, "not registeed.");
+            return null;
+        }
+        return this.colors[theme][key];
+    }
+}
