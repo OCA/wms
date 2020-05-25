94e118a9761fa951e568748f3196b54e5f8e7635
Simone Orsi <simahawk@gmail.com>
2020-05-25 14:49:00 +0200

    frontend: centralize color for info icon

--- shopfloor_mobile/static/wms/src/services/color_registry.js
+++ shopfloor_mobile/static/wms/src/services/color_registry.js
@@ -5,4 +5,5 @@ export var color_registry = new ColorRegistry();
 color_registry.add_theme({
     screen_step_done: "green accent-4",
     screen_step_todo: "amber",
+    info_icon: "blue lighten-1",
 });
