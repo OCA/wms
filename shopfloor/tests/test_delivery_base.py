36d1b2776d2323ed78bbd08b6b443030aa744da3
sebalix <seb@usr-src.org>
2020-06-01 14:54:36 +0200

    delivery: implement 'list_stock_picking' endpoint

--- shopfloor/tests/test_delivery_base.py
+++ shopfloor/tests/test_delivery_base.py
@@ -69,7 +69,7 @@ class DeliveryCommonCase(CommonCase):
             self.service = work.component(usage="delivery")
 
     def _stock_picking_data(self, picking):
-        return self.service._data_for_stock_picking(picking)
+        return self.service.data_struct.picking_detail(picking)
 
     def assert_response_deliver(self, response, picking=None, message=None):
         self.assert_response(
