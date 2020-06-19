b205b6cb13be11e2e3b48efe487b63722659007c
Guewen Baconnier <guewen.baconnier@camptocamp.com>
2020-06-19 14:59:46 +0200

    location transfer: implement /scan_package

---
+++ shopfloor/tests/test_location_content_transfer_single.py
@@ -0,0 +1,203 @@
+from .test_location_content_transfer_base import LocationContentTransferCommonCase
+
+
+class LocationContentTransferSingleCase(LocationContentTransferCommonCase):
+    """Tests for endpoint used from state start_single
+
+    * /scan_package
+    * /scan_line
+
+    """
+
+    # TODO common with set_destination_all?
+    @classmethod
+    def setUpClassBaseData(cls):
+        super().setUpClassBaseData()
+        products = cls.product_a + cls.product_b + cls.product_c + cls.product_d
+        for product in products:
+            cls.env["stock.putaway.rule"].sudo().create(
+                {
+                    "product_id": product.id,
+                    "location_in_id": cls.stock_location.id,
+                    "location_out_id": cls.shelf1.id,
+                }
+            )
+
+        cls.product_d.tracking = "lot"
+        cls.picking1 = picking1 = cls._create_picking(
+            lines=[(cls.product_a, 10), (cls.product_b, 10)]
+        )
+        cls.picking2 = picking2 = cls._create_picking(
+            lines=[(cls.product_c, 10), (cls.product_d, 10)]
+        )
+        cls.pickings = picking1 | picking2
+        cls._fill_stock_for_moves(
+            picking1.move_lines, in_package=True, location=cls.content_loc
+        )
+        cls.product_d_lot = cls.env["stock.production.lot"].create(
+            {"product_id": cls.product_d.id, "company_id": cls.env.company.id}
+        )
+        cls._fill_stock_for_moves(picking2.move_lines[0], location=cls.content_loc)
+        cls._fill_stock_for_moves(
+            picking2.move_lines[1], location=cls.content_loc, in_lot=cls.product_d_lot
+        )
+        cls.pickings.action_assign()
+        cls._simulate_pickings_selected(cls.pickings)
+
+    def _test_scan_package_ok(self, barcode):
+        package_level = self.picking1.move_line_ids.package_level_id
+        response = self.service.dispatch(
+            "scan_package",
+            params={
+                "location_id": self.content_loc.id,
+                "package_level_id": package_level.id,
+                "barcode": barcode,
+            },
+        )
+        self.assert_response_scan_destination(response, package_level)
+
+    def test_scan_package_package_ok(self):
+        package_level = self.picking1.move_line_ids.package_level_id
+        self._test_scan_package_ok(package_level.package_id.name)
+
+    def test_scan_package_barcode_not_found(self):
+        package_level = self.picking1.move_line_ids.package_level_id
+        response = self.service.dispatch(
+            "scan_package",
+            params={
+                "location_id": self.content_loc.id,
+                "package_level_id": package_level.id,
+                "barcode": "NOT_FOUND",
+            },
+        )
+        self.assert_response_start_single(
+            response, self.pickings, message=self.service.msg_store.barcode_not_found()
+        )
+
+    def test_scan_package_product_ok(self):
+        # product_a is in the package and anywhere else so it's
+        # accepted to check we scanned the correct package
+        self._test_scan_package_ok(self.product_a.barcode)
+
+    def test_scan_package_product_packaging_ok(self):
+        # product_a is in the package and anywhere else so it's
+        # accepted to check we scanned the correct package
+        self._test_scan_package_ok(self.product_a.packaging_ids[0].barcode)
+
+    def test_scan_package_lot_ok(self):
+        package_level = self.picking1.move_line_ids.package_level_id
+        line_product_a = package_level.move_line_ids[0]
+        self.product_a.tracking = "lot"
+        line_product_a.lot_id = self.env["stock.production.lot"].create(
+            {"product_id": self.product_a.id, "company_id": self.env.company.id}
+        )
+        # lot of product_a is in the package and anywhere else so it's
+        # accepted to check we scanned the correct package
+        self._test_scan_package_ok(line_product_a.lot_id.name)
+
+    def _test_scan_package_nok(self, pickings, barcode, message):
+        package_level = self.picking1.move_line_ids.package_level_id
+        response = self.service.dispatch(
+            "scan_package",
+            params={
+                "location_id": self.content_loc.id,
+                "package_level_id": package_level.id,
+                "barcode": self.product_a.barcode,
+            },
+        )
+        self.assert_response_start_single(response, pickings, message=message)
+
+    def test_scan_package_product_nok_different_package(self):
+        # add another picking with a package with product a,
+        # if we scan product A, we can't know for which package it is
+        picking = self._create_picking(lines=[(self.product_a, 10)])
+        self._fill_stock_for_moves(
+            picking.move_lines, in_package=True, location=self.content_loc
+        )
+        picking.action_assign()
+        self._simulate_pickings_selected(picking)
+        self._test_scan_package_nok(
+            self.pickings | picking,
+            self.product_a.barcode,
+            {"message_type": "error", "body": "Scan the package"},
+        )
+
+    def test_scan_package_product_nok_different_line(self):
+        # add another picking with a raw line with product a,
+        # if we scan product A, we can't know which line/package we want
+        picking = self._create_picking(lines=[(self.product_a, 10)])
+        self._fill_stock_for_moves(picking.move_lines, location=self.content_loc)
+        picking.action_assign()
+        self._simulate_pickings_selected(picking)
+        self._test_scan_package_nok(
+            self.pickings | picking,
+            self.product_a.barcode,
+            {"message_type": "error", "body": "Scan the package"},
+        )
+
+    def test_scan_package_product_nok_product_tracked(self):
+        # we scan product_a's barcode but it's tracked by lot
+        self.product_a.tracking = "lot"
+        self._test_scan_package_nok(
+            self.pickings,
+            self.product_a.barcode,
+            {"message_type": "error", "body": "Scan the package"},
+        )
+
+    def test_scan_package_lot_nok_different_package(self):
+        # add another picking with a package with the lot used in our package,
+        # if we scan the lot, we can't know for which package it is
+        package_level = self.picking1.move_line_ids.package_level_id
+        line_product_a = package_level.move_line_ids[0]
+        self.product_a.tracking = "lot"
+        line_product_a.lot_id = lot = self.env["stock.production.lot"].create(
+            {"product_id": self.product_a.id, "company_id": self.env.company.id}
+        )
+        picking = self._create_picking(lines=[(self.product_a, 10)])
+        self._fill_stock_for_moves(
+            picking.move_lines, in_package=True, in_lot=lot, location=self.content_loc
+        )
+        picking.action_assign()
+        self._simulate_pickings_selected(picking)
+        self._test_scan_package_nok(
+            self.pickings | picking,
+            self.product_a.barcode,
+            {"message_type": "error", "body": "Scan the package"},
+        )
+
+    def test_scan_package_lot_nok_different_line(self):
+        # add another picking with a raw line with a lot used in our package,
+        # if we scan the lot, we can't know which line/package we want
+        package_level = self.picking1.move_line_ids.package_level_id
+        line_product_a = package_level.move_line_ids[0]
+        self.product_a.tracking = "lot"
+        line_product_a.lot_id = lot = self.env["stock.production.lot"].create(
+            {"product_id": self.product_a.id, "company_id": self.env.company.id}
+        )
+        picking = self._create_picking(lines=[(self.product_a, 10)])
+        self._fill_stock_for_moves(
+            picking.move_lines, in_lot=lot, location=self.content_loc
+        )
+        picking.action_assign()
+        self._simulate_pickings_selected(picking)
+        self._test_scan_package_nok(
+            self.pickings | picking,
+            self.product_a.barcode,
+            {"message_type": "error", "body": "Scan the package"},
+        )
+
+    def test_scan_package_package_level_not_exists(self):
+        package_level = self.picking1.move_line_ids.package_level_id
+        package_level_id = package_level.id
+        package_level.unlink()
+        response = self.service.dispatch(
+            "scan_package",
+            params={
+                "location_id": self.content_loc.id,
+                "package_level_id": package_level_id,
+                "barcode": self.product_a.barcode,
+            },
+        )
+        self.assert_response_start_single(
+            response, self.pickings, message=self.service.msg_store.record_not_found()
+        )
