72690ff2b4966e4c21f9f52fa9467e3f8dbe199e
Guewen Baconnier <guewen.baconnier@camptocamp.com>
2020-06-24 16:00:41 +0200

    zone picking: add skeleton of the service

---
+++ shopfloor/tests/test_zone_picking_base.py
@@ -0,0 +1,20 @@
+from .common import CommonCase
+
+
+class ZonePickingCommonCase(CommonCase):
+    @classmethod
+    def setUpClassVars(cls, *args, **kwargs):
+        super().setUpClassVars(*args, **kwargs)
+        cls.menu = cls.env.ref("shopfloor.shopfloor_menu_zone_picking")
+        cls.profile = cls.env.ref("shopfloor.shopfloor_profile_shelf_1_demo")
+        cls.wh = cls.profile.warehouse_id
+        cls.picking_type = cls.menu.picking_type_ids
+
+    @classmethod
+    def setUpClassBaseData(cls, *args, **kwargs):
+        super().setUpClassBaseData(*args, **kwargs)
+
+    def setUp(self):
+        super().setUp()
+        with self.work_on_services(menu=self.menu, profile=self.profile) as work:
+            self.service = work.component(usage="zone_picking")
