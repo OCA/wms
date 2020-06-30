25de86ad28ee65fc78d4bc8e0be44e2fb26dc963
Simone Orsi <simahawk@gmail.com>
2020-06-30 08:32:21 +0200

    frontend: add reload btn to home for demo mode

--- shopfloor_mobile/static/wms/src/homepage.js
+++ shopfloor_mobile/static/wms/src/homepage.js
@@ -4,6 +4,17 @@ export var HomePage = Vue.component("home-page", {
             return this.$root.appmenu.menus;
         },
     },
+    methods: {
+        nuke_data_and_reload: function() {
+            // Force reload config (loads profiles)
+            this.$root.loadConfig(true);
+            // Enforce 1st profile
+            const selected_profile = _.head(this.$root.appconfig.profiles);
+            this.$root.trigger("profile:selected", selected_profile, true);
+            // Reload page
+            location.reload();
+        },
+    },
     props: ["routes"],
     template: `
         <Screen
@@ -16,6 +27,13 @@ export var HomePage = Vue.component("home-page", {
             </v-list>
 
             <v-alert tile class="mt10" color="warning" v-if="$root.demo_mode">DEMO MODE ON</v-alert>
+            <div class="button-list button-vertical-list full" v-if="$root.demo_mode">
+                <v-row align="center">
+                    <v-col class="text-center" cols="12">
+                        <btn-action action="cancel" @click="nuke_data_and_reload()">Force reload data and refresh</btn-action>
+                    </v-col>
+                </v-row>
+            </div>
         </Screen>
     `,
 });
