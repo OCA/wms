74342ea3735d87a43c2a42126d0c3b3a503b668a
Guewen Baconnier <guewen.baconnier@camptocamp.com>
2020-07-07 09:26:45 +0200

    Add option to disable zero check in scenario
    Currently, it applies only for Cluster Picking, it will apply
    on Zone Picking and Discrete Order Picking too.
    
    When a location becomes empty and the option is active, the user has
    to validate that the location is really empty. It doesn't really make
    sense for picking types that move pallets, so they would disable it
    there.
    
    There is an ongoing discussion whether this option should be on the
    picking type or on the shopfloor menu, it is possible that this option
    will be moved.

--- shopfloor/models/stock_picking_type.py
+++ shopfloor/models/stock_picking_type.py
@@ -7,3 +7,9 @@ class StockPickingType(models.Model):
     shopfloor_menu_ids = fields.Many2many(
         comodel_name="shopfloor.menu", string="Shopfloor Menus", readonly=True,
     )
+    shopfloor_zero_check = fields.Boolean(
+        string="Activate Zero Check",
+        help="For Shopfloor scenarios using it (Cluster Picking, Zone Picking,"
+        " Discrete order Picking), the zero check step will be activated when"
+        " a location becomes empty after a move.",
+    )
