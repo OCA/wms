e9bc51f2179b83ae132b2be765ffbdae3a3d59cd
sebalix <seb@usr-src.org>
2020-07-27 13:17:22 +0200

    zone picking: implement /stock_issue

--- shopfloor/tests/__init__.py
+++ shopfloor/tests/__init__.py
@@ -44,3 +44,4 @@ from . import test_zone_picking_select_picking_type
 from . import test_zone_picking_select_line
 from . import test_zone_picking_set_line_destination
 from . import test_zone_picking_zero_check
+from . import test_zone_picking_stock_issue
