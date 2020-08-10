ac294f4640357532238e12b50b1f8d92059a4a6f
sebalix <seb@usr-src.org>
2020-08-10 11:20:43 +0200

    checkout: fix packaging field to use

--- shopfloor/tests/test_actions_data.py
+++ shopfloor/tests/test_actions_data.py
@@ -142,7 +142,7 @@ class ActionsDataCase(ActionsDataCaseBase):
 
     def test_data_package(self):
         package = self.move_a.move_line_ids.package_id
-        package.product_packaging_id = self.packaging.id
+        package.packaging_id = self.packaging.id
         package.package_storage_type_id = self.storage_type_pallet
         data = self.data.package(package, picking=self.picking, with_packaging=True)
         self.assert_schema(self.schema.package(with_packaging=True), data)
@@ -150,7 +150,7 @@ class ActionsDataCase(ActionsDataCaseBase):
             "id": package.id,
             "name": package.name,
             "move_line_count": 1,
-            "packaging": self._expected_packaging(package.product_packaging_id),
+            "packaging": self._expected_packaging(package.packaging_id),
             "storage_type": self._expected_storage_type(
                 package.package_storage_type_id
             ),
@@ -222,7 +222,7 @@ class ActionsDataCase(ActionsDataCaseBase):
     def test_data_move_line_package(self):
         move_line = self.move_a.move_line_ids
         result_package = self.env["stock.quant.package"].create(
-            {"product_packaging_id": self.packaging.id}
+            {"packaging_id": self.packaging.id}
         )
         move_line.write({"qty_done": 3.0, "result_package_id": result_package.id})
         data = self.data.move_line(move_line)
