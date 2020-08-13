55c94f082c64d27540b31f813da560f7b8472665
Simone Orsi <simahawk@gmail.com>
2020-08-13 09:44:10 +0200

    front: single_pack_transfer remove confirm states

--- shopfloor_mobile/static/wms/src/scenario/single_pack_transfer.js
+++ shopfloor_mobile/static/wms/src/scenario/single_pack_transfer.js
@@ -5,23 +5,8 @@ export var SinglePackStatesMixin = {
     data: function() {
         return {
             states: {
-                // Generic state for when to start w/ scanning a pack
-                start_scan_pack: {
-                    display_info: {
-                        title: "Start by scanning a pack",
-                        scan_placeholder: "Scan pack",
-                    },
-                    enter: () => {
-                        this.state_reset_data();
-                    },
-                    on_scan: scanned => {
-                        this.wait_call(
-                            this.odoo.call("start", {barcode: scanned.text})
-                        );
-                    },
-                },
                 // Generic state for when to start w/ scanning a pack or loc
-                start_scan_pack_or_location: {
+                start: {
                     display_info: {
                         title: "Start by scanning a pack or a location",
                         scan_placeholder: "Scan pack",
@@ -30,13 +15,15 @@ export var SinglePackStatesMixin = {
                         this.state_reset_data();
                     },
                     on_scan: scanned => {
+                        const data = this.state.data;
                         this.wait_call(
-                            this.odoo.call("start", {barcode: scanned.text})
+                            this.odoo.call("start", {
+                                barcode: scanned.text,
+                                confirmation: data.confirmation_required,
+                            })
                         );
                     },
                 },
-                // TODO: these states should be splitted out to a specific mixin
-                // for putaway and pack transfer
                 scan_location: {
                     display_info: {
                         title: "Set a location",
@@ -61,68 +48,6 @@ export var SinglePackStatesMixin = {
                         );
                     },
                 },
-                confirm_location: {
-                    display_info: {
-                        scan_placeholder: "Scan location",
-                    },
-                    enter: () => {
-                        this.need_confirmation = true;
-                    },
-                    exit: () => {
-                        this.need_confirmation = false;
-                    },
-                    on_user_confirm: answer => {
-                        if (answer == "yes") {
-                            // Reuse data from scan_location and
-                            // simulate the event that on_scan expects
-                            const scan_data = this.state_get_data("scan_location");
-                            this.state.on_scan(
-                                {
-                                    text: scan_data.location_barcode,
-                                },
-                                true
-                            );
-                        } else {
-                            this.state_to("scan_location");
-                        }
-                    },
-                    on_scan: (scanned, confirmation = true) => {
-                        this.on_state_exit();
-                        // FIXME: use state_load
-                        this.current_state_key = "scan_location";
-                        this.state.on_scan(scanned, confirmation);
-                    },
-                },
-                confirm_start: {
-                    display_info: {
-                        title: "Confirm start and select a location",
-                        scan_placeholder: "Scan location",
-                    },
-                    enter: () => {
-                        this.need_confirmation = true;
-                    },
-                    exit: () => {
-                        this.need_confirmation = false;
-                    },
-                    on_user_confirm: answer => {
-                        if (answer == "yes") {
-                            // Keep the data received from previous state but not the question answered
-                            const state_data = this.state_get_data(
-                                this.current_state_key
-                            );
-                            state_data.message = {};
-                            this.state_set_data(state_data, "scan_location");
-                            this.state_to("scan_location");
-                        } else {
-                            this.state_to("start");
-                        }
-                    },
-                    on_scan: scanned => {
-                        this.on_state_exit();
-                        this.current_state_key = "scan_location";
-                        this.state.on_scan(scanned);
-                    },
-                },
             },
         };
     },
@@ -137,8 +62,7 @@ export var SinglePackTransfer = Vue.component("single-pack-transfer", {
             </template>
             <searchbar v-if="state_is(initial_state_key)" v-on:found="on_scan" :input_placeholder="search_input_placeholder"></searchbar>
             <searchbar v-if="state_is('scan_location')" v-on:found="on_scan" :input_placeholder="search_input_placeholder" :input_data_type="'location'"></searchbar>
-            <user-confirmation v-if="need_confirmation" v-on:user-confirmation="on_user_confirm" v-bind:question="user_notification.message"></user-confirmation>
-            <detail-operation v-if="state.key != initial_state_key && state.key != 'show_completion_info'" :record="state.data" />
+            <detail-operation v-if="state.key != 'show_completion_info' && _.result(state, 'data.picking')" :record="state.data" />
             <last-operation v-if="state_is('show_completion_info')" v-on:confirm="state.on_confirm"></last-operation>
             <cancel-button v-on:cancel="on_cancel" v-if="show_cancel_button"></cancel-button>
         </Screen>
@@ -147,7 +71,7 @@ export var SinglePackTransfer = Vue.component("single-pack-transfer", {
         return {
             usage: "single_pack_transfer",
             show_reset_button: true,
-            initial_state_key: "start_scan_pack_or_location",
+            initial_state_key: "start",
             states: {
                 show_completion_info: {
                     on_confirm: () => {
