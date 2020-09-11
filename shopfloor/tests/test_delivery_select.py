27c420c2f218dea4d85a9da28a062f535470116a
Simone Orsi <simahawk@gmail.com>
2020-09-11 10:34:19 +0200

    bknd: fix delivery picking state check
    In general we can process only pickings that are ready (state = 'assigned').
    This is because even if the lines are partially available
    and the picking is of type 'immediate transfer'
    it will be available (in ready state) only when all the lines are available.

--- shopfloor/tests/test_delivery_select.py
+++ shopfloor/tests/test_delivery_select.py
@@ -16,6 +16,10 @@ class DeliverySelectCase(DeliveryCommonCase):
         cls.picking2 = cls._create_picking(
             lines=[(cls.product_a, 10), (cls.product_b, 10)]
         )
+        cls._fill_stock_for_moves(cls.picking1.move_lines)
+        cls._fill_stock_for_moves(cls.picking2.move_lines)
+        cls.pickings = cls.picking1 | cls.picking2
+        cls.pickings.action_assign()
 
     def test_select_ok(self):
         response = self.service.dispatch(
@@ -24,10 +28,9 @@ class DeliverySelectCase(DeliveryCommonCase):
         self.assert_response_deliver(response, picking=self.picking1)
 
     def test_select_not_found(self):
-        pickings = self.picking1 | self.picking2
         response = self.service.dispatch("select", params={"picking_id": -1})
         self.assert_response_manual_selection(
             response,
-            pickings=pickings,
+            pickings=self.pickings,
             message=self.service.msg_store.stock_picking_not_found(),
         )
