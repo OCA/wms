104c056dab7259264966e8a29b584997e04e097f
Guewen Baconnier <guewen.baconnier@camptocamp.com>
2020-09-11 12:05:04 +0200

    Ensure compatibility between release and dynamic routing
    Add a module with tests to check a release of moves with dynamic routing
    correctly sets the "printed" flag on all the chain.
    
    When we release moves, we set the "printed" flag on the released
    transfers, so we do not include any new moves (like a "wave" of
    release).
    
    As the dynamic routing is applied when we assign the moves, new moves
    and transfers may be created after the "assign". We therefore have
    to set the printed flag after it.

---
+++ stock_available_to_promise_release_dynamic_routing/__manifest__.py
@@ -0,0 +1,17 @@
+# Copyright 2020 Camptocamp (https://www.camptocamp.com)
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+{
+    "name": "Available to Promise Release - Dynamic Routing",
+    "summary": "Glue between moves release and dynamic routing",
+    "author": "Camptocamp, Odoo Community Association (OCA)",
+    "website": "https://github.com/OCA/wms",
+    "category": "Warehouse Management",
+    "version": "13.0.1.0.0",
+    "license": "AGPL-3",
+    "depends": ["stock_available_to_promise_release", "stock_dynamic_routing"],
+    "demo": [],
+    "data": [],
+    "auto_install": True,
+    "installable": True,
+    "development_status": "Alpha",
+}
---
+++ stock_available_to_promise_release_dynamic_routing/tests/__init__.py
@@ -0,0 +1,1 @@
+from . import test_release_dynamic_routing
