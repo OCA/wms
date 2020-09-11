c75a20b9c4021cce541b25fdae87ec4c196f24d3
Simone Orsi <simahawk@gmail.com>
2020-09-11 10:34:19 +0200

    bknd: fix delivery picking state check
    In general we can process only pickings that are ready (state = 'assigned').
    This is because even if the lines are partially available
    and the picking is of type 'immediate transfer'
    it will be available (in ready state) only when all the lines are available.

--- shopfloor/tests/test_delivery_list_stock_picking.py
+++ shopfloor/tests/test_delivery_list_stock_picking.py
@@ -17,9 +17,28 @@ class DeliveryListStockPickingCase(DeliveryCommonCase):
             lines=[(cls.product_a, 10), (cls.product_b, 10)]
         )
 
-    def test_list_stock_picking_ok(self):
-        pickings = self.picking1 | self.picking2
+    def test_list_stock_picking_ko(self):
+        """No picking is ready, no picking to list."""
         response = self.service.dispatch("list_stock_picking", params={})
         self.assert_response_manual_selection(
-            response, pickings=pickings,
+            response, pickings=[],
+        )
+
+    def test_list_stock_picking_ok(self):
+        """Picking ready to list."""
+        # prepare 1st picking
+        self._fill_stock_for_moves(self.picking1.move_lines)
+        self.picking1.action_assign()
+        response = self.service.dispatch("list_stock_picking", params={})
+        # picking1 only available
+        self.assert_response_manual_selection(
+            response, pickings=self.picking1,
+        )
+        # prepare 2nd picking
+        self._fill_stock_for_moves(self.picking2.move_lines)
+        self.picking2.action_assign()
+        response = self.service.dispatch("list_stock_picking", params={})
+        # all pickings available
+        self.assert_response_manual_selection(
+            response, pickings=self.picking1 + self.picking2,
         )
