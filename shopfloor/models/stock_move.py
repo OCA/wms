b58ccb0952bc2b3114aa311b8e102d74ce6f925c
Sébastien Alix <sebalix@users.noreply.github.com>
2020-09-17 13:13:08 +0200

    new method to validate moves in a separate transfer (#72)
    Calling _action_done() directly on the stock.move while leaving the
    stock.picking "assigned" with the other moves already required various
    workarounds as it is normally not an expected flow in Odoo.
    
    This method is to be used in services instead of calling _action_done() on
    the moves. If a move to set to done contains move lines that should not
    be validated, the line to keep should be extracted beforehand with
    `move_id.split_other_move_lines`.

--- shopfloor/models/stock_move.py
+++ shopfloor/models/stock_move.py
@@ -1,4 +1,4 @@
-from odoo import models
+from odoo import _, models
 
 
 class StockMove(models.Model):
@@ -36,3 +36,45 @@ class StockMove(models.Model):
                 if picking.state == "done":
                     picking.action_done()
         return moves
+
+    def extract_and_action_done(self):
+        """Extract the moves in a separate transfer and validate them.
+
+        You can combine this method with `split_other_move_lines` method
+        to first extract some move lines in a separate move, then validate it
+        with this method.
+        """
+        moves = self.filtered(lambda m: m.state == "assigned")
+        if not moves:
+            return False
+        for picking in moves.picking_id:
+            moves_todo = picking.move_lines & moves
+            if moves_todo == picking.move_lines:
+                # No need to create a new transfer if we are processing all moves
+                new_picking = picking
+            else:
+                new_picking = picking.copy(
+                    {
+                        "name": "/",
+                        "move_lines": [],
+                        "move_line_ids": [],
+                        "backorder_id": picking.id,
+                    }
+                )
+                new_picking.message_post(
+                    body=_(
+                        "Created from backorder "
+                        "<a href=# data-oe-model=stock.picking data-oe-id=%d>%s</a>."
+                    )
+                    % (picking.id, picking.name)
+                )
+                moves_todo.write({"picking_id": new_picking.id})
+                moves_todo.package_level_id.write({"picking_id": new_picking.id})
+                moves_todo.move_line_ids.write({"picking_id": new_picking.id})
+                moves_todo.move_line_ids.package_level_id.write(
+                    {"picking_id": new_picking.id}
+                )
+                new_picking.action_assign()
+                assert new_picking.state == "assigned"
+            new_picking.action_done()
+        return True
