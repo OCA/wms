c28f15ed0593529ebdfed7191471e8819ceb26fd
Simone Orsi <simahawk@gmail.com>
2020-09-24 11:05:56 +0200

    shopfloor_mobile: process registry cleanup and doc

--- shopfloor_mobile/static/wms/src/services/process_registry.js
+++ shopfloor_mobile/static/wms/src/services/process_registry.js
@@ -4,6 +4,6 @@
  * License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
  */
 
-import {Registry} from "./registry.js";
+import {ProcessRegistry} from "./registry.js";
 
-export var process_registry = new Registry();
+export var process_registry = new ProcessRegistry();
