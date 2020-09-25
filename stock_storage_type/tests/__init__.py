a1e7b7e41e5b33c994a5faa60aeee1885cc59c4f
sebalix <seb@usr-src.org>
2020-09-25 11:58:44 +0200

    [IMP] stock_storage_type: sort putaway locations by supported max_height
    New max_height field on the the location storage type to indicate the
    max height supported by a location among all its storage type.
    
    We want to propose the first location corresponding to the height of
    a package by losing a minimum of space, so we sort leaf locations on
    their supported max height.

--- stock_storage_type/tests/__init__.py
+++ stock_storage_type/tests/__init__.py
@@ -3,3 +3,4 @@ from . import common
 from . import test_package_height_required
 from . import test_storage_type_move
 from . import test_storage_type_putaway_strategy
+from . import test_stock_location
