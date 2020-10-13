cbf6d9fd2a3ea34309259b5f41bbb256fffdaf3a
Simone Orsi <simahawk@gmail.com>
2020-10-13 10:08:18 +0200

    shopfloor_checkout_sync: update manifest and README

--- shopfloor_checkout_sync/__manifest__.py
+++ shopfloor_checkout_sync/__manifest__.py
@@ -3,10 +3,11 @@
     "name": "Shopfloor - Checkout Sync",
     "summary": "Glue module",
     "author": "Camptocamp, Odoo Community Association (OCA)",
-    "website": "https://github.com/OCA/stock-logistics-warehouse",
+    "website": "https://github.com/OCA/wms",
     "category": "Warehouse Management",
     "version": "13.0.1.0.0",
     "license": "AGPL-3",
+    "maintainers": ["guewen"],
     "depends": [
         "shopfloor",
         # OCA/stock-logistics-warehouse
