fce5efcae8b537d88d2f9c3f6a01455256603c93
Simone Orsi <simahawk@gmail.com>
2020-11-25 10:44:24 +0100

    shopfloor: checkout option to turn off 'no_package'

--- shopfloor/tests/test_checkout_no_package.py
+++ shopfloor/tests/test_checkout_no_package.py
@@ -1,37 +1,44 @@
 # Copyright 2020 Camptocamp SA (http://www.camptocamp.com)
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+import werkzeug
+
 from .test_checkout_base import CheckoutCommonCase
 from .test_checkout_select_package_base import CheckoutSelectPackageMixin
 
 
 class CheckoutNoPackageCase(CheckoutCommonCase, CheckoutSelectPackageMixin):
-    def test_no_package_ok(self):
-        picking = self._create_picking(
+    @classmethod
+    def setUpClassBaseData(cls):
+        super().setUpClassBaseData()
+        cls.picking = picking = cls._create_picking(
             lines=[
-                (self.product_a, 10),
-                (self.product_b, 10),
-                (self.product_c, 10),
-                (self.product_d, 10),
+                (cls.product_a, 10),
+                (cls.product_b, 10),
+                (cls.product_c, 10),
+                (cls.product_d, 10),
             ]
         )
-        pack1_moves = picking.move_lines[:3]
-        pack2_moves = picking.move_lines[3:]
+        cls.pack1_moves = pack1_moves = picking.move_lines[:3]
+        cls.pack2_moves = pack2_moves = picking.move_lines[3:]
         # put in 2 packs, for this test, we'll work on pack1
-        self._fill_stock_for_moves(pack1_moves)
-        self._fill_stock_for_moves(pack2_moves)
+        cls._fill_stock_for_moves(pack1_moves)
+        cls._fill_stock_for_moves(pack2_moves)
         picking.action_assign()
 
-        move_line1, move_line2, move_line3 = pack1_moves.move_line_ids
+    def test_no_package_ok(self):
+        move_line1, move_line2, move_line3 = self.pack1_moves.move_line_ids
         selected_lines = move_line1 + move_line2
 
         # we'll put only the first 2 lines (product A and B) w/ no package
         move_line1.qty_done = move_line1.product_uom_qty
         move_line2.qty_done = move_line2.product_uom_qty
         move_line3.qty_done = 0
-
         response = self.service.dispatch(
             "no_package",
-            params={"picking_id": picking.id, "selected_line_ids": selected_lines.ids},
+            params={
+                "picking_id": self.picking.id,
+                "selected_line_ids": selected_lines.ids,
+            },
         )
 
         self.assertRecordValues(
@@ -48,9 +55,21 @@ class CheckoutNoPackageCase(CheckoutCommonCase, CheckoutSelectPackageMixin):
             response,
             # go pack to the screen to select lines to put in packages
             next_state="select_line",
-            data={"picking": self._stock_picking_data(picking)},
+            data={"picking": self._stock_picking_data(self.picking)},
             message={
                 "message_type": "success",
                 "body": "Product(s) processed as raw product(s)",
             },
         )
+
+    def test_no_package_disabled(self):
+        self.service.work.options = {"checkout:disable_no_package": True}
+        with self.assertRaises(werkzeug.exceptions.BadRequest) as err:
+            self.service.dispatch(
+                "no_package",
+                params={
+                    "picking_id": self.picking.id,
+                    "selected_line_ids": self.pack1_moves.move_line_ids.ids,
+                },
+            )
+            self.assertEqual(err.name, "`checkout.no_package` endpoint is not enabled")
