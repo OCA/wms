0ea16a444ac6d210f9005d34bd5ad58080a05a43
Sébastien BEAU <sebastien.beau@akretion.com>
2020-12-10 11:15:40 +0100

    [IMP] black, isort, prettier

--- stock_available_to_promise_release/models/res_config_settings.py
+++ stock_available_to_promise_release/models/res_config_settings.py
@@ -9,8 +9,10 @@ class ResConfigSettings(models.TransientModel):
     _inherit = "res.config.settings"
 
     stock_reservation_horizon = fields.Integer(
-        related="company_id.stock_reservation_horizon", readonly=False,
+        related="company_id.stock_reservation_horizon",
+        readonly=False,
     )
     stock_release_max_prep_time = fields.Integer(
-        related="company_id.stock_release_max_prep_time", readonly=False,
+        related="company_id.stock_release_max_prep_time",
+        readonly=False,
     )
