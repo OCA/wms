0a96633a9b9af80ef1fad7685d2d1a3f00ae2743
Sébastien Alix <sebastien.alix@camptocamp.com>
2020-12-10 12:02:50 +0100

    [FIX] 'Release ready' based on the shipping policy of the order

--- sale_stock_available_to_promise_release/models/stock_picking.py
+++ sale_stock_available_to_promise_release/models/stock_picking.py
@@ -10,3 +10,11 @@ class StockPicking(models.Model):
     sale_date_expected = fields.Date(
         string="Delivery Date", related="group_id.date_expected", store=True
     )
+
+    def _get_shipping_policy(self):
+        # Override the method to get the shipping policy from the related order:
+        # We want to release the transfers based on the shipping policy of the
+        # order, and the policy defined at the transfer level will be used only
+        # to process the transfer as usual.
+        self.ensure_one()
+        return self.sale_id.picking_policy or super()._get_shipping_policy()
