35e879ee808211c3911a0dd884a45b636aeaa4c5
Guewen Baconnier <guewen.baconnier@camptocamp.com>
2020-12-11 11:37:49 +0100

    Change license of stock_available_to_promise_release to LGPL-3

--- sale_stock_available_to_promise_release/__manifest__.py
+++ sale_stock_available_to_promise_release/__manifest__.py
@@ -1,5 +1,5 @@
 # Copyright 2019 Camptocamp (https://www.camptocamp.com)
-# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+# License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl.html).
 
 {
     "name": "Stock Available to Promise Release - Sale Integration",
@@ -15,7 +15,7 @@
         "views/sale_order_views.xml",
     ],
     "installable": False,
-    "license": "AGPL-3",
+    "license": "LGPL-3",
     "application": False,
     "development_status": "Alpha",
 }
