5789b3db33b61dce0ee771cd26e89e8eed452d02
Thierry Ducrest <thierry.ducrest@camptocamp.com>
2020-12-11 08:51:57 +0100

    [13.0][ADD] delivery_preference_glue_stock_picking_group

---
+++ delivery_preference_glue_stock_picking_group/models/stock_move.py
@@ -0,0 +1,23 @@
+# Copyright 2020 Camptocamp SA
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html)
+
+from odoo import models
+
+
+class StockMove(models.Model):
+
+    _inherit = "stock.move"
+
+    def release_available_to_promise(self):
+        """After release reset to customer prefered carrier the backorder."""
+        pickings = (
+            self.filtered(self._filter_recompute_preferred_carrier)
+            .filtered(lambda r: r.picking_id.picking_type_code == "outgoing")
+            .mapped("picking_id")
+        )
+        sales = pickings.sale_ids
+        res = super().release_available_to_promise()
+        need_release_pickings = sales.picking_ids.filtered("need_release")
+        for picking in need_release_pickings:
+            picking.carrier_id = picking.sale_id.carrier_id
+        return res
