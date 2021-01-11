886e106a57607c1b0e369dccd29e5b261769c76c
Guewen Baconnier <guewen.baconnier@camptocamp.com>
2021-01-11 16:31:29 +0100

    Switch stock_release_channel to AGPL

--- stock_release_channel/__manifest__.py
+++ stock_release_channel/__manifest__.py
@@ -1,11 +1,11 @@
 # Copyright 2020 Camptocamp
-# License OPL-1
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html)
 
 {
     "name": "Stock Release Channels",
     "summary": "Manage workload in WMS with release channels",
     "version": "13.0.1.0.0",
-    "license": "OPL-1",
+    "license": "AGPL-3",
     "author": "Camptocamp",
     "website": "https://www.camptocamp.com",
     "depends": [
--- stock_release_channel/models/stock_release_channel.py
+++ stock_release_channel/models/stock_release_channel.py
@@ -1,5 +1,5 @@
 # Copyright 2020 Camptocamp
-# License OPL-1
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html)
 
 import datetime
 import logging
