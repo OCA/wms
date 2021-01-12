0c1caf2058df1da12dd0fd1b1e2eff3e8084d8dd
Guewen Baconnier <guewen.baconnier@camptocamp.com>
2021-01-12 15:21:48 +0100

    Add tests

---
+++ stock_release_channel/tests/test_channel_release_batch.py
@@ -0,0 +1,63 @@
+# Copyright 2020 Camptocamp (https://www.camptocamp.com)
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html)
+
+from odoo import _, exceptions
+
+from .common import ChannelReleaseCase
+
+
+class TestChannelReleaseBatch(ChannelReleaseCase):
+    @classmethod
+    def setUpClass(cls):
+        super().setUpClass()
+        cls.pickings = cls.picking + cls.picking2 + cls.picking3
+        for __ in range(3):
+            cls.pickings += cls._out_picking(
+                cls._create_picking_chain(
+                    cls.wh, [(cls.product1, 5), (cls.product2, 5)], move_type="direct"
+                )
+            )
+        cls._update_qty_in_location(cls.loc_bin1, cls.product1, 1000.0)
+        cls._update_qty_in_location(cls.loc_bin1, cls.product2, 1000.0)
+
+    def test_release_next_batch_no_config(self):
+        self.channel.max_auto_release = 0
+        with self.assertRaises(exceptions.UserError):
+            self.channel.release_next_batch()
+
+    def test_release_next_batch(self):
+        self.channel.max_auto_release = 2
+        self.channel.release_next_batch()
+        # 2 have been released
+        self.assertEqual(
+            self.pickings.mapped("need_release"), [False, False, True, True, True, True]
+        )
+
+        with self.assertRaises(exceptions.UserError):
+            # nothing new to release
+            self.channel.release_next_batch()
+
+        self._action_done_picking(self.pickings[0].move_lines.move_orig_ids.picking_id)
+        self._action_done_picking(self.pickings[0])
+
+        self.channel.release_next_batch()
+        # 1 have been released to reach the max of  2
+        self.assertEqual(
+            self.pickings.mapped("need_release"),
+            [False, False, False, True, True, True],
+        )
+
+    def test_release_no_next_batch(self):
+        self.pickings.need_release = False  # cheat for getting the right condition
+        action = self.channel.release_next_batch()
+        self.assertEqual(
+            action,
+            {
+                "effect": {
+                    "fadeout": "fast",
+                    "message": _("Nothing in the queue!"),
+                    "img_url": "/web/static/src/img/smile.svg",
+                    "type": "rainbow_man",
+                }
+            },
+        )
