0d20c9d711b2dd4e5c7f4f330c1496bf485ecbbe
Simone Orsi <simahawk@gmail.com>
2021-01-13 11:34:51 +0100

    shopfloor: expose user info

--- shopfloor/services/user.py
+++ shopfloor/services/user.py
@@ -1,5 +1,6 @@
 # Copyright 2020 Camptocamp SA (http://www.camptocamp.com)
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+from odoo.addons.base_rest.components.service import to_int
 from odoo.addons.component.core import Component
 
 
@@ -17,6 +18,17 @@ class ShopfloorUser(Component):
         menus = menu_comp._to_json(menu_comp._search())
         return self._response(data={"menus": menus})
 
+    # TODO: this endpoint does not require profile header
+    def user_info(self):
+        return self._response(data={"user_info": self._user_info()})
+
+    def _user_info(self):
+        return self.env.user.jsonify(self._user_info_parser, one=True)
+
+    @property
+    def _user_info_parser(self):
+        return ["id", "name"]
+
 
 class ShopfloorUserValidator(Component):
     """Validators for the User endpoints"""
@@ -26,6 +38,9 @@ class ShopfloorUserValidator(Component):
     _usage = "user.validator"
 
     def menu(self):
+        return {}
+
+    def user_info(self):
         return {}
 
 
@@ -50,3 +65,20 @@ class ShopfloorUserValidatorResponse(Component):
                 },
             }
         )
+
+    def user_info(self):
+        return self._response_schema(
+            {
+                "user_info": {
+                    "type": "dict",
+                    "required": True,
+                    "schema": self._user_info_schema(),
+                }
+            }
+        )
+
+    def _user_info_schema(self):
+        return {
+            "id": {"coerce": to_int, "required": True, "type": "integer"},
+            "name": {"type": "string", "nullable": False, "required": True},
+        }
