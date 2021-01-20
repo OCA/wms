4c7d322eb51aaa81d72fb3aeafb94455c725e8ef
Thierry Ducrest <thierry.ducrest@camptocamp.com>
2021-01-20 11:45:30 +0100

    [13.0] Add shopfloor_workstation
    Shopfloor workstation are useful for a quick configuration of the shopfloor
    mobile application and its connected user.
    
    To synchronize a connected user to a workstation setting, go to
    Settings - Workstation in the mobile application and scan a workstation barcode.

---
+++ shopfloor_workstation/__manifest__.py
@@ -0,0 +1,17 @@
+# Copyright 2021 Camptocamp SA (http://www.camptocamp.com)
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+
+{
+    "name": "Shopfloor Workstation",
+    "summary": "Manage warehouse workstation with barcode scanners",
+    "version": "13.0.1.0.0",
+    "development_status": "Alpha",
+    "category": "Inventory",
+    "website": "https://github.com/OCA/wms",
+    "author": "Camptocamp, Odoo Community Association (OCA)",
+    "license": "AGPL-3",
+    "application": False,
+    "depends": ["shopfloor", "base_report_to_printer"],
+    "demo": ["demo/shopfloor_workstation_demo.xml"],
+    "data": ["security/ir.model.access.csv", "views/shopfloor_workstation.xml"],
+}
