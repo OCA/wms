1fc2ba1b0572cc6acabe41b292c5dea5fafabfe5
Thierry Ducrest <thierry.ducrest@camptocamp.com>
2021-01-20 10:33:13 +0100

    [13.0] shopfloor_mobile: Add setting page improvements
    Move the log out button to the main setting page.
    On the profile and language setting buttons, add the
    active value.

--- shopfloor_mobile/static/wms/src/main.js
+++ shopfloor_mobile/static/wms/src/main.js
@@ -87,6 +87,24 @@ const app = new Vue({
         app_info: function() {
             return shopfloor_app_info;
         },
+        available_languages: function() {
+            // FIXME: this should come from odoo and from app config
+            // They will match w/ $i18n.availableLocales
+            return [
+                {
+                    id: "en-US",
+                    name: this.$t("language.name.English"),
+                },
+                {
+                    id: "fr-FR",
+                    name: this.$t("language.name.French"),
+                },
+                {
+                    id: "de-DE",
+                    name: this.$t("language.name.German"),
+                },
+            ];
+        },
         has_profile: function() {
             return !_.isEmpty(this.profile);
         },
