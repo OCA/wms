4abf3f914107f0d0f313492907455eed8fb2186c
Guewen Baconnier <guewen.baconnier@camptocamp.com>
2021-01-26 15:39:39 +0100

    Add automatic release per group of commercial partner
    The initial automatic release allows to release a number of transfers N
    up to a max of X in progress transfers at a time.
    
    Introduce a selection of types of automatic release, with, alongside the
    former "Max", a new grouping by commercial partner.
    
    When using this automatic release, we take the first (by priority)
    transfer ready to release, and all the transfers with the same
    commercial partner, then release this whole group of transfers.

--- stock_release_channel/tests/test_channel_release_batch.py
+++ stock_release_channel/tests/test_channel_release_batch.py
@@ -12,20 +12,22 @@ class TestChannelReleaseBatch(ChannelReleaseCase):
         super().setUpClass()
         cls.pickings = cls.picking + cls.picking2 + cls.picking3
         for __ in range(3):
-            cls.pickings += cls._out_picking(
+            delivery = cls._out_picking(
                 cls._create_picking_chain(
                     cls.wh, [(cls.product1, 5), (cls.product2, 5)], move_type="direct"
                 )
             )
+            delivery.partner_id = cls.other_partner
+            cls.pickings += delivery
         cls._update_qty_in_location(cls.loc_bin1, cls.product1, 1000.0)
         cls._update_qty_in_location(cls.loc_bin1, cls.product2, 1000.0)
 
-    def test_release_next_batch_no_config(self):
+    def test_release_auto_max_next_batch_no_config(self):
         self.channel.max_auto_release = 0
         with self.assertRaises(exceptions.UserError):
             self.channel.release_next_batch()
 
-    def test_release_next_batch(self):
+    def test_release_auto_max_next_batch(self):
         self.channel.max_auto_release = 2
         self.channel.release_next_batch()
         # 2 have been released
@@ -47,9 +49,26 @@ class TestChannelReleaseBatch(ChannelReleaseCase):
             [False, False, False, True, True, True],
         )
 
-    def test_release_no_next_batch(self):
+    def test_release_auto_max_no_next_batch(self):
         self.pickings.need_release = False  # cheat for getting the right condition
         action = self.channel.release_next_batch()
+        self._assert_action_nothing_in_the_queue(action)
+
+    def test_release_auto_group_commercial_partner(self):
+        self.channel.auto_release = "group_commercial_partner"
+        self.channel.release_next_batch()
+        self.assertFalse(self.picking.need_release)
+        self.assertFalse(self.picking2.need_release)
+        other_pickings = self.pickings - (self.picking | self.picking2)
+        self.assertTrue(all(p.need_release) for p in other_pickings)
+
+    def test_release_auto_group_commercial_partner_no_next_batch(self):
+        self.channel.auto_release = "group_commercial_partner"
+        self.pickings.need_release = False  # cheat for getting the right condition
+        action = self.channel.release_next_batch()
+        self._assert_action_nothing_in_the_queue(action)
+
+    def _assert_action_nothing_in_the_queue(self, action):
         self.assertEqual(
             action,
             {
