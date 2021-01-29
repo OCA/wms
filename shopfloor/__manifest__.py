52968f2b07ac1550cc3426f6d531faf906f9daef
Simone Orsi <simahawk@gmail.com>
2021-01-29 16:08:39 +0100

    shopfloor: use db logging from rest_log

--- shopfloor/__manifest__.py
+++ shopfloor/__manifest__.py
@@ -19,6 +19,7 @@
         "stock_picking_batch",
         "base_jsonify",
         "base_rest",
+        "rest_log",
         "base_sparse_field",
         "auth_api_key",
         #  OCA / stock-logistics-warehouse
@@ -43,8 +44,6 @@
         "product_packaging_type",
     ],
     "data": [
-        "data/ir_config_parameter_data.xml",
-        "data/ir_cron_data.xml",
         "security/ir.model.access.csv",
         "views/res_partner.xml",
         "views/shopfloor_menu.xml",
@@ -53,7 +52,6 @@
         "views/stock_move_line.xml",
         "views/stock_picking_views.xml",
         "views/shopfloor_profile_views.xml",
-        "views/shopfloor_log_views.xml",
         "views/menus.xml",
     ],
     "demo": [
