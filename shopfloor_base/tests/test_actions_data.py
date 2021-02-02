d41efe3db845da84d9da56db432c8a81f61653a5
Simone Orsi <simahawk@gmail.com>
2021-02-02 13:33:34 +0100

    shopfloor_base: reorganize misc test classes

--- shopfloor_base/tests/test_actions_data.py
+++ shopfloor_base/tests/test_actions_data.py
@@ -1,30 +1,15 @@
 # Copyright 2020 Camptocamp SA (http://www.camptocamp.com)
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
-import logging
-
 from .common import CommonCase
-
-_logger = logging.getLogger(__name__)
-
-
-try:
-    from cerberus import Validator
-except ImportError:
-    _logger.debug("Can not import cerberus")
+from .common_misc import ActionsDataTestMixin
 
 
-class ActionsDataCaseBase(CommonCase):
+class ActionsDataCase(CommonCase, ActionsDataTestMixin):
     @classmethod
     def setUpClassBaseData(cls):
         super().setUpClassBaseData()
         cls.partner = cls.env.ref("base.res_partner_12").sudo()
 
-    def assert_schema(self, schema, data):
-        validator = Validator(schema)
-        self.assertTrue(validator.validate(data), validator.errors)
-
-
-class ActionsDataCase(ActionsDataCaseBase):
     def test_data_partner(self):
         data = self.data.partner(self.partner)
         self.assert_schema(self.schema._simple_record(), data)
