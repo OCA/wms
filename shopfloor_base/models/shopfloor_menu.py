b80b7f0c3f9f918cfaedd8de2645eeb580cacfdc
Simone Orsi <simahawk@gmail.com>
2021-02-02 10:30:55 +0100

    shopfloor_base: add shopfloor.scenario

--- shopfloor_base/models/shopfloor_menu.py
+++ shopfloor_base/models/shopfloor_menu.py
@@ -1,4 +1,6 @@
 # Copyright 2020 Camptocamp SA (http://www.camptocamp.com)
+# Copyright 2021 ACSONE SA/NV (http://www.camptocamp.com)
+# @author Simone Orsi <simahawk@gmail.com>
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 from odoo import fields, models
 
@@ -10,5 +12,11 @@ class ShopfloorMenu(models.Model):
 
     name = fields.Char(required=True, translate=True)
     sequence = fields.Integer()
-    scenario = fields.Char()
+    profile_id = fields.Many2one(
+        "shopfloor.profile", string="Profile", help="Visible on this profile only"
+    )
+    scenario_id = fields.Many2one(
+        comodel_name="shopfloor.scenario", required=True, ondelete="cascade"
+    )
+    scenario = fields.Char(related="scenario_id.key")
     active = fields.Boolean(default=True)
