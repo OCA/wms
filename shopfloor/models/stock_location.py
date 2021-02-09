75dac20b119185847d654ee8120a4ca01b114fb7
Guewen Baconnier <guewen.baconnier@camptocamp.com>
2021-02-09 12:17:33 +0100

    shopfloor: add dependency on stock_helper
    To remove the duplicate implementation of
    StockLocation.is_sublocation_of()

--- shopfloor/models/stock_location.py
+++ shopfloor/models/stock_location.py
@@ -23,18 +23,6 @@ class StockLocation(models.Model):
         compute="_compute_reserved_move_lines",
     )
 
-    def is_sublocation_of(self, others, func=any):
-        """Return True if self is a sublocation of others (or equal)
-
-        By default, it return True if any other is a parent or equal.
-        ``all`` can be passed to ``func`` to require all the other locations
-        to be parent or equal to be True.
-        """
-        self.ensure_one()
-        # Efficient way to verify that the current location is
-        # below one of the other location without using SQL.
-        return func(self.parent_path.startswith(other.parent_path) for other in others)
-
     def _get_reserved_move_lines(self):
         return self.env["stock.move.line"].search(
             [
