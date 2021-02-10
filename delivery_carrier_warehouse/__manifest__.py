4b068d4cba86c7743fe7a9810e7440107508ecbb
Tran Thanh Phuc <phuc@trobz.com>
2021-02-10 21:23:31 +0700

    [MIG] delivery_carrier_warehouse: Migration to 14.0

--- delivery_carrier_warehouse/__manifest__.py
+++ delivery_carrier_warehouse/__manifest__.py
@@ -3,7 +3,7 @@
 {
     "name": "Delivery Carrier Warehouse",
     "summary": "Get delivery method used in sales orders from warehouse",
-    "version": "13.0.1.2.0",
+    "version": "14.0.1.2.0",
     "category": "Operations/Inventory/Delivery",
     "website": "https://github.com/OCA/wms",
     "author": "Camptocamp, Odoo Community Association (OCA)",
