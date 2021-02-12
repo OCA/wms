3b78a0cb3f3eb2728c2bd00d43dd34159b8091e4
Simone Orsi <simahawk@gmail.com>
2021-02-12 14:38:12 +0100

    shopfloor_mobile: fix relative import paths

--- shopfloor_mobile_base/static/wms/src/components/list.js
+++ shopfloor_mobile_base/static/wms/src/components/list.js
@@ -4,7 +4,7 @@
  * License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
  */
 
-import {ItemDetailMixin} from "./detail/detail_mixin.js";
+import {ItemDetailMixin} from "/shopfloor_mobile_base/static/wms/src/components/detail/detail_mixin.js";
 
 /* eslint-disable strict */
 Vue.component("list", {
