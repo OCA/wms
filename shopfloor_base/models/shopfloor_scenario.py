bfeaf94ab279fe4779f9f02885909e5982b1f46c
Simone Orsi <simahawk@gmail.com>
2021-02-12 09:10:44 +0100

    shopfloor: fix copyright

--- shopfloor_base/models/shopfloor_scenario.py
+++ shopfloor_base/models/shopfloor_scenario.py
@@ -1,4 +1,4 @@
-# Copyright 2021 ACSONE SA/NV (http://www.camptocamp.com)
+# Copyright 2021 ACSONE SA/NV (http://www.acsone.eu)
 # @author Simone Orsi <simahawk@gmail.com>
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 import logging
