8b208e0e1d4f8fbb559916481239582f6bb804dd
Simone Orsi <simahawk@gmail.com>
2021-02-12 15:49:43 +0100

    shopfloor_mobile_base: reorganize registries

--- shopfloor_mobile_base/static/wms/src/services/color_registry.js
+++ shopfloor_mobile_base/static/wms/src/services/color_registry.js
@@ -4,7 +4,30 @@
  * License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
  */
 
-import {ColorRegistry} from "./registry.js";
+export class ColorRegistry {
+    constructor(theme, _default = "light") {
+        this.themes = {};
+        this.default_theme = _default;
+    }
+
+    add_theme(colors, theme) {
+        if (_.isUndefined(theme)) theme = this.default_theme;
+        this.themes[theme] = colors;
+    }
+
+    color_for(key, theme) {
+        if (_.isUndefined(theme)) theme = this.default_theme;
+        if (!this.themes[theme]) {
+            console.log("Theme", theme, "not registered.");
+            return null;
+        }
+        return this.themes[theme][key];
+    }
+
+    get_themes() {
+        return this.themes;
+    }
+}
 
 export var color_registry = new ColorRegistry();
 
