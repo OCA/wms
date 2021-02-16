e72269a9a9346089514171ed744e4d182c984254
Simone Orsi <simahawk@gmail.com>
2021-02-16 08:42:03 +0100

    shopfloor_base: re-license to LGPL

--- shopfloor_base/models/shopfloor_scenario.py
+++ shopfloor_base/models/shopfloor_scenario.py
@@ -1,6 +1,6 @@
 # Copyright 2021 ACSONE SA/NV (http://www.acsone.eu)
 # @author Simone Orsi <simahawk@gmail.com>
-# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+# License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl.html).
 import json
 import logging
 
