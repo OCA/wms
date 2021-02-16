959b64ea26cb93c910fdfce66195606214473687
Simone Orsi <simahawk@gmail.com>
2021-02-16 08:42:03 +0100

    shopfloor_base: re-license to LGPL

--- shopfloor_base/controllers/main.py
+++ shopfloor_base/controllers/main.py
@@ -1,6 +1,6 @@
 # Copyright 2020 Camptocamp SA (http://www.camptocamp.com)
 # Copyright 2020 Akretion (http://www.akretion.com)
-# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+# License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl.html).
 
 from odoo.addons.base_rest.controllers import main
 
