ec8223ee4bbdbde0204a1e459cc3e5f013b742cb
Sébastien Alix <sebastien.alix@camptocamp.com>
2021-03-03 09:53:54 +0100

    s_storage_type: no storage type for delivery
    There is no need to set a package storage type on delivery packages (we
    are not storing them).
    This avoid to trigger blocking constraints (like height).

--- stock_storage_type/tests/__init__.py
+++ stock_storage_type/tests/__init__.py
@@ -4,3 +4,4 @@ from . import test_package_height_required
 from . import test_storage_type_move
 from . import test_storage_type_putaway_strategy
 from . import test_stock_location
+from . import test_auto_assign_storage_type
