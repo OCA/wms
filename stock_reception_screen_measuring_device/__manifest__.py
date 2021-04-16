97871509d1eeecfd3243c61f486681a4e3668f29
Alexandre Fayolle <alexandre.fayolle@camptocamp.com>
2021-04-16 08:48:04 +0200

    fixup! fixup! fixup! fixup! fixup! [ADD] stock_reception_screen_measuring_device

--- stock_reception_screen_measuring_device/__manifest__.py
+++ stock_reception_screen_measuring_device/__manifest__.py
@@ -10,7 +10,7 @@
     "license": "AGPL-3",
     "depends": ["stock_reception_screen", "stock_measuring_device"],
     "data": ["views/stock_reception_screen_view.xml", "views/measuring_device_view.xml"],
-    "website": "https://github.com/OCA/stock-logistics-warehouse",
+    "website": "https://github.com/OCA/wms",
     "installable": True,
     "auto_install": True,
     "development_status": "Alpha",
