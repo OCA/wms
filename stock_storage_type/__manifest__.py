3d91b926210cd71451e1900e30a9fe5e318ac511
Jacques-Etienne Baudoux <je@bcim.be>
2021-06-04 21:52:47 +0200

    Change sorting of locations in the pack putaway computation
    Invert the sorting for abc packs storage strategy to first apply the
    max_height which is a physical constraint, then apply the abc
    preference.
    
    Add a pack putaway sequence to be able to favor some locations (e.g.
    level). For ordered children strategy, finalize by sorting by location
    name. For abc, random selection is applied at last as it was already.
    
    Replaced multi-steps python sorting by an unique SQL quicksort query to
    quickly compute the result on large stock_location table. This is
    especially important for operations containing many moves requiring a
    putaway computation.

--- stock_storage_type/__manifest__.py
+++ stock_storage_type/__manifest__.py
@@ -1,4 +1,5 @@
-# Copyright 2019 Camptocamp SA
+# Copyright 2019-2021 Camptocamp SA
+# Copyright 2019-2021 Jacques-Etienne Baudoux (BCIM) <je@bcim.be>
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl)
 {
     "name": "Stock Storage Type",
@@ -7,7 +8,7 @@
     "development_status": "Alpha",
     "category": "Warehouse Management",
     "website": "https://github.com/OCA/wms",
-    "author": "Camptocamp, Odoo Community Association (OCA)",
+    "author": "Camptocamp, BCIM, Odoo Community Association (OCA)",
     "license": "AGPL-3",
     "application": False,
     "installable": True,
