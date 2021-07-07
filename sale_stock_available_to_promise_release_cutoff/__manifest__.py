aa94dd58f5607d0b746c25a482816cff91255bdf
Matthieu Méquignon <matthieu.mequignon@camptocamp.com>
2021-07-07 12:58:54 +0200

    13.0: Add sale_stock_available_to_promise_release_cutoff

---
+++ sale_stock_available_to_promise_release_cutoff/__manifest__.py
@@ -0,0 +1,19 @@
+# Copyright 2021 Camptocamp (https://www.camptocamp.com)
+# License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl.html).
+
+{
+    "name": "Sale Stock Available to Promise Release Cutoff",
+    "summary": "Cutoff management with respect to stock availability",
+    "version": "13.0.1.0.0",
+    "author": "Camptocamp,Odoo Community Association (OCA)",
+    "website": "https://github.com/OCA/wms",
+    "category": "Stock Management",
+    "depends": [
+        "sale_stock_available_to_promise_release",
+        "sale_cutoff_time_delivery",
+    ],
+    "installable": True,
+    "auto_install": True,
+    "license": "LGPL-3",
+    "application": False,
+}
