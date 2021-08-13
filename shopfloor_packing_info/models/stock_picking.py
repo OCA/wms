55d520c91aa4c82a7c73480e100a93a5b5613b56
Sébastien Alix <sebastien.alix@camptocamp.com>
2021-08-13 14:12:26 +0200

    [IMP] shopfloor_packing_info: black, isort, prettier

--- shopfloor_packing_info/models/stock_picking.py
+++ shopfloor_packing_info/models/stock_picking.py
@@ -7,7 +7,8 @@ class StockPicking(models.Model):
     _inherit = "stock.picking"
 
     shopfloor_packing_info_id = fields.Many2one(
-        string="Packing information", related="partner_id.shopfloor_packing_info_id",
+        string="Packing information",
+        related="partner_id.shopfloor_packing_info_id",
     )
     shopfloor_display_packing_info = fields.Boolean(
         related="picking_type_id.shopfloor_display_packing_info",
