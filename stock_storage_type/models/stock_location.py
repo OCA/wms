OCA/wms

commit 6b25b867c2f606d5f4220915ab9aae3b30c1f87f

Change sorting of locations in the pack putaway computation
Invert the sorting for abc packs storage strategy to first apply the
max_height which is a physical constraint, then apply the abc
preference.

Add a pack putaway sequence to be able to favor some locations (e.g.
level). For ordered children strategy, finalize by sorting by location
name. For abc, random selection is applied at last as it was already.

Replaced multi-steps python sorting by an unique SQL quicksort query to
quickly compute the result on large stock_location table. This is
especially important for operations containing many moves requiring a
putaway computation.

stock_storage_type/models/stock_location.py
@@ -1,6 +1,9 @@
-# Copyright 2019 Camptocamp SA
+# Copyright 2019-2021 Camptocamp SA
+# Copyright 2019-2021 Jacques-Etienne Baudoux (BCIM) <je@bcim.be>
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl)
 import logging
+
+from psycopg2 import sql
 
 from odoo import api, fields, models
 from odoo.tools import float_compare
@@ -49,6 +52,7 @@ class StockLocation(models.Model):
         "locations according to the restrictions defined on their "
         "respective location storage types.",
     )
+    pack_putaway_sequence = fields.Integer()
     storage_location_sequence_ids = fields.One2many(
         "stock.storage.location.sequence",
         "location_id",
@@ -361,9 +365,53 @@ class StockLocation(models.Model):
         locations = self.browse()
         if self.pack_putaway_strategy == "none":
             locations = self
-        elif self.pack_putaway_strategy == "ordered_locations":
-            locations = self._get_ordered_leaf_locations()
+        else:
+            locations = self._get_sorted_leaf_locations(products)
         return locations
+
+    def _get_sorted_leaf_locations_orderby(self, products):
+        """Return SQL orderby clause and params for sorting locations
+
+        First, locations are ordered by max height, knowing that a max height of 0
+        means "no limit" and as such it should be among the last locations.
+        Then, they are ordered by a sequence and name.
+        """
+        self.env["stock.location"].flush(
+            ["max_height", "pack_putaway_sequence", "name"]
+        )
+        orderby = []
+        if self.pack_putaway_strategy == "ordered_locations":
+            orderby = [
+                "CASE WHEN max_height > 0 THEN max_height ELSE 'Infinity' END",
+                "pack_putaway_sequence",
+                "name",
+            ]
+        return ", ".join(orderby), []
+
+    def _get_sorted_leaf_locations(self, products):
+        """Return sorted leaf sub-locations
+
+        The locations are candidate locations that will be evaluated one per
+        one in order to find the first available location. They must be leaf
+        locations where we can actually put goods.
+        """
+        if not self.leaf_location_ids:
+            return self.leaf_location_ids
+        query = self._where_calc([("id", "in", self.leaf_location_ids.ids)])
+        from_clause, where_clause, where_params = query.get_sql()
+        orderby_clause, orderby_params = self._get_sorted_leaf_locations_orderby(
+            products
+        )
+        query = sql.SQL(
+            "SELECT id FROM {table} WHERE {where} ORDER BY {orderby}"
+        ).format(
+            table=sql.Identifier(self._table),
+            where=sql.SQL(where_clause),
+            orderby=sql.SQL(orderby_clause),
+        )
+        self._cr.execute(query, where_params + orderby_params)
+        location_ids = [x[0] for x in self.env.cr.fetchall()]
+        return self.env["stock.location"].browse(location_ids)
 
     def select_first_allowed_location(self, package_storage_type, quants, products):
         allowed = self.select_allowed_locations(
@@ -527,20 +575,3 @@ class StockLocation(models.Model):
         ]
         valid_locations = self.browse(ordered_valid_location_ids)
         return valid_locations
-
-    def _get_ordered_leaf_locations(self):
-        """Return ordered leaf sub-locations
-
-        The locations are candidate locations that will be evaluated one per
-        one in order to find the first available location. They must be leaf
-        locations where we can actually put goods.
-
-        Locations are ordered by max height, knowing that a max height of 0
-        means "no limit" and as such it should be among the last locations.
-        """
-        if not self.leaf_location_ids:
-            return self.leaf_location_ids
-        max_height = max(self.leaf_location_ids.mapped("max_height"))
-        return self.leaf_location_ids.sorted(
-            lambda l: l.max_height if l.max_height else (max_height + 1)
-        )

stock_storage_type_putaway_abc/models/stock_location.py
@@ -1,25 +1,10 @@
-# Copyright 2019 Camptocamp SA
+# Copyright 2019-2021 Camptocamp SA
+# Copyright 2019-2021 Jacques-Etienne Baudoux (BCIM) <je@bcim.be>
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl)
-from random import shuffle
-
 from odoo import api, fields, models
 from odoo.fields import first
 
 ABC_SELECTION = [("a", "A"), ("b", "B"), ("c", "C")]
-
-
-def gather_location_ids(abc_sorted, max_heights_sorted, locations_grouped):
-    """Return a list of location IDs sorted on `abc_sorted` then
-    on `max_heights_sorted`.
-    """
-    location_ids = []
-    for abc_key in abc_sorted:
-        group_ids_per_height = locations_grouped.get(abc_key)
-        if not group_ids_per_height:
-            continue
-        for max_height in max_heights_sorted:
-            location_ids.extend(group_ids_per_height.get(max_height, []))
-    return location_ids
 
 
 class StockLocation(models.Model):
@@ -47,51 +32,23 @@ class StockLocation(models.Model):
                     current_location = current_location.location_id
             location.display_abc_storage = display_abc_storage
 
-    def get_storage_locations(self, products=None):
-        if products is None:
-            products = self.env["product.product"]
-        if self.pack_putaway_strategy == "abc":
-            return self._get_abc_locations(products)
-        return super().get_storage_locations(products)
-
-    def _get_abc_locations(self, products):
-        return self.leaf_location_ids._sort_abc_locations(first(products).abc_storage)
-
-    def _sort_abc_locations(self, product_abc):
-        product_abc = product_abc or "a"
-        # group locations by abc_storage and max_height
-        data = self.read_group(
-            [("id", "in", self.ids)], ["max_height"], ["max_height"],
-        )
-        locations_grouped = {}
-        max_heights = set()
-        for line in data:
-            domain = line["__domain"]
-            locations = self.search(domain)
-            for loc in locations:
-                locations_grouped.setdefault(loc.abc_storage, {}).setdefault(
-                    line["max_height"], []
-                )
-                locations_grouped[loc.abc_storage][line["max_height"]].append(loc.id)
-            # keep a list of available max heights
-            max_heights.add(line["max_height"])
-        # sort max heights and take care to put any 0 value at the end
-        max_heights = list(max_heights)
-        max_heights.sort()
-        if 0 in max_heights:
-            max_heights.pop(max_heights.index(0))
-            max_heights.append(0)
-        # shuffle each abc_storage/max_height chunk
-        for line in locations_grouped.values():
-            for max_height in line:
-                shuffle(line[max_height])
-        # prepare the result
+    def _get_sorted_leaf_locations_orderby(self, products):
+        if not self.pack_putaway_strategy == "abc":
+            return super()._get_sorted_leaf_locations_orderby(products)
+        product_abc = first(products).abc_storage or "a"
         if product_abc == "a":
-            location_ids = gather_location_ids("abc", max_heights, locations_grouped)
+            abc_seq = "a", "b", "c"
         elif product_abc == "b":
-            location_ids = gather_location_ids("bca", max_heights, locations_grouped)
+            abc_seq = "b", "c", "a"
         elif product_abc == "c":
-            location_ids = gather_location_ids("cba", max_heights, locations_grouped)
-        else:
-            raise ValueError("product_abc = %s" % product_abc)
-        return self.env["stock.location"].browse(location_ids)
+            abc_seq = "c", "b", "a"
+        self.env["stock.location"].flush(
+            ["abc_storage", "max_height", "pack_putaway_sequence", "name"]
+        )
+        orderby = [
+            "CASE WHEN max_height > 0 THEN max_height ELSE 'Infinity' END",
+            "array_position(%s, abc_storage::text)",
+            "pack_putaway_sequence",
+            "random()",
+        ]
+        return ", ".join(orderby), [list(abc_seq)]

stock_storage_type/__manifest__.py
@@ -1,13 +1,13 @@
-# Copyright 2019 Camptocamp SA
+# Copyright 2019-2021 Camptocamp SA
+# Copyright 2019-2021 Jacques-Etienne Baudoux (BCIM) <je@bcim.be>
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl)
 {
     "name": "Stock Storage Type",
     "summary": "Manage packages and locations storage types",
-    "version": "13.0.1.12.0",
-    "development_status": "Alpha",
+    "version": "13.0.1.13.0",
     "category": "Warehouse Management",
     "website": "https://github.com/OCA/wms",
-    "author": "Camptocamp, Odoo Community Association (OCA)",
+    "author": "Camptocamp, BCIM, Odoo Community Association (OCA)",
     "license": "AGPL-3",
     "application": False,
     "installable": True,

stock_storage_type/demo/stock_location.xml
@@ -25,6 +25,10 @@
         <field name="name">Bin 3</field>
         <field name="location_id" ref="stock_location_cardboxes" />
     </record>
+    <record id="stock_location_cardboxes_bin_4" model="stock.location">
+        <field name="name">Bin 4</field>
+        <field name="location_id" ref="stock_location_cardboxes" />
+    </record>
     <record id="stock_location_pallets" model="stock.location">
         <field name="name">Pallets storage area</field>
         <field
@@ -48,6 +52,10 @@
     </record>
     <record id="stock_location_pallets_bin_3" model="stock.location">
         <field name="name">Pallets Bin 3</field>
+        <field name="location_id" ref="stock_location_pallets" />
+    </record>
+    <record id="stock_location_pallets_bin_4" model="stock.location">
+        <field name="name">Pallets Bin 4</field>
         <field name="location_id" ref="stock_location_pallets" />
     </record>
     <record id="stock_location_pallets_reserve" model="stock.location">
@@ -75,6 +83,10 @@
         <field name="name">Pallets Reserve Bin 3</field>
         <field name="location_id" ref="stock_location_pallets_reserve" />
     </record>
+    <record id="stock_location_pallets_reserve_bin_4" model="stock.location">
+        <field name="name">Pallets Reserve Bin 4</field>
+        <field name="location_id" ref="stock_location_pallets_reserve" />
+    </record>
     <record id="stock_location_cardboxes_reserve" model="stock.location">
         <field name="name">Cardboxes reserve storage area</field>
         <field
@@ -98,6 +110,10 @@
     </record>
     <record id="stock_location_cardboxes_reserve_bin_3" model="stock.location">
         <field name="name">Cardboxes Reserve Bin 3</field>
+        <field name="location_id" ref="stock_location_cardboxes_reserve" />
+    </record>
+    <record id="stock_location_cardboxes_reserve_bin_4" model="stock.location">
+        <field name="name">Cardboxes Reserve Bin 4</field>
         <field name="location_id" ref="stock_location_cardboxes_reserve" />
     </record>
 </odoo>

stock_storage_type/tests/common.py
@@ -36,8 +36,8 @@ class TestStorageTypeCommon(SavepointCase):
         cls.cardboxes_bin_3_location = ref(
             "stock_storage_type.stock_location_cardboxes_bin_3"
         )
-        cls.cardboxes_bin_4_location = cls.cardboxes_bin_1_location.copy(
-            {"name": "Bin 4"}
+        cls.cardboxes_bin_4_location = ref(
+            "stock_storage_type.stock_location_cardboxes_bin_4"
         )
         cls.env["stock.location"]._parent_store_compute()
         cls.pallets_bin_1_location = ref(
@@ -48,6 +48,9 @@ class TestStorageTypeCommon(SavepointCase):
         )
         cls.pallets_bin_3_location = ref(
             "stock_storage_type.stock_location_pallets_bin_3"
+        )
+        cls.pallets_bin_4_location = ref(
+            "stock_storage_type.stock_location_pallets_bin_4"
         )
 
         cls.receipts_picking_type = ref("stock.picking_type_in")

stock_storage_type/tests/test_stock_location.py
@@ -8,7 +8,6 @@ class TestStockLocation(TestStorageTypeCommon):
     def setUpClass(cls):
         super().setUpClass()
         ref = cls.env.ref
-        cls.areas.write({"pack_putaway_strategy": "ordered_locations"})
         cls.pallets_reserve_bin_1_location = ref(
             "stock_storage_type.stock_location_pallets_reserve_bin_1"
         )
@@ -18,11 +17,24 @@ class TestStockLocation(TestStorageTypeCommon):
         cls.pallets_reserve_bin_3_location = ref(
             "stock_storage_type.stock_location_pallets_reserve_bin_3"
         )
+        cls.pallets_reserve_bin_4_location = ref(
+            "stock_storage_type.stock_location_pallets_reserve_bin_4"
+        )
 
     def test_get_ordered_leaf_locations(self):
+        sublocation = self.stock_location.copy(
+            {
+                "name": "Sub-location",
+                "pack_putaway_strategy": "ordered_locations",
+                "location_id": self.stock_location.id,
+            }
+        )
+        self.areas.write({"location_id": sublocation.id})
+
         # Test with the same max_height on all related storage types (0 here)
+        ordered_locations = sublocation.get_storage_locations(self.product)
         self.assertEqual(
-            self.areas._get_ordered_leaf_locations().ids,
+            ordered_locations.ids,
             (
                 self.cardboxes_bin_1_location
                 | self.cardboxes_bin_2_location
@@ -31,16 +43,19 @@ class TestStockLocation(TestStorageTypeCommon):
                 | self.pallets_bin_1_location
                 | self.pallets_bin_2_location
                 | self.pallets_bin_3_location
+                | self.pallets_bin_4_location
                 | self.pallets_reserve_bin_1_location
                 | self.pallets_reserve_bin_2_location
                 | self.pallets_reserve_bin_3_location
+                | self.pallets_reserve_bin_4_location
             ).ids,
         )
         # Set the max_height on pallets storage type higher than the others
         self.pallets_location_storage_type.max_height = 2
         self.cardboxes_location_storage_type.max_height = 1
+        ordered_locations = sublocation.get_storage_locations(self.product)
         self.assertEqual(
-            self.areas._get_ordered_leaf_locations().ids,
+            ordered_locations.ids,
             (
                 self.cardboxes_bin_1_location
                 | self.cardboxes_bin_2_location
@@ -49,23 +64,28 @@ class TestStockLocation(TestStorageTypeCommon):
                 | self.pallets_bin_1_location
                 | self.pallets_bin_2_location
                 | self.pallets_bin_3_location
+                | self.pallets_bin_4_location
                 | self.pallets_reserve_bin_1_location
                 | self.pallets_reserve_bin_2_location
                 | self.pallets_reserve_bin_3_location
+                | self.pallets_reserve_bin_4_location
             ).ids,
         )
         # Set the max_height on cardboxes storage type higher than the others
         self.pallets_location_storage_type.max_height = 1
         self.cardboxes_location_storage_type.max_height = 2
+        ordered_locations = sublocation.get_storage_locations(self.product)
         self.assertEqual(
-            self.areas._get_ordered_leaf_locations().ids,
+            ordered_locations.ids,
             (
                 self.pallets_bin_1_location
                 | self.pallets_bin_2_location
                 | self.pallets_bin_3_location
+                | self.pallets_bin_4_location
                 | self.pallets_reserve_bin_1_location
                 | self.pallets_reserve_bin_2_location
                 | self.pallets_reserve_bin_3_location
+                | self.pallets_reserve_bin_4_location
                 | self.cardboxes_bin_1_location
                 | self.cardboxes_bin_2_location
                 | self.cardboxes_bin_3_location

stock_storage_type/tests/test_storage_type.py
@@ -30,6 +30,9 @@ class TestStorageType(SavepointCase):
         cls.cardboxes_bin_3 = cls.env.ref(
             "stock_storage_type.stock_location_cardboxes_bin_3"
         )
+        cls.cardboxes_bin_4 = cls.env.ref(
+            "stock_storage_type.stock_location_cardboxes_bin_4"
+        )
 
     def test_location_allowed_storage_types(self):
         # As cardboxes location storage type is defined on parent stock

stock_storage_type/views/stock_location.xml
@@ -7,6 +7,7 @@
         <field name="arch" type="xml">
             <field name="removal_strategy_id" position="after">
                 <field name="pack_putaway_strategy" />
+                <field name="pack_putaway_sequence" />
                 <field
                     name="allowed_location_storage_type_ids"
                     widget="many2many_tags"

commit 91ea91c1b457ebc27a6722721a9fa4b5f1bbbf68

Fix Package storage type message computation.
The message was broken with the changes in commit 6b25b867c2f606d5f4220915ab9aae3b30c1f87f

stock_storage_type/models/stock_location.py
@@ -366,6 +366,7 @@ class StockLocation(models.Model):
         if self.pack_putaway_strategy == "none":
             locations = self
         else:
+            products = products or self.env["product.product"]
             locations = self._get_sorted_leaf_locations(products)
         return locations
 