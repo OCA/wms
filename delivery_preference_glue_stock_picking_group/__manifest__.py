4b3f3b30443349b27858a5c7aa871fd04fa35dc0
nguyen hoang hiep <hiepnh@trobz.com>
2021-09-27 12:03:55 +0000

    [MIG] delivery_preference_glue_stock_picking_group: Migration to 14.0

--- delivery_preference_glue_stock_picking_group/__manifest__.py
+++ delivery_preference_glue_stock_picking_group/__manifest__.py
@@ -3,7 +3,7 @@
 {
     "name": "Delivery Preference Glue Stock Picking Group",
     "summary": "Fix Delivery preferences module on grouping picking",
-    "version": "13.0.1.0.0",
+    "version": "14.0.1.0.0",
     "category": "Hidden",
     "author": "Camptocamp,Odoo Community Association (OCA)",
     "license": "AGPL-3",
