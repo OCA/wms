827cfdefb6dee1717cbdcda6d32309f4f344ad33
Simone Orsi <simahawk@gmail.com>
2021-10-15 11:02:32 +0200

    Add shopfloor_mobile_base_auth_user

---
+++ shopfloor_mobile_base_auth_user/__manifest__.py
@@ -0,0 +1,17 @@
+# Copyright 2021 Camptocamp SA (http://www.camptocamp.com)
+# @author Simone Orsi <simahawk@gmail.com>
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+
+{
+    "name": "Shopfloor Mobile Base auth via user auth",
+    "summary": "Provides authentication via standard user login",
+    "version": "14.0.1.0.0",
+    "development_status": "Alpha",
+    "category": "Inventory",
+    "website": "https://github.com/OCA/wms",
+    "author": "Camptocamp, Odoo Community Association (OCA)",
+    "maintainer": ["simahawk"],
+    "license": "AGPL-3",
+    "depends": ["shopfloor_mobile_base", "base_rest_auth_user_service"],
+    "data": ["templates/assets.xml"],
+}
