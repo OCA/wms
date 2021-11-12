1dae5f79fb29fc8a3c6ac3526a470b508600cc9a
Lindsay <lindsay.marion@acsone.eu>
2021-11-12 15:23:16 +0100

    [IMP] Add missing translations to shopfloor_mobile_base

--- shopfloor_mobile_base/static/wms/src/i18n/i18n.fr.js
+++ shopfloor_mobile_base/static/wms/src/i18n/i18n.fr.js
@@ -8,14 +8,31 @@ import {translation_registry} from "../services/translation_registry.js";
 
 const messages_fr = {
     screen: {
+        login: {
+            title: "Connexion",
+            api_key_placeholder: "Entrez votre API key",
+            api_key_label: "API key",
+            action: {
+                login: "Connexion",
+            },
+            error: {
+                api_key_invalid: "API Key non valide",
+            },
+        },
         home: {
-            title: "Barcode scanner",
+            title: "Home",
+            main_title: "Barcode scanner",
+            version: "Version:",
+            action: {
+                nuke_data_and_reload: "Recharger les données et rafraichir la page",
+            },
         },
         scan_anything: {
             name: "Scanner",
             title: "Scanner {what}",
         },
         settings: {
+            title: "Réglages",
             home: {
                 name: "Réglages",
                 title: "Réglages",
@@ -27,10 +44,11 @@ const messages_fr = {
             profile: {
                 name: "Profil",
                 title: "Choisissez un profil",
+                profile_updated: "Profil mis à jour",
             },
             fullscreen: {
-                enter: "Go fullscreen",
-                exit: "Exit fullscreen",
+                enter: "Plein écran",
+                exit: "Sortir du mode plein écran",
             },
         },
     },
@@ -48,9 +66,42 @@ const messages_fr = {
         confirm: {
             title: "Confirmer",
         },
+        cancel: {
+            title: "Annuler",
+        },
         ok: {
             title: "Ok",
         },
+        reset: {
+            title: "Reset",
+        },
+        reload_config: {
+            title: "Recharger la configuration et le menu",
+        },
+    },
+    app: {
+        profile_not_configured: "Profil non configuré. Choisissez-en un.",
+        profile_configure: "Configurer un profil",
+        loading: "Chargement...",
+        action: {
+            logout: "Déconnexion",
+        },
+        log_entry_link: "Voir le log",
+    },
+    misc: {
+        actions_popup: {
+            btn_action: "Action",
+        },
+    },
+    list: {
+        no_items: "Pas d'item dans la liste.",
+    },
+    select: {
+        no_items: "Pas d'item à sélectionner.",
+    },
+    order_lines_by: {
+        priority: "Ordonner par priorité",
+        location: "Ordonner par emplacement",
     },
 };
 
