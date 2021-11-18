b54fcbae096dd1ec1e87ead8878629d4d7ee2c51
Simone Orsi <simahawk@gmail.com>
2021-11-18 11:02:23 +0100

    sf_mobile_auth_api_key: ease access to groups

--- shopfloor_mobile_base_auth_api_key/models/shopfloor_app.py
+++ shopfloor_mobile_base_auth_api_key/models/shopfloor_app.py
@@ -21,3 +21,9 @@ class ShopfloorApp(models.Model):
 
     def _allowed_api_key_ids(self):
         return self.auth_api_key_group_ids.auth_api_key_ids.ids
+
+    def action_manage_api_key_groups(self):
+        xid = "auth_api_key_group.auth_api_key_group_act_window"
+        action = self.env["ir.actions.act_window"]._for_xml_id(xid)
+        action["domain"] = [("id", "in", self.auth_api_key_group_ids.ids)]
+        return action
