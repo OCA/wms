02c6271e0af81392baa401e123581cf3e88dd7bc
Simone Orsi <simahawk@gmail.com>
2021-11-26 14:14:48 +0100

    sf_mobile_base_auth_user: add specific demo data

--- shopfloor_mobile_base_auth_user/__manifest__.py
+++ shopfloor_mobile_base_auth_user/__manifest__.py
@@ -13,5 +13,8 @@
     "maintainer": ["simahawk"],
     "license": "AGPL-3",
     "depends": ["shopfloor_mobile_base", "base_rest_auth_user_service"],
-    "data": ["templates/assets.xml"],
+    "data": [
+        "demo/shopfloor_app_demo.xml",
+        "templates/assets.xml",
+    ],
 }
