e0a17c9f9b07f15e8273847f27fe57afc3f56f1c
Simone Orsi <simahawk@gmail.com>
2021-12-08 09:40:34 +0100

    shopfloor: 14.0.2.0.0

--- shopfloor/__manifest__.py
+++ shopfloor/__manifest__.py
@@ -6,7 +6,7 @@
 {
     "name": "Shopfloor",
     "summary": "manage warehouse operations with barcode scanners",
-    "version": "14.0.1.3.2",
+    "version": "14.0.2.0.0",
     "development_status": "Beta",
     "category": "Inventory",
     "website": "https://github.com/OCA/wms",
