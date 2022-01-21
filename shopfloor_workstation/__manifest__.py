248ea9c0d125303f5224234c77c431d4ac973bda
Simone Orsi <simahawk@gmail.com>
2022-01-21 11:30:08 +0100

    shopfloor_workstation: dev status -> Beta

--- shopfloor_workstation/__manifest__.py
+++ shopfloor_workstation/__manifest__.py
@@ -5,7 +5,7 @@
     "name": "Shopfloor Workstation",
     "summary": "Manage warehouse workstation with barcode scanners",
     "version": "14.0.1.1.0",
-    "development_status": "Alpha",
+    "development_status": "Beta",
     "category": "Inventory",
     "website": "https://github.com/OCA/wms",
     "author": "Camptocamp, Odoo Community Association (OCA)",
