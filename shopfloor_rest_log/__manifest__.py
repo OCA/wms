b8bc6e0619dfc09ccd16486b3f553aaa35b6e19f
Simone Orsi <simahawk@gmail.com>
2022-02-15 18:48:19 +0100

    shopfloor_rest_log 14.0.1.1.0

--- shopfloor_rest_log/__manifest__.py
+++ shopfloor_rest_log/__manifest__.py
@@ -5,7 +5,7 @@
 {
     "name": "Shopfloor REST log",
     "summary": "Integrate rest_log into Shopfloor app",
-    "version": "14.0.1.0.0",
+    "version": "14.0.1.1.0",
     "development_status": "Beta",
     "category": "Inventory",
     "website": "https://github.com/OCA/wms",
