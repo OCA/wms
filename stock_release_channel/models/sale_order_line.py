c51062cf2d224658363c779197a3fa2889b9f1d6
Iryna Vyshnevska <iryna.vyshnevska@camptocamp.com>
2022-02-16 14:27:37 +0200

    [FIX] stock_release_channel: increase user access rights
    So normal users can process a sale order.

--- stock_release_channel/models/sale_order_line.py
+++ stock_release_channel/models/sale_order_line.py
@@ -8,12 +8,13 @@ class SaleOrderLine(models.Model):
     _inherit = "sale.order.line"
 
     def _action_launch_stock_rule(self, previous_product_uom_qty=False):
+        # As the user who confirms the order might not have enough access rights to process
+        # the order (using orderpoints, assign channels, etc...) we use sudo here
+        self = self.sudo()
         result = super()._action_launch_stock_rule(
             previous_product_uom_qty=previous_product_uom_qty
         )
-        # As the user who confirms the order might not have access right to assign
-        # a release channel, we use sudo here, to avoid errors
-        pickings = self.move_ids.picking_id.sudo()
+        pickings = self.move_ids.picking_id
         # ensure we assign a channel on any picking OUT generated for the sale,
         # if moves are assigned to an existing transfer, we recompute the
         # channel
