90d9503d4bb38db5939aaf939f4e67eb153b76ad
Sébastien Alix <sebastien.alix@camptocamp.com>
2022-03-09 08:54:41 +0100

    [FIX] delivery_carrier_preference: fix dependency
    `weight` field isn't added on stock move tree view by
    `stock_available_to_promise_release` but by `sale_stock_available_to_promise_release`.
    
    As we also depend on `delivery` which already depends on `sale`, it is not an
    issue to depend on `sale_stock_available_to_promise_release`.

--- delivery_carrier_preference/__manifest__.py
+++ delivery_carrier_preference/__manifest__.py
@@ -13,7 +13,7 @@
     "depends": [
         "delivery",
         "product_total_weight_from_packaging",
-        "stock_available_to_promise_release",
+        "sale_stock_available_to_promise_release",
         "stock_picking_group_by_partner_by_carrier",
     ],
     "data": [
