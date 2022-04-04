b7cc6fa267542d694983cd512d94296af27e5656
Simone Orsi <simahawk@gmail.com>
2022-04-04 11:31:21 +0200

    shopfloor_mobile_base_auth_user: dev status = Beta

--- shopfloor_mobile_base_auth_user/__manifest__.py
+++ shopfloor_mobile_base_auth_user/__manifest__.py
@@ -6,7 +6,7 @@
     "name": "Shopfloor Mobile Base auth via user auth",
     "summary": "Provides authentication via standard user login",
     "version": "14.0.1.0.2",
-    "development_status": "Alpha",
+    "development_status": "Beta",
     "category": "Inventory",
     "website": "https://github.com/OCA/wms",
     "author": "Camptocamp, Odoo Community Association (OCA)",
