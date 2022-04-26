729ffc8b68ef130534c94f3b14791f8a6749e42c
JuMiSanAr <jumisan134@gmail.com>
2022-04-26 09:10:13 +0200

    shopfloor_mobile_base: add new translations

--- shopfloor_mobile_base/static/wms/src/i18n/i18n.de.js
+++ shopfloor_mobile_base/static/wms/src/i18n/i18n.de.js
@@ -8,15 +8,31 @@ import {translation_registry} from "../services/translation_registry.js";
 
 const messages_de = {
     screen: {
+        login: {
+            title: "Login",
+            action: {
+                login: "Login",
+            },
+            error: {
+                login_invalid: "Ungültige Anmeldeinformationen",
+            },
+        },
         home: {
-            title: "Home",
-            main_title: "Home",
+            title: "Startseite",
+            main_title: "Startseite",
+            version: "Version:",
+            action: {
+                nuke_data_and_reload:
+                    "Erzwingen das erneute Laden von Daten und aktualisieren",
+            },
         },
         scan_anything: {
             name: "Gescannt",
             title: "Gescannt {what}",
+            scan_placeholder: "Alles scannen",
         },
         settings: {
+            title: "Einstellungen",
             home: {
                 name: "Einstellungen",
                 title: "Einstellungen",
@@ -28,18 +44,40 @@ const messages_de = {
             profile: {
                 name: "Profil",
                 title: "Wähle Profil",
+                profile_updated: "Profil aktualisiert",
             },
             fullscreen: {
-                enter: "Go fullscreen",
-                exit: "Exit fullscreen",
+                enter: "Vollbildmodus",
+                exit: "Vollbild beenden",
             },
+        },
+    },
+    app: {
+        profile_not_configured:
+            "Profil noch nicht konfiguriert. Bitte wählen Sie eine aus.",
+        profile_configure: "Profil konfigurieren",
+        loading: "Wird geladen...",
+        action: {
+            logout: "Ausloggen",
+        },
+        nav: {
+            scenario: "Szenario:",
+            op_types: "Op Typen:",
+        },
+        log_entry_link: "Logeintrag ansehen / teilen",
+        running_env: {
+            prod: "Produktion",
+            integration: "Integration",
+            staging: "Staging",
+            test: "Test",
+            dev: "Entwicklung",
         },
     },
     language: {
         name: {
             English: "Englisch",
             French: "Französisch",
-            German: "Deutsche",
+            German: "Deutsch",
         },
     },
     btn: {
@@ -51,6 +89,15 @@ const messages_de = {
         },
         ok: {
             title: "Ok",
+        },
+        reset: {
+            title: "Zurücksetzen",
+        },
+        cancel: {
+            title: "Absagen",
+        },
+        reload_config: {
+            title: "Konfiguration und Menü neu laden",
         },
     },
 };
