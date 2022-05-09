fdbd11c6a248fcefd496d37131b58191f12fda5f
Simone Orsi <simahawk@gmail.com>
2022-05-09 21:45:32 +0200

    shopfloor_mobile_base_auth*: re-license as LGPL

--- shopfloor_mobile_base_auth_api_key/static/wms/src/login.js
+++ shopfloor_mobile_base_auth_api_key/static/wms/src/login.js
@@ -1,6 +1,6 @@
 /**
  * @author Simone Orsi <simone.orsi@camptocamp.com>
- * License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+ * License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl.html).
  */
 
 import {translation_registry} from "/shopfloor_mobile_base/static/wms/src/services/translation_registry.js";
