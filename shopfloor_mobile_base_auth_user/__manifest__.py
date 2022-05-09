c2ae0bd93b6fd560a3f4b15dfc22555b28ee3f55
Simone Orsi <simahawk@gmail.com>
2022-05-09 21:45:32 +0200

    shopfloor_mobile_base_auth*: re-license as LGPL

--- shopfloor_mobile_base_auth_user/__manifest__.py
+++ shopfloor_mobile_base_auth_user/__manifest__.py
@@ -1,6 +1,6 @@
 # Copyright 2021 Camptocamp SA (http://www.camptocamp.com)
 # @author Simone Orsi <simahawk@gmail.com>
-# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+# License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl.html).
 
 {
     "name": "Shopfloor Mobile Base auth via user auth",
@@ -11,7 +11,7 @@
     "website": "https://github.com/OCA/wms",
     "author": "Camptocamp, Odoo Community Association (OCA)",
     "maintainer": ["simahawk"],
-    "license": "AGPL-3",
+    "license": "LGPL-3",
     "depends": ["shopfloor_mobile_base", "base_rest_auth_user_service"],
     "demo": [
         "demo/shopfloor_app_demo.xml",
