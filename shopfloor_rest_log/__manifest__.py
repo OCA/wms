129cba4aaa6e24d9f387d9fb4fa943f3e2754869
Fernando La Chica <fernandolachica@gmail.com>
2022-10-01 16:18:24 +0200

    [MIG] shopfloor_rest_log: 15.0

--- shopfloor_rest_log/__manifest__.py
+++ shopfloor_rest_log/__manifest__.py
@@ -5,7 +5,7 @@
 {
     "name": "Shopfloor REST log",
     "summary": "Integrate rest_log into Shopfloor app",
-    "version": "14.0.1.1.0",
+    "version": "15.0.1.0.0",
     "development_status": "Beta",
     "category": "Inventory",
     "website": "https://github.com/OCA/wms",
--- shopfloor_rest_log/readme/CONTRIBUTORS.rst
+++ shopfloor_rest_log/readme/CONTRIBUTORS.rst
@@ -1,1 +1,2 @@
 * Simone Orsi <simahawk@gmail.com>
+* Fernando La Chica - GreenIce <fernandolachica@gmail.com>
