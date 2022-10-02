4541ba25a81cd028aab734e4fe42fe0812cacd6b
Fernando La Chica <fernandolachica@gmail.com>
2022-10-02 10:03:02 +0200

    [MIG] sale_stock_available_to_promise_release: 15.0

--- sale_stock_available_to_promise_release/__manifest__.py
+++ sale_stock_available_to_promise_release/__manifest__.py
@@ -3,7 +3,7 @@
 
 {
     "name": "Stock Available to Promise Release - Sale Integration",
-    "version": "14.0.1.1.1",
+    "version": "15.0.1.0.0",
     "summary": "Integration between Sales and Available to Promise Release",
     "author": "Camptocamp,Odoo Community Association (OCA)",
     "category": "Stock Management",
--- sale_stock_available_to_promise_release/readme/CONTRIBUTORS.rst
+++ sale_stock_available_to_promise_release/readme/CONTRIBUTORS.rst
@@ -1,2 +1,3 @@
 * Guewen Baconnier <guewen.baconnier@camptocamp.com>
 * Matthieu Méquignon <matthieu.mequignon@camptocamp.com>
+* Fernando La Chica - GreenIce <fernandolachica@gmail.com>
