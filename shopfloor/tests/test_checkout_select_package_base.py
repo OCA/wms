9d208cd2bde210bf8013a309e4bc1d5e79f00d53
Thierry Ducrest <thierry@ducrest.net>
2022-10-31 14:33:57 +0100

    shopfloor: checkout select orther line if not in pack
    On `select_line` when a product is scanned and it is not in a package,
    the other line not in a package are also selected, but their quantity
    done is not changed.

--- shopfloor/tests/test_checkout_select_package_base.py
+++ shopfloor/tests/test_checkout_select_package_base.py
@@ -47,15 +47,18 @@ class CheckoutSelectPackageMixin:
             response, selected_lines, message=message, packing_info=packing_info
         )
 
-    def _assert_selected(self, response, selected_lines, message=None, **kw):
+    def _assert_selected(
+        self, response, selected_lines, related_lines=None, message=None, **kw
+    ):
+        related_lines = related_lines or self.env["stock.move.line"]
         picking = selected_lines.mapped("picking_id")
         unselected_lines = picking.move_line_ids - selected_lines
-        for line in selected_lines:
+        for line in selected_lines - related_lines:
             self.assertEqual(
                 line.qty_done,
                 line.product_uom_qty,
                 "Scanned lines must have their qty done set to the reserved quantity",
             )
-        for line in unselected_lines:
+        for line in unselected_lines + related_lines:
             self.assertEqual(line.qty_done, 0)
         self._assert_selected_response(response, selected_lines, message=message, **kw)
