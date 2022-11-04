20d852cf3c7ca543fd383829ee6422a58a136521
Laurent Mignon (ACSONE) <laurent.mignon@acsone.eu>
2022-11-04 18:09:35 +0100

    [IMP] stock_available_to_promise_release: Makes the creation of backorder at release time configurable

--- stock_available_to_promise_release/models/stock_picking.py
+++ stock_available_to_promise_release/models/stock_picking.py
@@ -30,6 +30,15 @@ class StockPicking(models.Model):
     state_id = fields.Many2one(related="partner_id.state_id", store=True)
     city = fields.Char(related="partner_id.city", store=True)
 
+    set_printed_at_release = fields.Boolean(compute="_compute_set_printed_at_release")
+
+    @api.depends("move_ids")
+    def _compute_set_printed_at_release(self):
+        for picking in self:
+            picking.set_printed_at_release = not (
+                any(picking.move_ids.mapped("rule_id.no_backorder_at_release"))
+            )
+
     @api.depends("move_ids.need_release")
     def _compute_need_release(self):
         data = self.env["stock.move"].read_group(
@@ -153,14 +162,23 @@ class StockPicking(models.Model):
         self._after_release_set_expected_date()
 
     def _after_release_set_printed(self):
-        self.filtered(lambda p: not p.printed).printed = True
+        self.filtered(
+            lambda p: not p.printed and p.set_printed_at_release
+        ).printed = True
 
     def _after_release_set_expected_date(self):
         prep_time = self.env.company.stock_release_max_prep_time
         new_expected_date = fields.Datetime.add(
             fields.Datetime.now(), minutes=prep_time
         )
-        self.scheduled_date = new_expected_date
+        move_to_update = self.move_ids.filtered(lambda m: m.state == "assigned")
+        move_to_update_ids = move_to_update.ids
+        for origin_moves in move_to_update._get_chained_moves_iterator("move_dest_ids"):
+            move_to_update_ids += origin_moves.ids
+
+        self.env["stock.move"].browse(move_to_update_ids).write(
+            {"date": new_expected_date}
+        )
 
     def action_open_move_need_release(self):
         self.ensure_one()
