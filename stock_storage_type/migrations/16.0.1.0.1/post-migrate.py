d2c5c13046e6d34d5af07326bb2d88bbc2815916
Denis Roussel <denis.roussel@acsone.eu>
2022-11-09 11:09:36 +0100

    [IMP] stock_storage_type: Migration scripts

--- stock_storage_type/migrations/16.0.1.0.1/post-migrate.py
+++ stock_storage_type/migrations/16.0.1.0.1/post-migrate.py
@@ -45,16 +45,40 @@ def _move_location_storage_type(env):
     """
     # Update fields values
     query = """
-        UPDATE stock_storage_category_capacity
+        UPDATE stock_storage_category_capacity sscc
             SET allow_new_product = (
                 SELECT
-                    CASE
-                        WHEN do_not_mix_products = true THEN 'same'
+                    (CASE
+                        WHEN do_not_mix_lots = true THEN 'same_lot'
+                        WHEN do_not_mix_products = true
+                            AND do_not_mix_lots = false THEN 'same'
                         WHEN only_empty = True THEN 'empty'
                         ELSE 'mixed'
-                    END
+                    END)
                     FROM stock_location_storage_type
                     WHERE id = old_location_storage_type_id)
+            FROM stock_location_storage_type slst
+            WHERE slst.id = sscc.old_location_storage_type_id
+    """
+    openupgrade.logged_query(env.cr, query)
+
+
+def _update_location_sequence(env):
+    query = """
+        UPDATE stock_storage_location_sequence ssls
+            SET package_type_id = spt.id
+            FROM stock_package_type spt
+            WHERE spt.old_storage_type_id = ssls.package_storage_type_id
+    """
+    openupgrade.logged_query(env.cr, query)
+
+
+def _update_quant_package(env):
+    query = """
+        UPDATE stock_quant_package sqp
+            SET package_type_id = spt.id
+            FROM stock_package_type spt
+            WHERE spt.old_storage_type_id = sqp.package_storage_type_id
     """
     openupgrade.logged_query(env.cr, query)
 
@@ -63,3 +87,5 @@ def _move_location_storage_type(env):
 def migrate(env, version):
     _move_product_package_type(env)
     _move_location_storage_type(env)
+    _update_location_sequence(env)
+    _update_quant_package(env)
