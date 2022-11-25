dfe3b8f2a9d2f38f6366bf5364f337bb64250989
Laurent Mignon (ACSONE) <laurent.mignon@acsone.eu>
2022-11-25 17:12:43 +0100

    [ADD] stock_release_channel_auto_release: Add automatic release mode
    This addons define an automatic release mode on the release channels. By default
    release channels manage the release of the transfers in batch mode. This means
    that the transfers are released only when the user manually triggers the release
    process by clicking on the release button.
    
    When the automatic release mode is enabled, the transfers are released automatically
    when a new transfer is added to the channel or as soon a product becomes available.
    
    As for the batch mode, the automatic release process is only active on open channels.
    When is locked, the automatic release process is stopped. Once the channel is unlocked,
    the automatic release process is restarted and transfers into the channel are released
    if they are ready to be released (IOW if quantities are available for moves not
    yet released).

--- stock_release_channel/models/stock_release_channel.py
+++ stock_release_channel/models/stock_release_channel.py
@@ -6,6 +6,7 @@ import logging
 from pytz import timezone
 
 from odoo import _, api, exceptions, fields, models
+from odoo.osv.expression import NEGATIVE_TERM_OPERATORS
 from odoo.tools.safe_eval import (
     datetime as safe_datetime,
     dateutil as safe_dateutil,
@@ -64,8 +65,10 @@ class StockReleaseChannel(models.Model):
         help="Write Python code to filter out pickings.",
     )
     active = fields.Boolean(default=True)
-
-    auto_release = fields.Selection(
+    release_mode = fields.Selection(
+        [("batch", "Batch (Manual)")], required=True, default="batch"
+    )
+    batch_mode = fields.Selection(
         selection=[
             ("max", "Max"),
             ("group_commercial_partner", "Grouped by Commercial Partner"),
@@ -76,7 +79,7 @@ class StockReleaseChannel(models.Model):
         " in progress.\nGrouped by Commercial Partner: release all transfers for a"
         "commercial partner at once.",
     )
-    max_auto_release = fields.Integer(
+    max_batch_mode = fields.Integer(
         string="Max Transfers to release",
         default=10,
         help="When clicking on the package icon, it releases X transfers minus "
@@ -199,6 +202,7 @@ class StockReleaseChannel(models.Model):
     )
     is_release_allowed = fields.Boolean(
         compute="_compute_is_release_allowed",
+        search="_search_is_release_allowed",
         help="Technical field to check if the "
         "action 'Release Next Batch' is allowed.",
     )
@@ -227,6 +231,25 @@ class StockReleaseChannel(models.Model):
     def _compute_is_release_allowed(self):
         for rec in self:
             rec.is_release_allowed = rec.state == "open" and not rec.release_forbidden
+
+    @api.model
+    def _get_is_release_allowed_domain(self):
+        return [("state", "=", "open"), ("release_forbidden", "=", False)]
+
+    @api.model
+    def _get_is_release_not_allowed_domain(self):
+        return ["|", ("state", "!=", "open"), ("release_forbidden", "=", True)]
+
+    @api.model
+    def _search_is_release_allowed(self, operator, value):
+        if "in" in operator:
+            raise ValueError(f"Invalid operator {operator}")
+        negative_op = operator in NEGATIVE_TERM_OPERATORS
+        is_release_allowed = (value and not negative_op) or (not value and negative_op)
+        domain = self._get_is_release_allowed_domain()
+        if not is_release_allowed:
+            domain = self._get_is_release_not_allowed_domain()
+        return domain
 
     def _get_picking_to_unassign_domain(self):
         return [
@@ -652,17 +675,23 @@ class StockReleaseChannel(models.Model):
         return (-int(picking.priority or 1), picking.date_priority, picking.id)
 
     def _get_next_pickings(self):
-        return getattr(self, "_get_next_pickings_{}".format(self.auto_release))()
+        return getattr(self, "_get_next_pickings_{}".format(self.batch_mode))()
+
+    def _get_pickings_to_release(self):
+        """Get the pickings to release."""
+        domain = self._field_picking_domains()["count_picking_release_ready"]
+        domain += [("release_channel_id", "in", self.ids)]
+        return self.env["stock.picking"].search(domain)
 
     def _get_next_pickings_max(self):
-        if not self.max_auto_release:
+        if not self.max_batch_mode:
             raise exceptions.UserError(_("No Max transfers to release is configured."))
 
         waiting_domain = self._field_picking_domains()["count_picking_waiting"]
         waiting_domain += [("release_channel_id", "=", self.id)]
         released_in_progress = self.env["stock.picking"].search_count(waiting_domain)
 
-        release_limit = max(self.max_auto_release - released_in_progress, 0)
+        release_limit = max(self.max_batch_mode - released_in_progress, 0)
         if not release_limit:
             raise exceptions.UserError(
                 _(
@@ -670,9 +699,7 @@ class StockReleaseChannel(models.Model):
                     " progress is already at the maximum."
                 )
             )
-        domain = self._field_picking_domains()["count_picking_release_ready"]
-        domain += [("release_channel_id", "=", self.id)]
-        next_pickings = self.env["stock.picking"].search(domain)
+        next_pickings = self._get_pickings_to_release()
         # We have to use a python sort and not a order + limit on the search
         # because "date_priority" is computed and not stored. If needed, we
         # should evaluate making it a stored field in the module
@@ -680,15 +707,11 @@ class StockReleaseChannel(models.Model):
         return next_pickings.sorted(self._pickings_sort_key)[:release_limit]
 
     def _get_next_pickings_group_commercial_partner(self):
-        domain = self._field_picking_domains()["count_picking_release_ready"]
-        domain += [("release_channel_id", "=", self.id)]
         # We have to use a python sort and not a order + limit on the search
         # because "date_priority" is computed and not stored. If needed, we
         # should evaluate making it a stored field in the module
         # "stock_available_to_promise_release".
-        next_pickings = (
-            self.env["stock.picking"].search(domain).sorted(self._pickings_sort_key)
-        )
+        next_pickings = self._get_pickings_to_release().sorted(self._pickings_sort_key)
         if not next_pickings:
             return self.env["stock.picking"].browse()
         first_picking = next_pickings[0]
---
+++ stock_release_channel_auto_release/readme/DESCRIPTION.rst
@@ -0,0 +1,13 @@
+This addons define an automatic release mode on the release channels. By default
+release channels manage the release of the transfers in batch mode. This means
+that the transfers are released only when the user manually triggers the release
+process by clicking on the release button.
+
+When the automatic release mode is enabled, the transfers are released automatically
+when a new transfer is added to the channel or as soon a product becomes available.
+
+As for the batch mode, the automatic release process is only active on open channels.
+When is locked, the automatic release process is stopped. Once the channel is unlocked,
+the automatic release process is restarted and transfers into the channel are released
+if they are ready to be released (IOW if quantities are available for moves not
+yet released).
