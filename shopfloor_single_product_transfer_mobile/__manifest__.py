7f37d77552029d5c07b716ad66c78d80c3919465
JuMiSanAr <jumisan134@gmail.com>
2022-12-01 07:41:47 +0100

    add shopfloor_single_product_transfer_mobile

---
+++ shopfloor_single_product_transfer_mobile/__manifest__.py
@@ -0,0 +1,16 @@
+# Copyright 2022 Camptocamp SA (http://www.camptocamp.com)
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
+{
+    "name": "Shopfloor single product transfer mobile",
+    "summary": "Mobile frontend for single product transfer scenario",
+    "version": "14.0.1.0.0",
+    "development_status": "Beta",
+    "depends": ["shopfloor_mobile_base", "shopfloor_single_product_transfer"],
+    "author": "Camptocamp, Odoo Community Association (OCA)",
+    "maintainers": ["JuMiSanAr"],
+    "website": "https://github.com/OCA/wms",
+    "category": "Warehouse Management",
+    "license": "AGPL-3",
+    "installable": True,
+    "data": ["templates/assets.xml"],
+}
