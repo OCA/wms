21f1ae02250f32510238e6bf60c12c67c89dde24
Jacques-Etienne Baudoux <je@bcim.be>
2022-12-07 17:09:20 +0100

    stock_release_channel: assign channel
    When the move is not generated by a SO, also assign the release channel

--- stock_release_channel/models/stock_move.py
+++ stock_release_channel/models/stock_move.py
@@ -15,3 +15,10 @@ class StockMove(models.Model):
         res = super().release_available_to_promise()
         self.picking_id.assign_release_channel()
         return res
+
+    def _action_confirm(self, merge=True, merge_into=False):
+        moves = super()._action_confirm(merge=merge, merge_into=merge_into)
+        pickings = moves.filtered("need_release").picking_id
+        if pickings:
+            pickings._delay_assign_release_channel()
+        return moves
