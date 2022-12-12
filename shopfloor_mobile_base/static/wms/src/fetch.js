d5cc98e1d8fc02d0c0b9b154e909f4276f9ab296
JuMiSanAr <jumisan134@gmail.com>
2022-12-12 16:03:46 +0100

    sf_mobile_base: trigger event in fetch to handle 401 unauthorized

--- shopfloor_mobile_base/static/wms/src/fetch.js
+++ shopfloor_mobile_base/static/wms/src/fetch.js
@@ -10,7 +10,37 @@
 
 // Credit https://stackoverflow.com/questions/43792026
 
-window.standardFetch = fetch;
+window._standardFetch = fetch;
+
+window.standardFetch = function () {
+    var fetchUnauthorized = new Event("fetchUnauthorized", {
+        view: document,
+        bubbles: true,
+        cancelable: false,
+    });
+
+    // Pass the supplied arguments to the real fetch function
+    var fetchCall = window._standardFetch.apply(this, arguments);
+
+    const _check_unauthorized = (data) => {
+        if (data.status === 401) {
+            fetchUnauthorized.data = {
+                body: data.body,
+                url: data.url,
+                status: data.status,
+                status_text: data.statusText,
+            };
+            document.dispatchEvent(fetchUnauthorized);
+        }
+    };
+
+    fetchCall.then(function (data) {
+        // Check if the user is unauthorized
+        // and trigger the fetchUnauthorized event
+        _check_unauthorized(data);
+    });
+    return fetchCall;
+};
 
 /**
  * Patch `fetch` to trigger events on start and end request.
