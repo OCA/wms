19eeaf93b56efd0fd4209c6f589bab782ccc63eb
Simone Orsi <simahawk@gmail.com>
2023-01-26 14:26:43 +0100

    shopfloor: checkout allow pack all lines at once
    On the select_line screen,
    you can now skip the selection and scan a delivery package.
    
    When this happens, the system asks for confirmation.
    
    If you scan again the delivery package,
    all lines will be selected and packed at the same time in the same package.

--- shopfloor/tests/test_checkout_no_package.py
+++ shopfloor/tests/test_checkout_no_package.py
@@ -57,10 +57,7 @@ class CheckoutNoPackageCase(CheckoutCommonCase, CheckoutSelectPackageMixin):
             response,
             # go pack to the screen to select lines to put in packages
             next_state="select_line",
-            data={
-                "picking": self._stock_picking_data(self.picking),
-                "group_lines_by_location": True,
-            },
+            data=self._data_for_select_line(self.picking),
             message={
                 "message_type": "success",
                 "body": "Product(s) processed as raw product(s)",
