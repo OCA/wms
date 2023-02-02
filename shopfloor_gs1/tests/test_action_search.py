d28b7b7666ed8b88d9f762383b5c2ce36e35e4ec
Simone Orsi <simahawk@gmail.com>
2023-02-02 18:12:27 +0100

    shopfloor_gs1: refactor mapping handling

---
+++ shopfloor_gs1/tests/test_action_search.py
@@ -0,0 +1,74 @@
+# Copyright 2022 Camptocamp SA (http://www.camptocamp.com)
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+from odoo.addons.shopfloor.tests.test_actions_search import TestSearchBaseCase
+
+PROD_BARCODE = "09506000117843"
+MANUF_CODE = "K000075"
+DATE = "141231"
+LOT1 = "1234AB"
+LOT2 = "1234AC"
+GS1_GTIN_BARCODE_1 = f"(01){PROD_BARCODE}(11){DATE}(10){LOT1}"
+GS1_GTIN_BARCODE_2 = f"(01){PROD_BARCODE}(11){DATE}(10){LOT2}"
+GS1_MANUF_BARCODE = f"(240){MANUF_CODE}(11){DATE}(10){LOT1}"
+
+
+class TestFind(TestSearchBaseCase):
+    @classmethod
+    def setUpClassBaseData(cls):
+        super().setUpClassBaseData()
+        cls.product_a.barcode = PROD_BARCODE
+
+    def test_find_picking(self):
+        ptype = self.env.ref("shopfloor.picking_type_single_pallet_transfer_demo")
+        rec = self._create_picking(picking_type=ptype)
+        res = self.search.find(rec.name, types=("picking",))
+        self.assertEqual(res.record, rec)
+
+    def test_find_location(self):
+        rec = self.customer_location
+        barcode = GS1_GTIN_BARCODE_1 + "(254)" + rec.name
+        res = self.search.find(barcode, types=("location",))
+        self.assertEqual(res.record, rec)
+        res = self.search.find(rec.name, types=("location",))
+        self.assertEqual(res.record, rec)
+
+    def test_find_package(self):
+        rec = self.env["stock.quant.package"].sudo().create({"name": "ABC1234"})
+        res = self.search.find(rec.name, types=("package",))
+        self.assertEqual(res.record, rec)
+
+    def test_find_product(self):
+        rec = self.product_a
+        res = self.search.find(GS1_GTIN_BARCODE_1, types=("product",))
+        self.assertEqual(res.record, rec)
+        rec.barcode = MANUF_CODE
+        res = self.search.find(GS1_MANUF_BARCODE, types=("product",))
+        self.assertEqual(res.record, rec)
+
+    def test_find_lot(self):
+        rec = (
+            self.env["stock.production.lot"]
+            .sudo()
+            .create(
+                {
+                    "product_id": self.product_a.id,
+                    "company_id": self.env.company.id,
+                    "name": LOT1,
+                }
+            )
+        )
+        res = self.search.find(
+            GS1_GTIN_BARCODE_1,
+            types=("lot",),
+            handler_kw=dict(lot=dict(products=self.product_a)),
+        )
+        self.assertEqual(res.record, rec)
+
+    def test_find_generic_packaging(self):
+        rec = (
+            self.env["product.packaging"]
+            .sudo()
+            .create({"name": "TEST PKG", "barcode": "1234"})
+        )
+        res = self.search.find(rec.barcode, types=("delivery_packaging",))
+        self.assertEqual(res.record, rec)
--- shopfloor_gs1/tests/test_utils.py
+++ shopfloor_gs1/tests/test_utils.py
@@ -9,23 +9,19 @@ from ..utils import GS1Barcode
 
 
 class TestUtils(BaseCase):
-
     def test_parse1(self):
         code = "(01)09506000117843(11)141231(10)1234AB"
         res = GS1Barcode.parse(code)
         self.assertEqual(len(res), 3, res)
-        item = [x for x in res if x.type == "product"][0]
-        self.assertEqual(item.ai, "01")
+        item = [x for x in res if x.ai == "01"][0]
         self.assertEqual(item.code, code)
         self.assertEqual(item.value, "09506000117843")
         self.assertEqual(item.raw_value, "09506000117843")
-        item = [x for x in res if x.type == "production_date"][0]
-        self.assertEqual(item.ai, "11")
+        item = [x for x in res if x.ai == "11"][0]
         self.assertEqual(item.code, code)
         self.assertEqual(item.value, datetime.date(2014, 12, 31))
         self.assertEqual(item.raw_value, "141231")
-        item = [x for x in res if x.type == "lot"][0]
-        self.assertEqual(item.ai, "10")
+        item = [x for x in res if x.ai == "10"][0]
         self.assertEqual(item.code, code)
         self.assertEqual(item.value, "1234AB")
         self.assertEqual(item.raw_value, "1234AB")
@@ -34,21 +30,29 @@ class TestUtils(BaseCase):
         code = "(01)09506000117843(11)141231(10)1234AB"
         res = GS1Barcode.parse(code, ai_whitelist=("01",))
         self.assertEqual(len(res), 1, res)
-        item = [x for x in res if x.type == "product"][0]
-        self.assertEqual(item.ai, "01")
+        item = [x for x in res if x.ai == "01"][0]
         self.assertEqual(item.code, code)
         self.assertEqual(item.value, "09506000117843")
         self.assertEqual(item.raw_value, "09506000117843")
 
+    def test_parse3(self):
+        code = "(240)K000075(11)230201(10)0000392"
+        res = GS1Barcode.parse(code, ai_whitelist=("240",))
+        self.assertEqual(len(res), 1, res)
+        item = [x for x in res if x.ai == "240"][0]
+        self.assertEqual(item.code, code)
+        self.assertEqual(item.value, "K000075")
+        self.assertEqual(item.raw_value, "K000075")
+
     def test_parse_order(self):
         """Ensure ai whitelist order is respected"""
         code = "(01)09506000117843(11)141231(10)1234AB"
-        res = GS1Barcode.parse(code, ai_whitelist=("10","01", "11"))
+        res = GS1Barcode.parse(code, ai_whitelist=("10", "01", "11"))
         self.assertEqual(len(res), 3, res)
         self.assertEqual(res[0].ai, "10")
         self.assertEqual(res[1].ai, "01")
         self.assertEqual(res[2].ai, "11")
-        res = GS1Barcode.parse(code, ai_whitelist=("01","11", "10"))
+        res = GS1Barcode.parse(code, ai_whitelist=("01", "11", "10"))
         self.assertEqual(len(res), 3, res)
         self.assertEqual(res[0].ai, "01")
         self.assertEqual(res[1].ai, "11")
