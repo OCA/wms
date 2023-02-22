00fec87b76c02410dfe6ae301524a6bad0a6cb57
sbejaoui <souheil.bejaoui@acsone.eu>
2023-02-22 12:56:48 +0100

    [ADD] stock_release_channel_shipment_advice_toursolver
    Use TourSolver to plan shipment advices for ready and released pickings.

---
+++ stock_release_channel_shipment_advice_toursolver/models/toursolver_task.py
@@ -0,0 +1,19 @@
+# Copyright 2023 ACSONE SA/NV
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
+
+from odoo import fields, models
+
+
+class ToursolverTask(models.Model):
+
+    _inherit = "toursolver.task"
+
+    release_channel_id = fields.Many2one(
+        comodel_name="stock.release.channel", string="Release Channel", readonly=True
+    )
+
+    def _get_new_shipment_advice_planer(self, resource, pickings_to_plan):
+        planner = super()._get_new_shipment_advice_planer(resource, pickings_to_plan)
+        planner.release_channel_id = self.release_channel_id
+        planner.picking_to_plan_ids = pickings_to_plan
+        return planner
