fc0f9e131d854aafc28e528768a620ece3aa9510
Ruchir Shukla <ruchir@bizzappdev.com>
2023-02-27 18:35:10 +0530

    [MIG][15.0]stock_dynamic_routing_reserve_rule:Migration to 15.0

--- stock_dynamic_routing_reserve_rule/__manifest__.py
+++ stock_dynamic_routing_reserve_rule/__manifest__.py
@@ -5,7 +5,7 @@
     "author": "Camptocamp, Odoo Community Association (OCA)",
     "website": "https://github.com/OCA/wms",
     "category": "Warehouse Management",
-    "version": "14.0.1.0.1",
+    "version": "15.0.0.1.0",
     "license": "AGPL-3",
     "depends": [
         "stock_dynamic_routing",  # OCA/wms
