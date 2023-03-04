c6be3446ed99995f6b642449e669ce3e35966eb1
Ruchir Shukla <ruchir@bizzappdev.com>
2023-03-04 21:39:42 +0530

    [MIG][15.0]stock_dynamic_routing_checkout_sync: Migration to 15.0

--- stock_dynamic_routing_checkout_sync/__manifest__.py
+++ stock_dynamic_routing_checkout_sync/__manifest__.py
@@ -6,7 +6,7 @@
     "author": "Camptocamp, Odoo Community Association (OCA)",
     "website": "https://github.com/OCA/wms",
     "category": "Warehouse Management",
-    "version": "14.0.1.0.0",
+    "version": "15.0.1.0.0",
     "license": "AGPL-3",
     "depends": ["stock_dynamic_routing", "stock_checkout_sync"],
     "demo": [],
