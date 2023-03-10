514fa11a238b83183444c353c565f0274fe5169d
Simone Orsi <simahawk@gmail.com>
2023-03-10 09:08:33 +0100

    stock_storage_type: make not installable

--- stock_storage_type/__manifest__.py
+++ stock_storage_type/__manifest__.py
@@ -11,7 +11,7 @@
     "author": "Camptocamp, BCIM, Odoo Community Association (OCA)",
     "license": "AGPL-3",
     "application": False,
-    "installable": True,
+    "installable": False,
     "depends": [
         "stock_putaway_hook",
         "base_m2m_custom_field",
