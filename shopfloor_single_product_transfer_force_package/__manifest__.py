2c214eda087e8b9206950a478caa01378a09b2b4
JuMiSanAr <jumisan134@gmail.com>
2023-05-25 13:38:49 +0200

    add shopfloor_single_product_transfer_force_package

---
+++ shopfloor_single_product_transfer_force_package/__manifest__.py
@@ -0,0 +1,18 @@
+# Copyright 2023 Camptocamp SA
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html)
+
+{
+    "name": "Shopfloor Single Product Transfer Force Select Package",
+    "summary": "Force to select package if location already contains packages.",
+    "version": "14.0.1.0.0",
+    "category": "Inventory",
+    "website": "https://github.com/OCA/wms",
+    "author": "Camptocamp, Odoo Community Association (OCA)",
+    "maintainers": ["JuMiSanAr"],
+    "license": "AGPL-3",
+    "depends": [
+        "shopfloor",
+        "shopfloor_single_product_transfer",
+        "stock_location_package_restriction",
+    ],
+}
