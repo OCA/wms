cb2814aabfe18cd6df99ab24c5e2feae0189bdb6
sbejaoui <souheil.bejaoui@acsone.eu>
2023-06-05 17:18:57 +0200

    [FIX] stock_release_channel_shipment_advice_toursolver: fix method name

--- stock_release_channel_shipment_advice_toursolver/models/toursolver_task.py
+++ stock_release_channel_shipment_advice_toursolver/models/toursolver_task.py
@@ -12,8 +12,10 @@ class ToursolverTask(models.Model):
         comodel_name="stock.release.channel", string="Release Channel", readonly=True
     )
 
-    def _get_new_shipment_advice_planer(self, resource, pickings_to_plan):
-        planner = super()._get_new_shipment_advice_planer(resource, pickings_to_plan)
+    def _toursolver_new_shipment_advice_planer(self, resource, pickings_to_plan):
+        planner = super()._toursolver_new_shipment_advice_planer(
+            resource, pickings_to_plan
+        )
         planner.release_channel_id = self.release_channel_id
         planner.picking_to_plan_ids = pickings_to_plan
         return planner
