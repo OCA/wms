4fe98132e7983b77092d05ca5d8517e0de7cc71c
Thierry Ducrest <thierry.ducrest@camptocamp.com>
2023-06-07 05:18:46 +0200

    Add shopfloor_reception_packaging_dimension_mobile

---
+++ shopfloor_reception_packaging_dimension_mobile/__manifest__.py
@@ -0,0 +1,19 @@
+# Copyright 2023 Camptocamp SA
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl)
+
+{
+    "name": "Shopfloor Reception Packaging Dimension Mobile",
+    "summary": "Frontend for the packaging dimension on reception scenario",
+    "version": "14.0.1.0.0",
+    "development_status": "Alpha",
+    "category": "Inventory",
+    "website": "https://github.com/OCA/wms",
+    "author": "Camptocamp, Odoo Community Association (OCA)",
+    "maintainers": ["TDu"],
+    "license": "AGPL-3",
+    "depends": [
+        "shopfloor_reception_mobile",
+        "shopfloor_reception_packaging_dimension",
+    ],
+    "data": ["templates/assets.xml"],
+}
