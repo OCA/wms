ccea857bf5234abcdcf0357b06211ccd7b4207e7
Michael Tietz <mtietz@mt-software.de>
2023-07-19 04:47:53 +0200

    [IMP] stock_warehouse_flow_release: Release also split moves

--- stock_warehouse_flow_release/models/stock_move.py
+++ stock_warehouse_flow_release/models/stock_move.py
@@ -1,4 +1,5 @@
 # Copyright 2022 Camptocamp SA
+# Copyright 2023 Michael Tietz (MT Software) <mtietz@mt-software.de>
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl)
 
 from odoo import models
@@ -17,6 +18,11 @@ class StockMove(models.Model):
     def _before_release(self):
         # Apply the flow when releasing the move
         super()._before_release()
+        FLOW = self.env["stock.warehouse.flow"]
+        move_ids_to_release = []
         for move in self:
-            flow = self.env["stock.warehouse.flow"]._search_for_move(move)
-            flow._apply_on_move(move)
+            _move_ids_to_release = FLOW._search_and_apply_for_move(move).ids
+            _move_ids_to_release.remove(move.id)
+            move_ids_to_release += _move_ids_to_release
+        if move_ids_to_release:
+            self.browse(move_ids_to_release).release_available_to_promise()
