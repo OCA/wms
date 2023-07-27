a5e711ffbe2ceceaa8c697aa8870ef4af105576b
sbejaoui <souheil.bejaoui@acsone.eu>
2023-07-27 17:21:45 +0200

    [REF] - refactor release channel propagate for better inheritence

--- stock_release_channel_propagate_channel_picking/models/stock_move.py
+++ stock_release_channel_propagate_channel_picking/models/stock_move.py
@@ -10,11 +10,16 @@ class StockMove(models.Model):
     _inherit = "stock.move"
 
     def _after_release_update_chain(self):
+        self._propagate_release_channel()
+        return super()._after_release_update_chain()
+
+    def _propagate_release_channel(self):
         """
         We update the channel on corresponding pickings
         We update it per channel
         """
         pickings_per_channel = defaultdict(set)
+        pickings_updated = self.env["stock.picking"]
         for move in self:
             if not move.picking_type_id.propagate_to_pickings_chain:
                 continue
@@ -25,7 +30,7 @@ class StockMove(models.Model):
                 )
                 moves = moves.mapped("move_orig_ids")
         for channel, picking_ids in pickings_per_channel.items():
-            self.env["stock.picking"].browse(picking_ids).write(
-                {"release_channel_id": channel.id}
-            )
-        return super()._after_release_update_chain()
+            pickings = self.env["stock.picking"].browse(picking_ids)
+            pickings_updated |= pickings
+            pickings.write({"release_channel_id": channel.id})
+        return pickings_updated
