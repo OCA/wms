7a359fd84880d653ac4e86743f787672fa8f2d9e
sbejaoui <souheil.bejaoui@acsone.eu>
2023-08-07 18:18:22 +0200

    [16.0][MIG] - shopfloor_rest_log

--- shopfloor_rest_log/__manifest__.py
+++ shopfloor_rest_log/__manifest__.py
@@ -5,7 +5,7 @@
 {
     "name": "Shopfloor REST log",
     "summary": "Integrate rest_log into Shopfloor app",
-    "version": "14.0.1.2.1",
+    "version": "16.0.1.0.0",
     "development_status": "Beta",
     "category": "Inventory",
     "website": "https://github.com/OCA/wms",
