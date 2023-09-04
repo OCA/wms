02cbc51a857e86e46b9dc7d3031b9c40b65142ec
sbejaoui <souheil.bejaoui@acsone.eu>
2023-09-04 15:44:31 +0200

    [IMP] - stock_release_channel_delivery: better handle carrier filter

---
+++ stock_release_channel_delivery/models/stock_picking.py
@@ -0,0 +1,21 @@
+# Copyright 2023 ACSONE SA/NV
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
+
+from odoo import models
+
+
+class StockPicking(models.Model):
+
+    _inherit = "stock.picking"
+
+    def _get_release_channel_possible_candidate_domain(self):
+        domain = super()._get_release_channel_possible_candidate_domain()
+        if self.carrier_id:
+            domain.extend(
+                [
+                    "|",
+                    ("carrier_id", "=", False),
+                    ("carrier_id", "=", self.carrier_id.id),
+                ]
+            )
+        return domain
--- stock_release_channel_delivery/models/stock_release_channel.py
+++ stock_release_channel_delivery/models/stock_release_channel.py
@@ -1,8 +1,7 @@
 # Copyright 2023 ACSONE SA/NV
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
 
-from odoo import api, fields, models
-from odoo.tools.safe_eval import safe_eval
+from odoo import fields, models
 
 
 class StockReleaseChannel(models.Model):
@@ -14,13 +13,3 @@ class StockReleaseChannel(models.Model):
         help="The carrier linked to the release channel.",
         string="Transporter",
     )
-
-    @api.onchange("carrier_id")
-    def _onchange_carrier_id(self):
-        if self.carrier_id:
-            carrier_term = ("carrier_id.id", "=", self.carrier_id.id)
-            safe_dom = safe_eval(self.rule_domain)
-            if carrier_term not in safe_dom:
-                # add the term in front of the domain if not present (=> AND)
-                safe_dom.insert(0, carrier_term)
-                self.rule_domain = str(safe_dom)
