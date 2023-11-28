0f2e0a9786b571a4addf0de2bd24baacffd706d6
Francois Poizat <francois.poizat@gmail.com>
2023-11-28 13:57:41 +0100

    wms_connector: ADD wms_connector_exported and is_wms_exportable fields
    This allows to display the export to wms contextually and display wms_connector export state in tree view

--- wms_connector/models/stock_picking.py
+++ wms_connector/models/stock_picking.py
@@ -1,9 +1,22 @@
 # Copyright 2023 Akretion
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
 
-from odoo import models
+from odoo import api, fields, models
 
 
 class StockPicking(models.Model):
     _inherit = ["synchronize.exportable.mixin", "stock.picking"]
     _name = "stock.picking"
+
+    wms_connector_exported = fields.Boolean(String="Exported to WMS", compute="_compute_wms_exported", readonly=True, store=True)
+    is_wms_exportable = fields.Boolean(compute="_compute_is_wms_exportable", readonly=True, store=True)
+
+    @api.depends("wms_export_attachment")
+    def _compute_wms_exported(self):
+        for rec in self:
+            rec.wms_connector_exported = bool(rec.wms_export_attachment)
+
+    @api.depends("picking_type_id.warehouse_id.active_wms_sync")
+    def _compute_is_wms_exportable(self):
+        for rec in self:
+            rec.is_wms_exportable = rec.picking_type_id.warehouse_id.active_wms_sync
