a28fd670bc8fbbaca3c8d3ada81d1e9f5a513e6d
Michael Tietz <mtietz@mt-software.de>
2024-03-06 10:27:50 +0100

    stock_release_channel: Add missing Copyright MT Software

--- stock_release_channel/models/stock_release_channel.py
+++ stock_release_channel/models/stock_release_channel.py
@@ -1,4 +1,5 @@
 # Copyright 2020 Camptocamp
+# Copyright 2023 Michael Tietz (MT Software) <mtietz@mt-software.de>
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html)
 
 import logging
