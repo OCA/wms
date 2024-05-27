d936e963844956cdddf9063ab8b2242062043b91
Sébastien Alix <sebastien.alix@camptocamp.com>
2024-05-27 12:15:28 +0200

    sale_stock_available_to_promise_release_block: add Unblock Release wizard
    This new wizard allows to give more options to the user regarding the
    unblocking process, like the scheduled date to set on unblocked moves,
    and re-assign automatically a stock operation on them (so they could be
    grouped together in the same transfer).

--- sale_stock_available_to_promise_release_block/__manifest__.py
+++ sale_stock_available_to_promise_release_block/__manifest__.py
@@ -5,7 +5,7 @@
 {
     "name": "Stock Available to Promise Release - Block from Sales",
     "summary": """Block release of deliveries from sales orders.""",
-    "version": "16.0.1.0.0",
+    "version": "16.0.1.1.0",
     "license": "AGPL-3",
     "author": "Camptcamp, ACSONE SA/NV, BCIM, Odoo Community Association (OCA)",
     "website": "https://github.com/OCA/wms",
@@ -15,8 +15,12 @@
         "stock_available_to_promise_release_block",
     ],
     "data": [
+        "security/ir.model.access.csv",
         "views/sale_order.xml",
         "views/sale_order_line.xml",
+        "views/stock_move.xml",
+        "wizards/unblock_release.xml",
     ],
     "installable": True,
+    "post_init_hook": "post_init_hook",
 }
---
+++ sale_stock_available_to_promise_release_block/wizards/__init__.py
@@ -0,0 +1,1 @@
+from . import unblock_release
