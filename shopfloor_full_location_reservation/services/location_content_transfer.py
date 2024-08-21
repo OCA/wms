4f19f3f00acdf8d3c4cde04e6e28b502046bbbee
Denis Roussel <denis.roussel@acsone.eu>
2024-08-21 14:41:56 +0200

    [IMP] shopfloor_full_location_reservation: Filter lines with one location
    As after full reservation, returned lines could concern several source locations,
    we need to filter them one more time for the first location

--- shopfloor_full_location_reservation/services/location_content_transfer.py
+++ shopfloor_full_location_reservation/services/location_content_transfer.py
@@ -8,14 +8,18 @@ class LocationContentTransfer(Component):
     _inherit = "shopfloor.location.content.transfer"
 
     def _select_move_lines_first_location(self, move_lines):
-        move_lines = super()._select_move_lines_first_location(move_lines)
         if not self.work.menu.full_location_reservation:
-            return move_lines
+            return super()._select_move_lines_first_location(move_lines)
 
         if any(move_lines.move_id.mapped("is_full_location_reservation")):
-            return move_lines
+            return super()._select_move_lines_first_location(move_lines)
 
+        move_lines = super()._select_move_lines_first_location(move_lines)
         move_lines |= move_lines._full_location_reservation().move_line_ids
+
+        # As lines should concern only one source location and as lines
+        # can have been split, we need to retrieve only those with first location
+        move_lines = super()._select_move_lines_first_location(move_lines.exists())
         return move_lines.exists()
 
     def _move_lines_cancel_work(self, move_lines):
