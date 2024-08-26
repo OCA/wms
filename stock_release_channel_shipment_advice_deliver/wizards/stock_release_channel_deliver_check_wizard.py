ca32e43d22c589755fb30205e263e9f1b27ccaca
Denis Roussel <denis.roussel@acsone.eu>
2024-08-26 09:32:22 +0200

    [IMP] stock_release_channel_shipment_advice_deliver: Allows to display pickings in confirm wizard
    In order to let user aware of remaining pickings when delivering,
    display them in the delivering wizard

--- stock_release_channel_shipment_advice_deliver/wizards/stock_release_channel_deliver_check_wizard.py
+++ stock_release_channel_shipment_advice_deliver/wizards/stock_release_channel_deliver_check_wizard.py
@@ -1,7 +1,7 @@
 # Copyright 2023 ACSONE SA/NV
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
 
-from odoo import fields, models
+from odoo import api, fields, models
 
 
 class StockReleaseChannelDeliverCheckWizard(models.TransientModel):
@@ -10,6 +10,25 @@ class StockReleaseChannelDeliverCheckWizard(models.TransientModel):
     _description = "stock release channel deliver check wizard"
 
     release_channel_id = fields.Many2one("stock.release.channel")
+    allowed_to_unrelease_picking_ids = fields.One2many(
+        comodel_name="stock.picking",
+        compute="_compute_allowed_to_unrelease_picking_ids",
+        string="Current pickings that will be unreleased",
+    )
+
+    @api.depends("release_channel_id")
+    def _compute_allowed_to_unrelease_picking_ids(self) -> None:
+        """
+        Compute the pickings that are allowed to be unreleased.
+        This can show the user the possible ones that could be delivered.
+        """
+        for wizard in self:
+            moves_to_unrelease = (
+                wizard.release_channel_id._shipping_moves_to_unrelease()
+            )
+            wizard.allowed_to_unrelease_picking_ids = moves_to_unrelease.filtered(
+                "unrelease_allowed"
+            ).picking_id
 
     def action_deliver(self):
         self.ensure_one()
