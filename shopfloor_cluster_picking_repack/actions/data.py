a0ed045e2447ead069a0507fda385a696b7d599c
Denis Roussel <denis.roussel@acsone.eu>
2024-09-20 16:44:01 +0200

    [IMP] shopfloor_packing: Move data retrieval to data action + enable tests

--- shopfloor_cluster_picking_repack/actions/data.py
+++ shopfloor_cluster_picking_repack/actions/data.py
@@ -12,3 +12,21 @@ class DataAction(Component):
         res = super()._package_parser
         res.append("is_internal")
         return res
+
+    def _data_for_packing_info(self, picking):
+        """Return the packing information
+
+        Intended to be extended.
+        """
+        # TODO: This could be avoided if included in the picking parser.
+        return ""
+
+    def select_package(self, picking, lines):
+        return {
+            "selected_move_lines": self.move_lines(lines.sorted()),
+            "picking": self.picking(picking),
+            "packing_info": self._data_for_packing_info(picking),
+            # "no_package_enabled": not self.options.get("checkout__disable_no_package"),
+            # Used by inheriting module
+            "package_allowed": True,
+        }
--- shopfloor_cluster_picking_repack/tests/__init__.py
+++ shopfloor_cluster_picking_repack/tests/__init__.py
@@ -1,5 +1,5 @@
-from . import (  # test_cluster_picking_pick_pack,
-    common,
+from . import (
     test_cluster_picking_pack_picking,
+    test_cluster_picking_pick_pack,
     test_cluster_picking_unload,
 )
