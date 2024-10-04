f0eb29fc2a4864b0ce914b9076cec09fa99d3d16
Jacques-Etienne Baudoux <je@bcim.be>
2024-10-04 14:16:32 +0200

    stock_release_channel: minor fix

--- stock_release_channel/models/stock_release_channel.py
+++ stock_release_channel/models/stock_release_channel.py
@@ -193,7 +193,7 @@ class StockReleaseChannel(models.Model):
         selection=[("open", "Open"), ("locked", "Locked"), ("asleep", "Asleep")],
         help="The state allows you to control the availability of the release channel.\n"
         "* Open: Manual and automatic picking assignment to the release is effective "
-        "and release operations are allowed.\n "
+        "and release operations are allowed.\n"
         "* Locked: Release operations are forbidden. (Assignement processes are "
         "still working)\n"
         "* Asleep: Assigned pickings not processed are unassigned from the release "
