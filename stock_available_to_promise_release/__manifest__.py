b4eacb37ae0e3449b638bd7b90a6f4ed572240a9
samibc2c <sami.bouzidi@camptocamp.com>
2025-01-01 12:04:22 +0000

    Added translation using Weblate (French)

--- stock_available_to_promise_release/__manifest__.py
+++ stock_available_to_promise_release/__manifest__.py
@@ -3,7 +3,7 @@
 
 {
     "name": "Stock Available to Promise Release",
-    "version": "16.0.3.6.1",
+    "version": "16.0.3.6.2",
     "summary": "Release Operations based on available to promise",
     "author": "Camptocamp, BCIM, Odoo Community Association (OCA)",
     "website": "https://github.com/OCA/wms",
