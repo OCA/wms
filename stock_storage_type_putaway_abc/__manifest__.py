1e5203cce5a3afa6779d37a4d32dffe2896bd060
Sébastien Alix <sebastien.alix@camptocamp.com>
2025-05-06 12:02:28 +0200

    [MIG] stock_storage_type_putaway_abc: Migration to 18.0

--- stock_storage_type_putaway_abc/__manifest__.py
+++ stock_storage_type_putaway_abc/__manifest__.py
@@ -4,7 +4,7 @@
 {
     "name": "Stock Storage Type ABC Strategy",
     "summary": "Advanced storage strategy ABC for WMS",
-    "version": "16.0.1.0.0",
+    "version": "18.0.1.0.0",
     "category": "Warehouse Management",
     "website": "https://github.com/OCA/wms",
     "author": "Camptocamp, BCIM, Odoo Community Association (OCA)",
