26024895b13a51b2b951469caffe1a56e7747e3c
Jacques-Etienne Baudoux <je@bcim.be>
2025-05-18 19:16:02 +0200

    [ADD] sale_stock_release_channel_delivery

---
+++ sale_stock_release_channel_delivery/__manifest__.py
@@ -0,0 +1,16 @@
+# Copyright 2025 Jacques-Etienne Baudoux (BCIM) <je@bcim.be>
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
+
+{
+    "name": "Sales Stock Release Channel Delivery",
+    "version": "16.0.1.0.0",
+    "license": "AGPL-3",
+    "maintainers": ["jbaudoux"],
+    "author": "BCIM, Odoo Community Association (OCA)",
+    "website": "https://github.com/OCA/wms",
+    "depends": [
+        "sale_stock_release_channel",
+        "stock_release_channel_delivery",
+    ],
+    "auto_install": True,
+}
