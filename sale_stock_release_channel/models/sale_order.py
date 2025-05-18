a5e2f228ddf7eda6d41790b1651df183c389e1ce
Jacques-Etienne Baudoux <je@bcim.be>
2025-05-18 14:00:43 +0200

    [ADD] sale_stock_release_channel

---
+++ sale_stock_release_channel/models/sale_order.py
@@ -0,0 +1,24 @@
+# Copyright 2025 Jacques-Etienne Baudoux (BCIM) <je@bcim.be>
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
+
+from odoo import models
+
+
+class SaleOrder(models.Model):
+
+    _inherit = "sale.order"
+
+    @property
+    def _release_channel_possible_candidate_domain_base(self):
+        """Base domain for finding channel candidates based on picking.
+
+        Mimick the domain defined on stock.picking
+        Do not check company, only warehouse
+        """
+        # Nice to have: also check picking types contain one of the outgoing
+        # picking types of the warehouse. For now, we rely on the warehouse
+        # properly filled in in case of multi-warehouse setup. This is
+        # reasonable.
+        return [
+            ("warehouse_id", "in", (False, self.warehouse_id.id)),
+        ]
---
+++ sale_stock_release_channel/tests/common.py
@@ -0,0 +1,14 @@
+# Copyright 2025 Jacques-Etienne Baudoux (BCIM) <je@bcim.be>
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
+
+from odoo.tests.common import Form, TransactionCase
+
+
+class SaleStockReleaseChannelCommon(TransactionCase):
+    @classmethod
+    def setUpClass(cls):
+        super().setUpClass()
+        cls.customer = cls.env.ref("base.res_partner_1")
+        sale_form = Form(cls.env["sale.order"])
+        sale_form.partner_id = cls.customer
+        cls.so = sale_form.save()
