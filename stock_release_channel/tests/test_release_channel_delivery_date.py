890054570e87d217fc3348639fd09ca72821f06a
Jacques-Etienne Baudoux <je@bcim.be>
2025-05-26 22:04:24 +0200

    stock_release_channel: add test for extension modules

--- stock_release_channel/tests/test_release_channel_delivery_date.py
+++ stock_release_channel/tests/test_release_channel_delivery_date.py
@@ -5,15 +5,26 @@ from freezegun import freeze_time
 
 from odoo import fields
 
-from .common import StockReleaseChannelDeliveryDateCommon
+from .common import ReleaseChannelCase, StockReleaseChannelDeliveryDateCommon
 
 to_datetime = fields.Datetime.to_datetime
 
 
-class TestStockReleaseChannelDeliverydate(StockReleaseChannelDeliveryDateCommon):
+class TestReleaseChannelDeliveryDateFake(StockReleaseChannelDeliveryDateCommon):
     @freeze_time("2025-01-02 10:00:00")
     def test_delivery_date(self):
         """Test generator on channel object"""
         now = fields.Datetime.now()
         dt = self.channel._get_earliest_delivery_date(self.partner, now)
         self.assertEqual(dt, to_datetime("2025-01-04 10:00:00"))
+
+
+class TestReleaseChannelDeliveryDate(ReleaseChannelCase):
+    def test_compute_delivery_date(self):
+        """Test delivery date computes with registered generators
+
+        This test will run with other modules loaded.
+        """
+        now = fields.Datetime.now()
+        partner = self.env.ref("base.main_partner")
+        self.default_channel._get_earliest_delivery_date(partner, now)
