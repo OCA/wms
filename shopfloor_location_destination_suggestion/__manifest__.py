3e9057c292fd4d759859ccc10553320148361755
Denis Roussel <denis.roussel@acsone.eu>
2025-06-26 13:02:19 +0200

    [16.0][ADD] shopfloor_location_destination_suggestion

---
+++ shopfloor_location_destination_suggestion/__manifest__.py
@@ -0,0 +1,15 @@
+# Copyright 2024 ACSONE SA/NV
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
+
+{
+    "name": "Shopfloor Location Destination Suggestion",
+    "summary": """This module allows to provide destination suggestion to users""",
+    "version": "16.0.1.0.0",
+    "license": "AGPL-3",
+    "author": "ACSONE SA/NV,Odoo Community Association (OCA)",
+    "website": "https://github.com/OCA/wms",
+    "depends": [
+        "shopfloor",
+        "stock_picking_operation_destination_suggestion",
+    ],
+}
