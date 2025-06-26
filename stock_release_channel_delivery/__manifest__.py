bb54541f845793c11c091c001e2cbc4ff4e3bf7a
Ruchir Shukla <ruchir@bizzappdev.com>
2025-06-26 16:00:22 +0530

    [MIG] stock_release_channel_delivery: Migration to 18.0

--- stock_release_channel_delivery/__manifest__.py
+++ stock_release_channel_delivery/__manifest__.py
@@ -5,7 +5,7 @@
     "name": "Stock Release Channel Delivery",
     "summary": """
         Add a carrier selection criteria on the release channel """,
-    "version": "16.0.3.0.0",
+    "version": "18.0.1.0.0",
     "license": "AGPL-3",
     "author": "ACSONE SA/NV,BCIM,Odoo Community Association (OCA)",
     "website": "https://github.com/OCA/wms",
