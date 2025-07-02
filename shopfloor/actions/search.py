426acd92eebd3d52eef15f4f77ba610616cad7b8
Denis Roussel <denis.roussel@acsone.eu>
2025-07-02 12:56:10 +0200

    [IMP] shopfloor: Add dummy barcode handler for expiration dates

--- shopfloor/actions/search.py
+++ shopfloor/actions/search.py
@@ -65,7 +65,7 @@ class SearchAction(Component):
             "delivery_packaging": self.delivery_packaging_from_scan,
             "origin_move": self.origin_move_from_scan,
             # Extra data can be contained in barcodes
-            "expiration_date": self.dummy_from_scan,
+            "expiration_date": self.expiration_date_from_scan,
         }
 
     def _make_search_result(self, **kwargs):
@@ -208,4 +208,8 @@ class SearchAction(Component):
         return model.search(outgoing_move_domain)
 
     def dummy_from_scan(self, barcode):
+        return None
+
+    def expiration_date_from_scan(self, barcode):
+        # TODO
         return None
