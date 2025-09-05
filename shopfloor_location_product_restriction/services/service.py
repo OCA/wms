5f0b87e60f6dc68c040ad171b86c05b8e171da74
Denis Roussel <denis.roussel@acsone.eu>
2025-09-05 15:33:21 +0200

    [FIX] shopfloor_location_product_restriction: Use pick_type argument

--- shopfloor_location_product_restriction/services/service.py
+++ shopfloor_location_product_restriction/services/service.py
@@ -7,12 +7,12 @@ from odoo.addons.component.core import AbstractComponent
 class BaseShopfloorProcess(AbstractComponent):
     _inherit = "base.shopfloor.process"
 
-    def is_dest_location_valid(self, moves, location):
+    def is_dest_location_valid(self, moves, location, pick_type=False):
         """
         Check if destination location is restricted for
         current moves
         """
-        result = super().is_dest_location_valid(moves, location)
+        result = super().is_dest_location_valid(moves, location, pick_type=pick_type)
         for move in moves:
             if location._check_has_location_product_restriction(move.product_id):
                 return bool(False and result)
