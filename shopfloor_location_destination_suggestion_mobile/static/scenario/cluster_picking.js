8334f448083e35aaf0d6119a30c4af5a515144ac
Laurent Mignon (ACSONE) <laurent.mignon@acsone.eu>
2025-09-05 16:58:05 +0200

    [IMP] shopfloor_location_destination_suggestion_mobile: More relevant title
    Makes the suggestion part of the title for the location destination card

--- shopfloor_location_destination_suggestion_mobile/static/scenario/cluster_picking.js
+++ shopfloor_location_destination_suggestion_mobile/static/scenario/cluster_picking.js
@@ -19,3 +19,49 @@ methods.location_operation_detail_fields = function () {
     ];
     return new_result;
 };
+
+// Adds method to compute the title from the record
+methods._get_title_from_record = function (record) {
+    let cart_title = record?.location_dest?.name || "";
+    if (record?.location_destination_suggestions) {
+        cart_title = cart_title + " / " + record.location_destination_suggestions;
+    }
+    return cart_title;
+};
+
+// Adds method to set the title property on the record
+methods._set_record_title = function (record) {
+    const title = this._get_title_from_record(record);
+    Vue.set(record, "title", title);
+};
+
+// Adds handler to define add the new property on the record
+// At component creation
+const created_original =
+    location_operation_detail_component.extendOptions.created || function () {};
+
+const created_method = function () {
+    created_original.bind(this)();
+    this._set_record_title(this.record);
+};
+location_operation_detail_component.extendOptions.created = created_method;
+
+// Adds handler to define add the new property on the record
+// At record update
+location_operation_detail_component.extendOptions.watch = {
+    record: {
+        handler(newVal) {
+            this._set_record_title(newVal);
+        },
+        deep: true,
+    },
+};
+
+// override the title to display the destination location name and suggestion
+const location_operation_detail_options_method =
+    methods.location_operation_detail_options;
+methods.location_operation_detail_options = function () {
+    const result = location_operation_detail_options_method.bind(this)();
+    result.key_title = "title";
+    return result;
+};
