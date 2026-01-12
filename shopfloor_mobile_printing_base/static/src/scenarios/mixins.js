c5aaa0e805ad516c71f6772be20b4c9bcb77db4e
Nicolas Delbovier <nicolas.delbovier@acsone.eu>
2026-01-12 17:14:44 +0100

    [FIX] shopfloor_printing_base: ensure "allow_printing_label" gets stored in session at refresh.
    In previous implementation, the variable was only given in API response but not stored inside session storage. As a refresh does not query odoo again but looks for variables inside session, the value was lost at refresh (and replaced with default false value).

--- shopfloor_mobile_printing_base/static/src/scenarios/mixins.js
+++ shopfloor_mobile_printing_base/static/src/scenarios/mixins.js
@@ -6,34 +6,7 @@
 import {ScenarioBaseMixin} from "/shopfloor_mobile_base/static/wms/src/scenario/mixins.js";
 
 const methods = ScenarioBaseMixin.methods;
-const data_method = ScenarioBaseMixin.data;
-
-const on_call_success_method = methods.on_call_success;
-
-methods.on_call_success = function (result) {
-    // This will fill in the 'allow_print_label' value from service call result
-    on_call_success_method.bind(this)(result);
-    if (result.allow_print_label) {
-        this.set_allow_print_label(result.allow_print_label);
-    }
-};
-
-methods.set_allow_print_label = function (allow_print_label) {
-    this.allow_print_label = allow_print_label;
-};
 
 methods._print_label_allowed = function () {
-    // Use this method in scenarios to display the component or not
-    // TODO: Should this be part of the python code in service response
-    // depending on the state ?
-    return this.allow_print_label;
-};
-
-ScenarioBaseMixin.data = function () {
-    const result = data_method();
-    const new_result = {
-        ...result,
-        allow_print_label: false,
-    };
-    return new_result;
+    return this.state.data && this.state.data.allow_print_label === true;
 };
