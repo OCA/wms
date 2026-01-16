c39629e0cb35fcce3d293ad343656a3cc1f550c9
Nicolas Delbovier <nicolas.delbovier@acsone.eu>
2026-01-16 17:23:58 +0100

    [REF] shopfloor_printing_base: pass "allow_print_label" variable to global next state fields

--- shopfloor_printing_base/services/validator.py
+++ shopfloor_printing_base/services/validator.py
@@ -4,10 +4,14 @@ from odoo.addons.component.core import AbstractComponent
 class BaseShopfloorValidatorResponse(AbstractComponent):
     _inherit = "base.shopfloor.validator.response"
 
-    def _response_schema(self, data_schema=None, next_states=None):
-        data_schema = data_schema or {}
-        data_schema.update({"allow_print_label": {"type": "boolean", "nullable": True}})
-
-        return super()._response_schema(
-            data_schema=data_schema, next_states=next_states
+    def _get_global_fields_schemas(self) -> dict:
+        res = super()._get_global_fields_schemas()
+        res.update(
+            {
+                "allow_print_label": {
+                    "type": "boolean",
+                    "nullable": True,
+                }
+            }
         )
+        return res
