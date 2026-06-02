21ac02e9833bb1b6a2ff4819a4bd0e9f30b9a89f
Nicolas Delbovier <nicolas.delbovier@acsone.eu>
2026-06-02 10:32:06 +0200

    [ADD] shopfloor_reception_dock_mobile: show docks info in picking details cards

---
+++ shopfloor_reception_dock_mobile/__manifest__.py
@@ -0,0 +1,15 @@
+# Copyright 2026 ACSONE SA/NV
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
+
+{
+    "name": "Shopfloor Reception Dock Mobile",
+    "summary": """Add docks info to picking cards in shopfloor app.""",
+    "version": "16.0.1.0.0",
+    "license": "AGPL-3",
+    "author": "ACSONE SA/NV,Odoo Community Association (OCA)",
+    "website": "https://github.com/OCA/wms",
+    "maintainers": ["jbaudoux", "nicolas-delbovier-acsone"],
+    "depends": ["shopfloor_reception_dock", "shopfloor_reception_mobile"],
+    "data": ["templates/assets.xml"],
+    "demo": [],
+}
