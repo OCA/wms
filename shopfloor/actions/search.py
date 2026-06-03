23f08c0b8949eab956898575f90407a8d89eaf78
Nicolas Delbovier <nicolas.delbovier@acsone.eu>
2026-06-03 11:29:29 +0200

    [IMP] shopfloor: avoid useless searches in `generic_find`
    When the parser was able to detect some type (e.g. "lot") this is stupid to test all the others that we know will not work ("packaging", "product", ...)

--- shopfloor/actions/search.py
+++ shopfloor/actions/search.py
@@ -8,7 +8,6 @@ from odoo.addons.component.core import Component
 
 
 class SearchResult:
-
     __slots__ = ("record", "type", "code", "parse_result")
 
     def __init__(self, **kw) -> None:
@@ -96,7 +95,8 @@ class SearchAction(Component):
         # TODO: decide the best default order in case we don't pass `types`
         parse_results = self.parser.parse(barcode, types)
         for parse_result in parse_results:
-            for btype in _types:
+            btypes = _types if parse_result.type == "unknown" else [parse_result.type]
+            for btype in btypes:
                 record = self._find_record_by_type(
                     parse_result.value, btype, handler_kw
                 )
