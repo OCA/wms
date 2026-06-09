46773b74e563936c3aff67b134daad5dcb35a6d2
Nicolas Delbovier <nicolas.delbovier@acsone.eu>
2026-06-09 13:47:11 +0200

    [FIX]: shopfloor: add missing test setup from v18

--- shopfloor/tests/test_stock_split.py
+++ shopfloor/tests/test_stock_split.py
@@ -12,19 +12,114 @@ class TestStockSplit(ModelCommon):
         cls._update_qty_in_location(
             cls.stock_location, cls.product_a, 6, package=cls.package_1
         )
-
-    @classmethod
-    def _update_qty_in_location(
-        cls, location, product, quantity, package=None, lot=None
-    ):
-        quants = cls.env["stock.quant"]._gather(
-            product, location, lot_id=lot, package_id=package, strict=True
+        cls._update_qty_in_location(
+            cls.stock_location, cls.product_a, 4, package=cls.package_2
         )
-        # this method adds the quantity to the current quantity, so remove it
-        quantity -= sum(quants.mapped("quantity"))
-        cls.env["stock.quant"]._update_available_quantity(
-            product, location, quantity, package_id=package, lot_id=lot
+        cls._update_qty_in_location(
+            cls.stock_location, cls.product_a, 5, package=cls.package_3
         )
+        # Put product_b quantities in stock
+        cls._update_qty_in_location(cls.stock_location, cls.product_b, 10)
+        # Create the pick/pack/ship transfer
+        cls.ship_move_a = cls.env["stock.move"].create(
+            {
+                "name": cls.product_a.display_name,
+                "product_id": cls.product_a.id,
+                "product_uom_qty": 15.0,
+                "product_uom": cls.product_a.uom_id.id,
+                "location_id": cls.ship_location.id,
+                "location_dest_id": cls.customer_location.id,
+                "warehouse_id": cls.warehouse.id,
+                "picking_type_id": cls.warehouse.out_type_id.id,
+                "procure_method": "make_to_order",
+                "state": "waiting",
+            }
+        )
+        cls.pack_move_a = cls.env["stock.move"].create(
+            {
+                "name": cls.product_a.display_name,
+                "product_id": cls.product_a.id,
+                "product_uom_qty": 15.0,
+                "product_uom": cls.product_a.uom_id.id,
+                "location_id": cls.pack_location.id,
+                "location_dest_id": cls.ship_location.id,
+                "warehouse_id": cls.warehouse.id,
+                "picking_type_id": cls.warehouse.pack_type_id.id,
+                "procure_method": "make_to_order",
+                "state": "waiting",
+                "move_dest_ids": [(4, cls.ship_move_a.id)],
+            }
+        )
+        cls.pick_move_a = cls.env["stock.move"].create(
+            {
+                "name": cls.product_a.display_name,
+                "product_id": cls.product_a.id,
+                "product_uom_qty": 15.0,
+                "product_uom": cls.product_a.uom_id.id,
+                "location_id": cls.stock_location.id,
+                "location_dest_id": cls.pack_location.id,
+                "warehouse_id": cls.warehouse.id,
+                "picking_type_id": cls.warehouse.pick_type_id.id,
+                "procure_method": "make_to_stock",
+                "state": "confirmed",
+                "move_dest_ids": [(4, cls.pack_move_a.id)],
+            }
+        )
+        cls.ship_move_b = cls.env["stock.move"].create(
+            {
+                "name": cls.product_b.display_name,
+                "product_id": cls.product_b.id,
+                "product_uom_qty": 4,
+                "product_uom": cls.product_b.uom_id.id,
+                "location_id": cls.ship_location.id,
+                "location_dest_id": cls.customer_location.id,
+                "warehouse_id": cls.warehouse.id,
+                "picking_type_id": cls.warehouse.out_type_id.id,
+                "procure_method": "make_to_order",
+                "state": "waiting",
+            }
+        )
+        cls.pack_move_b = cls.env["stock.move"].create(
+            {
+                "name": cls.product_b.display_name,
+                "product_id": cls.product_b.id,
+                "product_uom_qty": 4.0,
+                "product_uom": cls.product_b.uom_id.id,
+                "location_id": cls.pack_location.id,
+                "location_dest_id": cls.ship_location.id,
+                "warehouse_id": cls.warehouse.id,
+                "picking_type_id": cls.warehouse.pack_type_id.id,
+                "procure_method": "make_to_order",
+                "state": "waiting",
+                "move_dest_ids": [(4, cls.ship_move_b.id)],
+            }
+        )
+        cls.pick_move_b = cls.env["stock.move"].create(
+            {
+                "name": cls.product_b.display_name,
+                "product_id": cls.product_b.id,
+                "product_uom_qty": 4.0,
+                "product_uom": cls.product_b.uom_id.id,
+                "location_id": cls.stock_location.id,
+                "location_dest_id": cls.pack_location.id,
+                "warehouse_id": cls.warehouse.id,
+                "picking_type_id": cls.warehouse.pick_type_id.id,
+                "procure_method": "make_to_stock",
+                "state": "confirmed",
+                "move_dest_ids": [(4, cls.pack_move_b.id)],
+            }
+        )
+        (
+            cls.ship_move_a
+            | cls.ship_move_b
+            | cls.pack_move_a
+            | cls.pack_move_b
+            | cls.pick_move_a
+            | cls.pick_move_b
+        )._assign_picking()
+        cls.picking = cls.pick_move_a.picking_id
+        cls.packing = cls.pack_move_a.picking_id
+        cls.picking.action_assign()
 
     def test_split_pickings_from_source_location(self):
         dest_location = self.pick_move_a.location_dest_id.sudo().copy(
@@ -50,7 +145,8 @@ class TestStockSplit(ModelCommon):
         self.assertEqual(len(self.pack_move_a.move_line_ids), 3)
         self.assertEqual(len(self.packing.package_level_ids), 3)
         self.assertEqual(len(move_lines_to_process), 1)
-        new_packing = move_lines_to_process._split_pickings_from_source_location()
+        move_lines_to_process._extract_in_split_order()
+        new_packing = self.packing.backorder_ids
         self.assertEqual(len(self.packing.package_level_ids), 2)
         self.assertEqual(len(new_packing.package_level_ids), 1)
         self.assertEqual(len(new_packing.move_line_ids), 1)
