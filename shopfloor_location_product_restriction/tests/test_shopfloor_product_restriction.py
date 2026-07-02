0dd612b04b64bd0fca7756d42cdf65f98fc6c4ff
Denis Roussel <denis.roussel@acsone.eu>
2026-07-02 16:11:20 +0200

    [IMP] shopfloor_location_product_restriction: Use message_queue to store explicit messages

--- shopfloor_location_product_restriction/tests/test_shopfloor_product_restriction.py
+++ shopfloor_location_product_restriction/tests/test_shopfloor_product_restriction.py
@@ -18,7 +18,15 @@ class TestStockLocation(CommonCase):
         cls.StockLocation = cls.StockLocation.sudo()
         cls.move_obj = cls.env["stock.move"]
         cls.StockLocation._parent_store_compute()
+        cls.parking = cls.StockLocation.sudo().create(
+            {
+                "name": "Parking",
+                "location_id": cls.wh.view_location_id.id,
+                "barcode": "L#PARK",
+            }
+        )
         cls.loc_lvl = cls.env.ref("stock.stock_location_locations")
+        cls.loc_lvl.sudo().usage = "view"
         cls.loc_lvl_1 = cls.StockLocation.create(
             {"name": "level_1", "location_id": cls.loc_lvl.id}
         )
@@ -44,40 +52,32 @@ class TestStockLocation(CommonCase):
             {"name": "Stone", "type": "product", "uom_id": cls.uom_unit.id}
         )
 
-        # # quants
-        # StockQuant = cls.env["stock.quant"].sudo()
-        # cls.quant_1_lvl_1_1_1 = StockQuant.create(
-        #     {
-        #         "product_id": cls.product_1.id,
-        #         "location_id": cls.loc_lvl_1_1_1.id,
-        #         "quantity": 10.0,
-        #         "owner_id": cls.env.user.id,
-        #     }
-        # )
-        # cls.quant_2_lvl_1_1_1 = StockQuant.create(
-        #     {
-        #         "product_id": cls.product_2.id,
-        #         "location_id": cls.loc_lvl_1_1_1.id,
-        #         "quantity": 10.0,
-        #         "owner_id": cls.env.user.id,
-        #     }
-        # )
-        # cls.quant_1_lvl_1_1_2 = StockQuant.create(
-        #     {
-        #         "product_id": cls.product_1.id,
-        #         "location_id": cls.loc_lvl_1_1_2.id,
-        #         "quantity": 10.0,
-        #         "owner_id": cls.env.user.id,
-        #     }
-        # )
-        # cls.quant_2_lvl_1_1_2 = StockQuant.create(
-        #     {
-        #         "product_id": cls.product_2.id,
-        #         "location_id": cls.loc_lvl_1_1_2.id,
-        #         "quantity": 10.0,
-        #         "owner_id": cls.env.user.id,
-        #     }
-        # )
+        # Create Putaways
+        cls.env["stock.putaway.rule"].sudo().create(
+            {
+                "product_id": cls.product_2.id,
+                "location_in_id": cls.loc_lvl.id,
+                "location_out_id": cls.loc_lvl_1_1_1.id,
+            }
+        )
+
+        cls.store = (
+            cls.env["stock.picking.type"]
+            .sudo()
+            .create(
+                {
+                    "name": "Parking -> Stock",
+                    "sequence_code": "PARK/",
+                    "default_location_dest_id": cls.loc_lvl.id,
+                    "default_location_src_id": cls.parking.id,
+                    "code": "internal",
+                }
+            )
+        )
+        cls.menu.sudo().write(
+            {"allow_move_create": True, "move_create_is_possible": True}
+        )
+        cls.menu.sudo().picking_type_ids = cls.store
 
     def setUp(self):
         super().setUp()
@@ -87,11 +87,8 @@ class TestStockLocation(CommonCase):
         self.stock_action = self.service._actions_for("stock")
 
     def test_set_destination(self):
-        # Put product 1 in location 1-1-2
-        # Put product 2 in location 1-1-1
-
-        # Enable restriction on 1-1-2
-        # Try to transfer location 1-1-1 to 1-1-2
+        # Put product 2 in Parking
+        # Put product 1 in product 2 putaway location
         manager = (
             self.env["res.users"]
             .sudo()
@@ -105,7 +102,7 @@ class TestStockLocation(CommonCase):
             {
                 "product_id": self.product_2.id,
                 "inventory_quantity": 10.0,
-                "location_id": self.loc_lvl_1_1_1.id,
+                "location_id": self.parking.id,
             }
         )._apply_inventory()
         self.env["stock.quant"].with_user(manager).with_context(
@@ -114,32 +111,37 @@ class TestStockLocation(CommonCase):
             {
                 "product_id": self.product_1.id,
                 "inventory_quantity": 10.0,
-                "location_id": self.loc_lvl_1_1_2.id,
+                "location_id": self.loc_lvl_1_1_1.id,
             }
         )._apply_inventory()
         self.loc_lvl_1_1_2.barcode = "LVL_1_1_2"
         self.loc_lvl_1_1_1.barcode = "LVL_1_1_1"
-        self.move = self.move_obj.create(
-            {
-                "name": "Level 1-1-1 -> Level 1-1-2",
-                "location_id": self.loc_lvl_1_1_2.id,
-                "location_dest_id": self.loc_lvl_1_1_1.id,
-                "product_id": self.product_1.id,
-                "product_uom": self.product_1.uom_id.id,
-                "product_uom_qty": 5.0,
-                "picking_type_id": self.env.ref("stock.picking_type_internal").id,
-            }
+
+        response = self.service.dispatch(
+            "scan_location",
+            params={
+                "barcode": self.parking.barcode,
+            },
         )
 
-        self.move._action_confirm()
-        self.move._action_assign()
-        self.move._assign_picking()
-
-        # Assign user to move
-        self.move.move_line_ids.qty_done = 5.0
-        self.move.picking_id.user_id = self.env.user
+        self.assertEqual("scan_destination_all", response.get("next_state"))
 
         self.loc_lvl_1_1_1.product_restriction = "same"
+        response = self.service.dispatch(
+            "set_destination_all",
+            params={
+                "location_id": self.parking.id,
+                "barcode": self.loc_lvl_1_1_1.barcode,
+            },
+        )
+
+        self.assertEqual(
+            "You cannot place it here\nThe location level_1_1_1 "
+            "contains already another product than ['Stone']",
+            response.get("message").get("body"),
+        )
+        self.loc_lvl_1_1_1.product_restriction = "any"
+        self.loc_lvl_1_1_1.invalidate_recordset()
         response = self.service.dispatch(
             "set_destination_all",
             params={
@@ -148,9 +150,57 @@ class TestStockLocation(CommonCase):
             },
         )
 
-        self.assertEqual(
-            "You cannot place it here", response.get("message").get("body")
+        self.assertEqual("scan_location", response.get("next_state"))
+
+    def test_putaways(self):
+        """
+        In location content transfer, the moves destinations are computed at
+        picking creation.
+
+        If two products have the same destination, a proper message should
+        be returned to user
+        """
+        manager = (
+            self.env["res.users"]
+            .sudo()
+            .create({"name": "Manager", "login": "stock_manager"})
         )
+        manager.groups_id |= self.env.ref("stock.group_stock_manager")
+        self.env.user.groups_id |= self.env.ref("stock.group_stock_user")
+        self.env["stock.quant"].with_user(manager).with_context(
+            inventory_mode=True
+        ).create(
+            {
+                "product_id": self.product_2.id,
+                "inventory_quantity": 10.0,
+                "location_id": self.parking.id,
+            }
+        )._apply_inventory()
+        self.env["stock.quant"].with_user(manager).with_context(
+            inventory_mode=True
+        ).create(
+            {
+                "product_id": self.product_1.id,
+                "inventory_quantity": 10.0,
+                "location_id": self.loc_lvl_1_1_1.id,
+            }
+        )._apply_inventory()
+        self.loc_lvl_1_1_2.barcode = "LVL_1_1_2"
+        self.loc_lvl_1_1_1.barcode = "LVL_1_1_1"
+
+        self.loc_lvl_1_1_1.product_restriction = "same"
+        response = self.service.dispatch(
+            "scan_location",
+            params={
+                "barcode": self.parking.barcode,
+            },
+        )
+        message = (
+            "The location level_1_1_1 contains already another product than ['Stone']\n"
+            + "The content of Parking cannot be transferred to Parking with this "
+            "scenario for product Stone."
+        )
+        self.assertEqual(message, response.get("message").get("body"))
         self.loc_lvl_1_1_1.product_restriction = "any"
         self.loc_lvl_1_1_1.invalidate_recordset()
         response = self.service.dispatch(
