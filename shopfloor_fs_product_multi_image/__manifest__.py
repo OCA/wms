4fefcf6167d38407506943c8ec0223746daf7eac
Nicolas Delbovier <nicolas.delbovier@acsone.eu>
2026-07-07 16:02:51 +0200

    [ADD] shopfloor_fs_product_multi_image: glue module fixing incompatibility with fs_product_multi_image

---
+++ shopfloor_fs_product_multi_image/__manifest__.py
@@ -0,0 +1,16 @@
+# Copyright 2026 ACSONE SA/NV
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
+
+{
+    "name": "Shopfloor Fs Product Multi Image",
+    "summary": """Glue module between shopfloor and fs_product_multi_image to ensure
+    images load correctly in shopfloor screens when using fs_product_multi_image.""",
+    "version": "16.0.1.0.0",
+    "license": "AGPL-3",
+    "author": "ACSONE SA/NV,Odoo Community Association (OCA)",
+    "website": "https://github.com/OCA/wms",
+    "depends": ["shopfloor", "shopfloor_reception", "fs_product_multi_image"],
+    "data": [],
+    "demo": [],
+    "auto_install": True,
+}
---
+++ shopfloor_fs_product_multi_image/readme/DESCRIPTION.md
@@ -0,0 +1,3 @@
+This is a technical module aiming at fixing incompatibility issue between shopfloor and fs_product_multi_image.
+
+Without this glue module, your product images would not load correctly in shopfloor if you were to have the fs_product_multi_image module installed.
