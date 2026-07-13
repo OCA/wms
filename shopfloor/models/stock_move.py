e5f3fb4addf83db6638eea8e9995931aa3990fc8
Jacques-Etienne Baudoux <je@bcim.be>
2026-07-13 22:40:30 +0200

    [IMP] shopfloor: add helpers
    Helpers to decouple logic from odoo version

--- shopfloor/models/stock_move.py
+++ shopfloor/models/stock_move.py
@@ -2,11 +2,31 @@
 # Copyright 2022 Jacques-Etienne Baudoux (BCIM) <je@bcim.be>
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 from odoo import _, models
-from odoo.tools.float_utils import float_compare
+from odoo.tools.float_utils import float_compare, float_is_zero
 
 
 class StockMove(models.Model):
     _inherit = "stock.move"
+
+    @property
+    def picked(self):
+        """:return: True if there is a quantity picked."""
+        self.ensure_one()
+        return (
+            float_compare(
+                self.quantity_done,
+                0,
+                precision_rounding=self.product_uom.rounding,
+            )
+            > 0
+        )
+
+    @property
+    def has_quantity_reserved(self):
+        self.ensure_one()
+        return not float_is_zero(
+            self.reserved_availability, precision_rounding=self.product_uom.rounding
+        )
 
     def _qty_is_satisfied(self):
         compare = float_compare(
