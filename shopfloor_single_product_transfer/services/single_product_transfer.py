879581209c706f4f3333bcb50c96ea514d1cd11b
Jacques-Etienne Baudoux <je@bcim.be>
2026-09-14 18:50:19 +0200

    [IMP] shopfloor_single_product_transfer: concurrent work
    When marking a move line as picked, always check for concurrent work unless split is disabled

--- shopfloor_single_product_transfer/services/single_product_transfer.py
+++ shopfloor_single_product_transfer/services/single_product_transfer.py
@@ -13,6 +13,7 @@ from odoo.addons.base_rest.components.service import to_int
 from odoo.addons.component.core import Component
 from odoo.addons.component.exception import NoComponentError
 from odoo.addons.shopfloor.actions.search import SearchInvalidProduct
+from odoo.addons.shopfloor.exceptions import ConcurentWorkOnTransfer
 from odoo.addons.shopfloor.utils import to_float
 
 _logger = logging.getLogger("shopfloor.services.single_product_transfer")
@@ -256,7 +257,14 @@ class ShopfloorSingleProductTransfer(Component):
     ):
         move_line = self._select_move_line_from_product(product, location, package, lot)
         if move_line:
-            self._mark_move_line_as_picked(move_line, packaging=packaging)
+            try:
+                self._mark_move_line_as_picked(move_line, packaging=packaging)
+            except ConcurentWorkOnTransfer:
+                return self._response_for_select_product(
+                    location=location,
+                    package=package,
+                    message=self.msg_store.concurrent_work(),
+                )
             return self._response_for_set_quantity(move_line)
 
     def _mark_move_line_as_picked(self, move_line, packaging=None):
