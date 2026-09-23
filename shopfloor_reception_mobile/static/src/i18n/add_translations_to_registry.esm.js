4fb9ae12d7e141cf69d51648a553aafad696c401
Nicolas Delbovier <nicolas.delbovier@acsone.eu>
2026-09-23 15:59:00 +0200

    [IMP] shopfloor_reception_mobile: simplify translation language keys from locale variants to base ISO codes (e.g., fr-FR -> fr)

--- shopfloor_reception_mobile/static/src/i18n/add_translations_to_registry.esm.js
+++ shopfloor_reception_mobile/static/src/i18n/add_translations_to_registry.esm.js
@@ -1,10 +1,4 @@
 import {translation_registry} from "/shopfloor_mobile_base/static/wms/src/services/translation_registry.js";
 
-translation_registry.load(
-    "en-US",
-    "/shopfloor_reception_mobile/static/src/i18n/en.json"
-);
-translation_registry.load(
-    "fr-FR",
-    "/shopfloor_reception_mobile/static/src/i18n/fr.json"
-);
+translation_registry.load("en", "/shopfloor_reception_mobile/static/src/i18n/en.json");
+translation_registry.load("fr", "/shopfloor_reception_mobile/static/src/i18n/fr.json");
